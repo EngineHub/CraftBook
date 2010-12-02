e905e90c2fdaf15316163e89760cd2ba73963082
sk89q <the.sk89q@gmail.com>
2010-12-02 11:24:15 -0800

    Added MC1201: Dispenser.

--- src/CraftBookListener.java
+++ src/CraftBookListener.java
@@ -182,6 +182,7 @@ public class CraftBookListener extends PluginListener {
         sisoICs.put("MC1110", new MC1110());
         sisoICs.put("MC1111", new MC1111());
         sisoICs.put("MC1200", new MC1200());
+        sisoICs.put("MC1201", new MC1201());
         sisoICs.put("MC1230", new MC1230());
         sisoICs.put("MC1231", new MC1231());
         si3oICs.put("MC2020", new MC2020());
