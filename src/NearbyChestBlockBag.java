24eeed3490c17ec095d26a0c5a3cd9de9a41789f
sk89q <the.sk89q@gmail.com>
2010-12-04 10:01:28 -0800

    Updated for 0.2.8.

--- src/NearbyChestBlockBag.java
+++ src/NearbyChestBlockBag.java
@@ -53,7 +53,7 @@ public class NearbyChestBlockBag extends BlockBag {
     public void fetchBlock(int id) throws BlockBagException {
         for (BagComplexBlock<Chest> c : chests) {
             Chest chest = c.getChest();
-            hm[] itemArray = chest.getArray();
+            hn[] itemArray = chest.getArray();
             
             // Find the item
             for (int i = 0; itemArray.length > i; i++) {
@@ -89,7 +89,7 @@ public class NearbyChestBlockBag extends BlockBag {
     public void storeBlock(int id) throws BlockBagException {
         for (BagComplexBlock<Chest> c : chests) {
             Chest chest = c.getChest();
-            hm[] itemArray = chest.getArray();
+            hn[] itemArray = chest.getArray();
             int emptySlot = -1;
 
             // Find an existing slot to put it into
@@ -110,7 +110,7 @@ public class NearbyChestBlockBag extends BlockBag {
 
             // Didn't find an existing stack, so let's create a new one
             if (emptySlot != -1) {
-                itemArray[emptySlot] = new hm(id, 1);
+                itemArray[emptySlot] = new hn(id, 1);
                 flushChanges(); // Just in case
                 
                 return;
@@ -142,7 +142,7 @@ public class NearbyChestBlockBag extends BlockBag {
 
                         if (complexBlock instanceof Chest) {
                             Chest chest = (Chest)complexBlock;
-                            hm[] itemArray = chest.getArray();
+                            hn[] itemArray = chest.getArray();
                             boolean occupied = false;
                             
                             // Got to make sure that at least one slot is occupied
