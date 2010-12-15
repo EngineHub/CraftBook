091370826d6c7dddd3d21fbb9269ee50c04e9162
sk89q <the.sk89q@gmail.com>
2010-12-15 01:34:41 -0800

    Allowed placement of tracks one block below for minecart dispensers.

--- src/CraftBookListener.java
+++ src/CraftBookListener.java
@@ -1856,16 +1856,20 @@ public class CraftBookListener extends PluginListener implements CustomICAccepte
                 Vector depositPt = null;
 
                 if (CraftBook.getBlockID(pt.add(1, 0, 0)) == BlockType.CHEST
-                        && CraftBook.getBlockID(pt.add(-1, 0, 0)) == BlockType.MINECART_TRACKS) {
+                        && (CraftBook.getBlockID(pt.add(-1, 0, 0)) == BlockType.MINECART_TRACKS
+                        || CraftBook.getBlockID(pt.add(-1, -1, 0)) == BlockType.MINECART_TRACKS)) {
                     depositPt = pt.add(1, 0, 0);
-                } else if(CraftBook.getBlockID(pt.add(-1, 0, 0)) == BlockType.CHEST
-                        && CraftBook.getBlockID(pt.add(1, 0, 0)) == BlockType.MINECART_TRACKS) {
+                } else if (CraftBook.getBlockID(pt.add(-1, 0, 0)) == BlockType.CHEST
+                        && (CraftBook.getBlockID(pt.add(1, 0, 0)) == BlockType.MINECART_TRACKS
+                        || CraftBook.getBlockID(pt.add(1, -1, 0)) == BlockType.MINECART_TRACKS)) {
                     depositPt = pt.add(-1, 0, 0);
-                } else if(CraftBook.getBlockID(pt.add(0, 0, 1)) == BlockType.CHEST
-                        && CraftBook.getBlockID(pt.add(0, 0, -1)) == BlockType.MINECART_TRACKS) {
+                } else if (CraftBook.getBlockID(pt.add(0, 0, 1)) == BlockType.CHEST
+                        && (CraftBook.getBlockID(pt.add(0, 0, -1)) == BlockType.MINECART_TRACKS
+                        || CraftBook.getBlockID(pt.add(0, -1, -1)) == BlockType.MINECART_TRACKS)) {
                     depositPt = pt.add(0, 0, 1);
-                } else if(CraftBook.getBlockID(pt.add(0, 0, -1)) == BlockType.CHEST
-                        && CraftBook.getBlockID(pt.add(0, 0, 1)) == BlockType.MINECART_TRACKS) {
+                } else if (CraftBook.getBlockID(pt.add(0, 0, -1)) == BlockType.CHEST
+                        && (CraftBook.getBlockID(pt.add(0, 0, 1)) == BlockType.MINECART_TRACKS
+                        || CraftBook.getBlockID(pt.add(0, -1, 1)) == BlockType.MINECART_TRACKS)) {
                     depositPt = pt.add(0, 0, -1);
                 }
 
