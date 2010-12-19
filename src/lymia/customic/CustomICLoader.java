adbda48d871fa5b614d31a236fd03c3253ca863b
Lymia <lymiahugs@gmail.com>
2010-12-19 04:38:22 +0000

    Fixed warning.

--- src/lymia/customic/CustomICLoader.java
+++ src/lymia/customic/CustomICLoader.java
@@ -22,7 +22,6 @@ import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
