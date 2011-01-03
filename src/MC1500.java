17264d9459a7ccbccae9d0081de5b9de2ed8faba
tomhartley <tomhartley@rocketmail.com>
2011-01-03 16:47:16 +0530

    Updated copyright and license

--- src/MC1500.java
+++ src/MC1500.java
@@ -1,3 +1,22 @@
+// $Id$
+/*
+ * CraftBook
+ * Copyright (C) 2010 tmhrtly <http://www.tmhrtly.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /**
  * Takes in a clock input, and outputs whether a specified player is online.
  *
