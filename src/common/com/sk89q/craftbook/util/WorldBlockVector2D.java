828b4869eab57c7b5cf48871996268e0561a00e3
sk89q <the.sk89q@gmail.com>
2011-02-25 12:29:34 -0800

    Added vector library, extending off of WorldEdit's.

---
+++ src/common/com/sk89q/craftbook/util/WorldBlockVector2D.java
@@ -0,0 +1,76 @@
+// $Id$
+/*
+ * Copyright (C) 2010, 2011 sk89q <http://www.sk89q.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package com.sk89q.craftbook.util;
+
+import org.bukkit.World;
+import com.sk89q.worldedit.Vector2D;
+
+public class WorldBlockVector2D extends WorldVector2D {
+
+    public WorldBlockVector2D(World world, double x, double z) {
+        super(world, x, z);
+    }
+
+    public WorldBlockVector2D(World world, float x, float z) {
+        super(world, x, z);
+    }
+
+    public WorldBlockVector2D(World world, int x, int z) {
+        super(world, x, z);
+    }
+
+    public WorldBlockVector2D(World world, Vector2D pt) {
+        super(world, pt);
+    }
+
+    public WorldBlockVector2D(World world) {
+        super(world);
+    }
+
+    /**
+     * Checks if another object is equivalent.
+     *
+     * @param obj
+     * @return whether the other object is equivalent
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof WorldVector2D)) {
+            return false;
+        }
+        WorldVector2D other = (WorldVector2D)obj;
+        return other.getWorld().equals(world)
+                && (int)other.getX() == (int)this.x
+                && (int)other.getZ() == (int)this.z;
+
+    }
+
+    /**
+     * Gets the hash code.
+     *
+     * @return hash code
+     */
+    @Override
+    public int hashCode() {
+        return (world.hashCode() >> 7) ^
+                ((new Double(x)).hashCode() >> 13) ^
+                (new Double(z)).hashCode();
+    }
+
+}
