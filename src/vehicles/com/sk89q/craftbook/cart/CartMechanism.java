948722e842034367edb8f5854a80244ebe72864d
hash <hash@exultant.us>
2011-03-27 17:16:14 -0500

    isActive detection for CartMechanism.

--- src/vehicles/com/sk89q/craftbook/cart/CartMechanism.java
+++ src/vehicles/com/sk89q/craftbook/cart/CartMechanism.java
@@ -1,7 +1,11 @@
 package com.sk89q.craftbook.cart;
 
+import java.util.*;
+
 import org.bukkit.block.*;
 import org.bukkit.entity.*;
+
+import com.sk89q.craftbook.*;
 
 /**
  * Implementers of CartMechanism are intended to be singletons and do all their
@@ -14,7 +18,7 @@ import org.bukkit.entity.*;
  */
 public abstract class CartMechanism {
     public abstract void impact(Minecart cart, Block entered, Block from);
-
+    
     /**
      * Determins if a cart mechanism should be enabled.
      * 
@@ -23,8 +27,13 @@ public abstract class CartMechanism {
      *         powered and false if unpowered.
      */
     public boolean isActive(Block base) {
-        
-        //TODO
-        return true;
+        boolean isWired = false;
+        for (BlockFace face : Arrays.asList(BlockFace.NORTH, BlockFace.EAST, BlockFace.SOUTH, BlockFace.WEST)) {
+            Boolean b = RedstoneUtil.isBlockFacePowered(base, face);
+            if (b == Boolean.TRUE) return true;
+            if (b == Boolean.FALSE) isWired = true;
+            //TODO ...we need so much better code for dealing with redstone directions it's not even funny.
+        }
+        return (!isWired);
     }
 }
