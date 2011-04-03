fd48718de32942d061eac02b3eeca3e674577727
Wizjany <wizjany@gmail.com>
2011-04-03 17:57:47 -0400

    Fixed 50x and 20x slow blocks boosting carts.

--- src/vehicles/com/sk89q/craftbook/cart/CartBooster.java
+++ src/vehicles/com/sk89q/craftbook/cart/CartBooster.java
@@ -20,16 +20,26 @@ package com.sk89q.craftbook.cart;
 
 import org.bukkit.block.*;
 import org.bukkit.entity.*;
+import org.bukkit.util.Vector;
 
 public class CartBooster extends CartMechanism {
     public CartBooster(double multiplier) {
         super();
         this.multiplier = multiplier;
+        this.newVelocity = newVelocity;
     }
 
     private final double multiplier;
+    private Vector newVelocity;
 
     public void impact(Minecart cart, Block entered, Block from) {
-        cart.setVelocity(cart.getVelocity().normalize().multiply(multiplier));
+        if (multiplier > 1) {
+            newVelocity = cart.getVelocity().normalize().multiply(multiplier);
+        } else if (multiplier < 1) {
+            newVelocity = cart.getVelocity().multiply(multiplier);
+        } else if (multiplier == 1) {
+            return;
+        }
+        cart.setVelocity(newVelocity);
     }
 }
