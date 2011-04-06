528616d61ea604a4f39b3c691dfc9ccfa57e994c
sk89q <the.sk89q@gmail.com>
2011-04-06 11:21:49 -0700

    No, I did not write this. Also, cleanup.

--- src/mechanisms/com/sk89q/craftbook/mech/Ammeter.java
+++ src/mechanisms/com/sk89q/craftbook/mech/Ammeter.java
@@ -15,48 +15,47 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
+ */
 
 package com.sk89q.craftbook.mech;
 
 import org.bukkit.Material;
 import org.bukkit.event.player.*;
-
 import com.sk89q.craftbook.Mechanic;
 import com.sk89q.craftbook.bukkit.MechanismsPlugin;
 
 /**
- * This allows users to Right-click to check the power level of redstone
- *
- * @author sk89q
+ * This allows users to Right-click to check the power level of redstone.
  */
 public class Ammeter extends Mechanic {
 
     protected MechanismsPlugin plugin;
-    
+
     public Ammeter(MechanismsPlugin plugin) {
         super();
         this.plugin = plugin;
     }
-    
 
     @Override
     public void onRightClick(PlayerInteractEvent event) {
-    	if(event.getPlayer().getItemInHand().getType() == Material.COAL && event.getClickedBlock().getType() == Material.REDSTONE_WIRE)
-    	{
-    		String line = getCurrentLine(event.getClickedBlock().getData());
-    		event.getPlayer().sendMessage("Current is: " + org.bukkit.ChatColor.RED + event.getClickedBlock().getData() + " " + org.bukkit.ChatColor.DARK_GREEN + line);
-    	}
+        if (event.getPlayer().getItemInHand().getType() == Material.COAL
+                && event.getClickedBlock().getType() == Material.REDSTONE_WIRE) {
+            String line = getCurrentLine(event.getClickedBlock().getData());
+            event.getPlayer().sendMessage(
+                    "Current is: " + org.bukkit.ChatColor.RED
+                            + event.getClickedBlock().getData() + " "
+                            + org.bukkit.ChatColor.DARK_GREEN + line);
+        }
     }
+
     private String getCurrentLine(byte data) {
-		String line = "[";
-		for(int i = 0;i < data;i++)
-		{
-			line = line + "|";
-		}
-		line = line + "]";
-		return line;
-	}
+        String line = "[";
+        for (int i = 0; i < data; i++) {
+            line = line + "|";
+        }
+        line = line + "]";
+        return line;
+    }
 
     @Override
     public void unload() {
@@ -64,7 +63,8 @@ public class Ammeter extends Mechanic {
 
     @Override
     public boolean isActive() {
-        return false;   // this isn't a persistent mechanic, so the manager will never keep it around long enough to even check this.
+        return false; // this isn't a persistent mechanic, so the manager will
+                      // never keep it around long enough to even check this.
     }
 
 }
