3b08a32cb922d3ed147ddae819c6fe96a43b2c4f
sk89q <the.sk89q@gmail.com>
2011-04-15 23:24:12 -0700

    Fixed permissions.

--- src/common/com/sk89q/craftbook/util/SignUtil.java
+++ src/common/com/sk89q/craftbook/util/SignUtil.java
@@ -1,3 +1,22 @@
+// $Id$
+/*
+ * CraftBook
+ * Copyright (C) 2010 sk89q <http://www.sk89q.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package com.sk89q.craftbook.util;
 
 import org.bukkit.*;
@@ -280,13 +299,12 @@ public class SignUtil {
     }
     
     /**
-     * @param BlockFace
-     *            Start from direction
+     * @param face Start from direction
      * @return clockwise direction
      */
-    public static BlockFace getClockWise(BlockFace yourFace) 
+    public static BlockFace getClockWise(BlockFace face) 
     {
-        switch (yourFace) 
+        switch (face) 
         {
 	        case NORTH: return BlockFace.EAST;
 	        case EAST: return BlockFace.SOUTH;
@@ -298,13 +316,12 @@ public class SignUtil {
     }
     
     /**
-     * @param BlockFace
-     *            Start from direction
+     * @param face Start from direction
      * @return clockwise direction
      */
-    public static BlockFace getCounterClockWise(BlockFace yourFace) 
+    public static BlockFace getCounterClockWise(BlockFace face) 
     {
-        switch (yourFace) 
+        switch (face) 
         {
 	        case NORTH: return BlockFace.WEST;
 	        case EAST: return BlockFace.NORTH;
