94596f04e3dd414d24bfedfb6ce0810ae0ce49ae
James Royston <purpleposeidon@gmail.com>
2011-06-07 15:08:07 -0700

    improved copyright

--- src/circuits/com/sk89q/craftbook/gates/world/SignSpinner.java
+++ src/circuits/com/sk89q/craftbook/gates/world/SignSpinner.java
@@ -1,6 +1,6 @@
 // $Id$
 /*
- * Copyright (C) 2011 purpleposeidon@gmail.com
+ * Copyright (C) 2011 wiz's mom
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
