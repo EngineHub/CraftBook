3342d34b69c8285167680481a49f1b0f5cecf4ce
Turtle9598 <Turtle9598@ChildersPC>
2011-08-28 23:07:55 -0400

    Fixed Spacing

--- circuits/src/main/java/com/sk89q/craftbook/circuits/GlowStone.java
+++ circuits/src/main/java/com/sk89q/craftbook/circuits/GlowStone.java
@@ -1,21 +1,21 @@
 // $Id$
 /*
-* CraftBook
-* Copyright (C) 2010 sk89q <http://www.sk89q.com>
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
+ * CraftBook
+ * Copyright (C) 2010 sk89q <http://www.sk89q.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 
 package com.sk89q.craftbook.circuits;
@@ -26,10 +26,10 @@ import com.sk89q.worldedit.blocks.BlockID;
 import com.sk89q.worldedit.bukkit.*;
 
 /**
-* This mechanism allow players to toggle GlowStone.
-*
-* @author sk89q
-*/
+ * This mechanism allow players to toggle GlowStone.
+ *
+ * @author sk89q
+ */
 public class GlowStone extends AbstractMechanic {
     public static class Factory extends AbstractMechanicFactory<GlowStone> {
         public Factory() {
@@ -48,17 +48,17 @@ public class GlowStone extends AbstractMechanic {
     }
 
     /**
-* Construct the mechanic for a location.
-*
-* @param pt
-*/
+     * Construct the mechanic for a location.
+     *
+     * @param pt
+     */
     private GlowStone(BlockWorldVector pt) {
         super();
     }
 
     /**
-* Raised when an input redstone current changes.
-*/
+     * Raised when an input redstone current changes.
+     */
     @Override
     public void onBlockRedstoneChange(SourcedBlockRedstoneEvent event) {
         byte data;
@@ -73,15 +73,15 @@ public class GlowStone extends AbstractMechanic {
     }
 
     /**
-* Unload this mechanic.
-*/
+     * Unload this mechanic.
+     */
     @Override
     public void unload() {
     }
 
     /**
-* Check if this mechanic is still active.
-*/
+     * Check if this mechanic is still active.
+     */
     @Override
     public boolean isActive() {
         return false;
