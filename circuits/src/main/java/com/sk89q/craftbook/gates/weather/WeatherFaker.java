413dc633c18c81e3b60eb2c0a12f0fd18fdc371b
me4502 <mnmiller1@hotmail.com>
2012-06-11 09:30:22 +1000

    This should do nicely.

--- circuits/src/main/java/com/sk89q/craftbook/gates/weather/WeatherFaker.java
+++ circuits/src/main/java/com/sk89q/craftbook/gates/weather/WeatherFaker.java
@@ -65,17 +65,20 @@ public class WeatherFaker extends AbstractIC implements SelfTriggeredIC{
 		if(chip.getInput(0))
 		{
 			int dist = Integer.parseInt(getSign().getLine(2));
-			dist = dist*dist;
-			//if(!(getSign().getWorld().getWeatherDuration()>0))
-				((CraftServer)getServer()).getHandle().sendPacketNearby(b.getX(), b.getY()+1,b.getZ(), dist+(2*2), ((CraftWorld) getSign().getWorld()).getHandle().dimension, new Packet70Bed(2,0));
+			if(!getSign().getWorld().hasStorm())
+				((CraftServer)getServer()).getHandle().sendPacketNearby(b.getX(), b.getY()+1,b.getZ(), dist+2, ((CraftWorld) getSign().getWorld()).getHandle().dimension, new Packet70Bed(2,0));
     		((CraftServer)getServer()).getHandle().sendPacketNearby(b.getX(), b.getY()+1,b.getZ(), dist, ((CraftWorld) getSign().getWorld()).getHandle().dimension, new Packet70Bed(1,0));
 		}
 		else if(!chip.getInput(0))
 		{
 			int dist = Integer.parseInt(getSign().getLine(2));
-			dist = dist*dist;
-			//if(!(getSign().getWorld().getWeatherDuration()>0))
-				((CraftServer)getServer()).getHandle().sendPacketNearby(b.getX(), b.getY()+1,b.getZ(), dist+(2*2), ((CraftWorld) getSign().getWorld()).getHandle().dimension, new Packet70Bed(2,0));
+			if(!getSign().getWorld().hasStorm())
+				((CraftServer)getServer()).getHandle().sendPacketNearby(b.getX(), b.getY()+1,b.getZ(), dist, ((CraftWorld) getSign().getWorld()).getHandle().dimension, new Packet70Bed(2,0));
+			else
+			{
+				((CraftServer)getServer()).getHandle().sendPacketNearby(b.getX(), b.getY()+1,b.getZ(), dist+2, ((CraftWorld) getSign().getWorld()).getHandle().dimension, new Packet70Bed(1,0));
+				((CraftServer)getServer()).getHandle().sendPacketNearby(b.getX(), b.getY()+1,b.getZ(), dist, ((CraftWorld) getSign().getWorld()).getHandle().dimension, new Packet70Bed(2,0));
+			}
 		}
 	}
 }
