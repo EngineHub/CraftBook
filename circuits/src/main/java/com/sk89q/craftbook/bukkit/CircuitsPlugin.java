87d31301eba015e754a2aa8943d9a7881159b74e
Silthus <m.silthus@gmail.com>
2012-06-25 14:56:06 +0200

    added detection ic and self triggered detection ic

--- circuits/src/main/java/com/sk89q/craftbook/bukkit/CircuitsPlugin.java
+++ circuits/src/main/java/com/sk89q/craftbook/bukkit/CircuitsPlugin.java
@@ -143,9 +143,10 @@ public class CircuitsPlugin extends BaseBukkitPlugin {
         icManager.register("MC1262", new LightSensor.Factory(server, true), familySISO);
         icManager.register("MC1263", new BlockSensor.Factory(server, true), familySISO);
         icManager.register("MC1270", new Melody.Factory(server), familySISO);
-        icManager.register("MC1420", new ClockDivider.Factory(server, true), familySISO);
-        icManager.register("MC1510", new MessageSender.Factory(server, true), familySISO);
-        
+	    icManager.register("MC1271", new Detection.Factory(server, true), familySISO);          // Restricted
+	    icManager.register("MC1420", new ClockDivider.Factory(server, true), familySISO);
+	    icManager.register("MC1510", new MessageSender.Factory(server, true), familySISO);
+
         //SI3Os
         icManager.register("MC2020", new Random3Bit.Factory(server, true), familySI3O);
         icManager.register("MC2999", new Marquee.Factory(server), familySI3O);
@@ -182,10 +183,11 @@ public class CircuitsPlugin extends BaseBukkitPlugin {
         icManager.register("MC0260", new WaterSensorST.Factory(server), familySISO);
         icManager.register("MC0261", new LavaSensorST.Factory(server), familySISO);
         icManager.register("MC0262", new LightSensorST.Factory(server), familySISO);
-        icManager.register("MC0263", new BlockSensorST.Factory(server), familySISO);
-        icManager.register("MC0420", new Clock.Factory(server), familySISO);
-        icManager.register("MC0421", new Monostable.Factory(server), familySISO);
-        icManager.register("MC0500", new RangedOutput.Factory(server, true), familySISO);
+	    icManager.register("MC0263", new BlockSensorST.Factory(server), familySISO);
+	    icManager.register("MC0271", new DetectionST.Factory(server, true), familySISO);      // Restricted
+	    icManager.register("MC0420", new Clock.Factory(server), familySISO);
+	    icManager.register("MC0421", new Monostable.Factory(server), familySISO);
+	    icManager.register("MC0500", new RangedOutput.Factory(server, true), familySISO);
         //Missing: 0020 self-triggered RNG (may cause server load issues)
 	//Missing: 0262
 	//Missing: 0420     
---
+++ circuits/src/main/java/com/sk89q/craftbook/gates/world/Detection.java
@@ -0,0 +1,215 @@
+package com.sk89q.craftbook.gates.world;
+
+import com.sk89q.craftbook.ic.*;
+import com.sk89q.craftbook.util.Enums;
+import com.sk89q.craftbook.util.SignUtil;
+import org.bukkit.Chunk;
+import org.bukkit.Location;
+import org.bukkit.Server;
+import org.bukkit.block.Sign;
+import org.bukkit.entity.*;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.logging.Logger;
+
+/**
+ * @author Silthus
+ */
+public class Detection extends AbstractIC {
+
+	private final static Logger logger = Logger.getLogger("Minecraft.Test");
+
+	private enum Type {
+		PLAYER,
+		MOBHOSTILE,
+		MOBPEACEFUL,
+		ANYMOB,
+		ANY,
+		CART,
+		STORAGECART,
+		POWEREDCART;
+
+		public boolean is(Entity entity) {
+
+			switch (this) {
+				case PLAYER:
+					logger.info("... is Player");
+					return entity instanceof Player;
+				case MOBHOSTILE:
+					logger.info("... is HOSTILE");
+					return entity instanceof Monster;
+				case MOBPEACEFUL:
+					logger.info("... is PEACEFUL");
+					return entity instanceof Animals;
+				case ANYMOB:
+					logger.info("... is ANYMOB");
+					return entity instanceof Creature;
+				case CART:
+					logger.info("... is CART");
+					return entity instanceof Minecart;
+				case STORAGECART:
+					logger.info("... is STORAGE");
+					return entity instanceof StorageMinecart;
+				case POWEREDCART:
+					logger.info("... is POWEREDCART");
+					return entity instanceof PoweredMinecart;
+				case ANY:
+					logger.info("... is ANY");
+					return true;
+			}
+			logger.info("... is NONE");
+			return false;
+		}
+
+		public static Type fromString(String name) {
+			return Enums.getEnumFromString(Detection.Type.class, name);
+		}
+	}
+
+	private final boolean risingEdge;
+	private Type type;
+
+	private int offsetX;
+	private int offsetY;
+	private int offsetZ;
+	private int radius;
+
+	public Detection(Server server, Sign block, boolean risingEdge) {
+		super(server, block);
+		this.risingEdge = risingEdge;
+		// lets set some defaults
+		offsetX = 0;
+		offsetY = 0;
+		offsetZ = 0;
+		radius = 0;
+		load();
+	}
+
+	private void load() {
+		Sign sign = getSign();
+		// lets get the type to detect first
+		this.type = Type.fromString(sign.getLine(3).trim());
+		// set the type to any if wrong format
+		if (type == null) this.type = Type.ANY;
+		// update the sign with correct upper case name
+		sign.setLine(3, type.name());
+		// now check the third line for the radius and offset
+		String line = sign.getLine(2).trim();
+		// if the line contains a = the offset is given
+		// the given string should look something like that:
+		// radius=x:y:z or radius, e.g. 1=-2:5:11
+		if (line.contains("=")) {
+			try {
+				String[] split = line.split("=");
+				this.radius = Integer.parseInt(split[0]);
+				// parse the offset
+				String[] offsetSplit = split[1].split(":");
+				offsetX = Integer.parseInt(offsetSplit[0]);
+				offsetY = Integer.parseInt(offsetSplit[1]);
+				offsetZ = Integer.parseInt(offsetSplit[2]);
+			} catch (NumberFormatException e) {
+				// do nothing and use the defaults
+			} catch (IndexOutOfBoundsException e) {
+				// do nothing and use the defaults
+			}
+		} else {
+			this.radius = Integer.parseInt(line);
+		}
+	}
+
+	@Override
+	public String getTitle() {
+		return "Detection";
+	}
+
+	@Override
+	public String getSignTitle() {
+		return "DETECTION";
+	}
+
+	@Override
+	public void trigger(ChipState chip) {
+		if (risingEdge && chip.getInput(0)
+				|| (!risingEdge && !chip.getInput(0))) {
+			chip.setOutput(0, isDetected());
+		}
+	}
+
+	protected boolean isDetected() {
+		Location location = SignUtil.getBackBlock(getSign().getBlock()).getLocation();
+		// add the offset to the location of the block connected to the sign
+		location.add(offsetX, offsetY, offsetZ);
+		for (Chunk chunk : getSurroundingChunks(location, radius)) {
+			if (chunk.isLoaded()) {
+				// get all entites from the chunks in the defined radius
+				for (Entity entity : chunk.getEntities()) {
+					if (!entity.isDead()) {
+						logger.info("Checking Entity...");
+						if (type.is(entity)) {
+							// at last check if the entity is within the radius
+							logger.info("Checking distance...");
+							if (getGreatestDistance(entity.getLocation(), location) <= radius) {
+								logger.info("...success!");
+								return true;
+							}
+						}
+					}
+				}
+			}
+		}
+		return false;
+	}
+
+	private Set<Chunk> getSurroundingChunks(Location loc, int radius) {
+		Set<Chunk> chunks = new LinkedHashSet<Chunk>();
+		Chunk chunk = loc.getChunk();
+		chunks.add(chunk);
+		// get the block the furthest away
+		loc.add(radius, 0, radius);
+		// add the chunk
+		Chunk chunk2 = loc.getChunk();
+		chunks.add(loc.getChunk());
+		// get the x, z difference between the two chunks then...
+		int z = 0;
+		// ...iterate over all chunks in between
+		for (int x = chunk.getX() - chunk2.getX(); x > 0; x--) {
+			// add all surrounding chunks one by one
+			chunks.add(chunk.getWorld().getChunkAt(chunk.getX() + x, chunk.getZ() + z));
+			for (z = chunk.getZ() - chunk2.getZ(); z > 0; z--) {
+				chunks.add(chunk.getWorld().getChunkAt(chunk.getX() + x, chunk.getZ() + z));
+			}
+		}
+		return chunks;
+	}
+
+	public static int getGreatestDistance(Location l1, Location l2) {
+		int x = Math.abs(l1.getBlockX() - l2.getBlockX());
+		int y = Math.abs(l1.getBlockY() - l2.getBlockY());
+		int z = Math.abs(l1.getBlockZ() - l2.getBlockZ());
+		if (x > y && x > z) {
+			return x;
+		} else if (y > x && y > z) {
+			return y;
+		} else if (z > x && z > y) {
+			return z;
+		} else {
+			return x;
+		}
+	}
+
+	public static class Factory extends AbstractICFactory implements RestrictedIC {
+
+		protected boolean risingEdge;
+
+		public Factory(Server server, boolean risingEdge) {
+			super(server);
+			this.risingEdge = risingEdge;
+		}
+
+		@Override
+		public IC create(Sign sign) {
+			return new Detection(getServer(), sign, risingEdge);
+		}
+	}
+}
