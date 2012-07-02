5823704eeb60e0a0d93af9e839399ca9ea14c14a
me4502 <mnmiller1@hotmail.com>
2012-07-02 20:46:27 +1000

    Fix snow not doing stuff...

--- mechanisms/src/main/java/com/sk89q/craftbook/mech/Snow.java
+++ mechanisms/src/main/java/com/sk89q/craftbook/mech/Snow.java
@@ -6,6 +6,7 @@ import java.util.logging.Level;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
@@ -27,152 +28,154 @@ public class Snow implements Listener {
     MechanismsPlugin plugin;
 
     public Snow(MechanismsPlugin plugin) {
-	this.plugin = plugin;
+        this.plugin = plugin;
     }
 
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
-	if (!plugin.getLocalConfiguration().snowSettings.placeSnow)
-	    return;
-	if(!(event.getAction() == Action.RIGHT_CLICK_BLOCK)) return;
-	try {
-	    if (event.getPlayer().getItemInHand().getType() == Material.SNOW_BALL
-		    && event.getClickedBlock().getTypeId() == 78) {
-		if (event.getClickedBlock().getData() < (byte)7) {
-		    incrementData(event.getClickedBlock());
-		}
-	    } else if (event.getPlayer().getItemInHand().getType() == Material.SNOW_BALL
-		    && event.getPlayer()
-		    .getWorld()
-		    .getBlockAt(
-			    event.getClickedBlock().getLocation()
-			    .add(0, 1, 0)).getTypeId() == 0) {
-		event.getPlayer().getWorld().getBlockAt(event.getClickedBlock().getLocation().add(0, 1, 0)).setTypeId(78);
-		incrementData(event.getPlayer().getWorld().getBlockAt(event.getClickedBlock().getLocation().add(0, 1, 0)));
-	    }
-	} catch (Exception e) {
-	}
+        if (!plugin.getLocalConfiguration().snowSettings.placeSnow)
+            return;
+        if(!(event.getAction() == Action.RIGHT_CLICK_BLOCK)) return;
+        try {
+            if (event.getPlayer().getItemInHand().getType() == Material.SNOW_BALL
+                    && event.getClickedBlock().getTypeId() == 78) {
+                if (event.getClickedBlock().getData() < (byte)7) {
+                    incrementData(event.getClickedBlock());
+                }
+            } else if (event.getPlayer().getItemInHand().getType() == Material.SNOW_BALL
+                    && event.getPlayer()
+                    .getWorld()
+                    .getBlockAt(
+                            event.getClickedBlock().getLocation()
+                            .add(0, 1, 0)).getTypeId() == 0) {
+                event.getPlayer().getWorld().getBlockAt(event.getClickedBlock().getLocation().add(0, 1, 0)).setTypeId(78);
+                incrementData(event.getPlayer().getWorld().getBlockAt(event.getClickedBlock().getLocation().add(0, 1, 0)));
+            }
+        } catch (Exception e) {
+        }
     }
 
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent event) {
-	if (!plugin.getLocalConfiguration().snowSettings.trample)
-	    return;
-	Random random = new Random();
-	if (random.nextInt(10) == 6) {
-	    Block b = event.getPlayer().getWorld()
-		    .getBlockAt(event.getPlayer().getLocation());
-	    if (b.getTypeId() == 78) {
-		if (b.getData() > (byte)1)
-		    lowerData(b);
-		else
-		    b.setTypeId(0);
-	    }
+        if (!plugin.getLocalConfiguration().snowSettings.trample)
+            return;
+        Random random = new Random();
+        if (random.nextInt(10) == 6) {
+            Block b = event.getPlayer().getWorld()
+                    .getBlockAt(event.getPlayer().getLocation());
+            if (b.getTypeId() == 78) {
+                if (b.getData() > (byte)1)
+                    lowerData(b);
+                else
+                    b.setTypeId(0);
+            }
 
-	    b = event
-		    .getPlayer()
-		    .getWorld()
-		    .getBlockAt(
-			    event.getPlayer().getLocation().subtract(0, 1, 0));
-	    if (b.getTypeId() == 78) {
-		if (b.getData() > (byte)1)
-		    lowerData(b);
-		else
-		    b.setTypeId(0);
-	    }
-	}
+            b = event
+                    .getPlayer()
+                    .getWorld()
+                    .getBlockAt(
+                            event.getPlayer().getLocation().subtract(0, 1, 0));
+            if (b.getTypeId() == 78) {
+                if (b.getData() > (byte)1)
+                    lowerData(b);
+                else
+                    b.setTypeId(0);
+            }
+        }
     }
 
     @EventHandler
     public void onBlockForm(final BlockFormEvent event) {
-	if (!plugin.getLocalConfiguration().snowSettings.enable)
-	    return;
-	if (event.getNewState().getTypeId() == BlockID.SNOW) {
-	    Block block = event.getBlock();
+        if (!plugin.getLocalConfiguration().snowSettings.enable)
+            return;
+        if (event.getNewState().getTypeId() == BlockID.SNOW) {
+            Block block = event.getBlock();
 
-	    if ((block.getTypeId() != 80) && (block.getTypeId() != 78)) {
-		if (block.getWorld()
-			.getBlockAt(block.getLocation().subtract(0, 1, 0))
-			.getTypeId() == 80
-			|| block.getWorld()
-			.getBlockAt(
-				block.getLocation().subtract(0, 1, 0))
-				.getTypeId() == 78)
-		    return;
-		Random random = new Random();
-		long delay = random.nextInt(100) + 60;
-		if (plugin
-			.getServer()
-			.getScheduler()
-			.scheduleSyncDelayedTask(plugin,
-				new makeSnow(block.getLocation()), delay * 20L) == -1)
-		    plugin.getLogger()
-		    .log(Level.SEVERE,
-			    "[CraftBookMechanisms] Snow Mechanic failed to schedule!");
-	    }
-	}
+            if ((block.getTypeId() != 80) && (block.getTypeId() != 78)) {
+                if (block.getWorld()
+                        .getBlockAt(block.getLocation().subtract(0, 1, 0))
+                        .getTypeId() == 80
+                        || block.getWorld()
+                        .getBlockAt(
+                                block.getLocation().subtract(0, 1, 0))
+                                .getTypeId() == 78)
+                    return;
+                Random random = new Random();
+                long delay = random.nextInt(100) + 60;
+                if (plugin
+                        .getServer()
+                        .getScheduler()
+                        .scheduleSyncDelayedTask(plugin,
+                                new makeSnow(block.getLocation()), delay * 20L) == -1)
+                    plugin.getLogger()
+                    .log(Level.SEVERE,
+                            "[CraftBookMechanisms] Snow Mechanic failed to schedule!");
+            }
+        }
     }
 
     public class makeSnow implements Runnable {
 
-	Location event;
+        Location event;
 
-	public makeSnow(Location event) {
-	    this.event = event;
-	}
+        public makeSnow(Location event) {
+            this.event = event;
+        }
 
-	@Override
-	public void run() {
-	    if (event.getWorld().hasStorm()) {
-		if (event.getBlock().getData() > (byte)7)
-		    return;
-		if (event.subtract(0, 1, 0).getBlock().getTypeId() == 0)
-		    return;
-		event.add(0, 1, 0);
-		if (!(event.getBlock().getTypeId() == 78))
-		    return;
-		incrementData(event.getBlock());
-		// if(event.getBlock().getData() >= (byte)7)
-		// event.getBlock().setTypeId(80);
-		Random random = new Random();
-		long delay = random.nextInt(100) + 60;
-		if (plugin
-			.getServer()
-			.getScheduler()
-			.scheduleSyncDelayedTask(plugin, new makeSnow(event),
-				delay * 20L) == -1)
-		    plugin.getLogger()
-		    .log(Level.SEVERE,
-			    "[CraftBookMechanisms] Snow Mechanic failed to schedule!");
-	    } else {
-		Random random = new Random();
-		long delay = random.nextInt(100) + 600;
-		if (plugin
-			.getServer()
-			.getScheduler()
-			.scheduleSyncDelayedTask(plugin, new makeSnow(event),
-				delay * 20L) == -1)
-		    plugin.getLogger()
-		    .log(Level.SEVERE,
-			    "[CraftBookMechanisms] Snow Mechanic failed to schedule!");
-	    }
-	}
+        @Override
+        public void run() {
+            if (event.getWorld().hasStorm()) {
+                if (event.getBlock().getData() > (byte)7)
+                    return;
+                if (event.subtract(0, 1, 0).getBlock().getTypeId() == 0)
+                    return;
+                event.add(0, 1, 0);
+                if (!(event.getBlock().getTypeId() == 78))
+                    return;
+                incrementData(event.getBlock());
+                // if(event.getBlock().getData() >= (byte)7)
+                // event.getBlock().setTypeId(80);
+                Random random = new Random();
+                long delay = random.nextInt(100) + 60;
+                if (plugin
+                        .getServer()
+                        .getScheduler()
+                        .scheduleSyncDelayedTask(plugin, new makeSnow(event),
+                                delay * 20L) == -1)
+                    plugin.getLogger()
+                    .log(Level.SEVERE,
+                            "[CraftBookMechanisms] Snow Mechanic failed to schedule!");
+            } else {
+                Random random = new Random();
+                long delay = random.nextInt(100) + 600;
+                if (plugin
+                        .getServer()
+                        .getScheduler()
+                        .scheduleSyncDelayedTask(plugin, new makeSnow(event),
+                                delay * 20L) == -1)
+                    plugin.getLogger()
+                    .log(Level.SEVERE,
+                            "[CraftBookMechanisms] Snow Mechanic failed to schedule!");
+            }
+        }
     }
 
     public void lowerData(Block block) {
-	byte newData = (byte) (block.getData() - 1);
-	setBlockDataWithNotify(block,newData);
+        byte newData = (byte) (block.getData() - 1);
+        setBlockDataWithNotify(block,newData);
     }
 
     public void incrementData(Block block) {
-	byte newData = (byte) (block.getData() + 1);
-	setBlockDataWithNotify(block,newData);
+        byte newData = (byte) (block.getData() + 1);
+        setBlockDataWithNotify(block,newData);
     }
 
     public void setBlockDataWithNotify(Block block, byte data) {
-	block.setData(data);
-	block.setType(block.getType()); 
-	// This notifies a block update, there is a bug in bukkit that doesn't
-	// notify the client when Snow's data gets changed
+        block.setData(data);
+        for(Player p : block.getWorld().getPlayers()) {
+            p.sendBlockChange(block.getLocation(), block.getTypeId(), data);
+        }
+        // This notifies a block update, there is a bug in bukkit that doesn't
+        // notify the client when Snow's data gets changed
     }
 }
