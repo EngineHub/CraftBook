92e84c1c0ca903cd71164bd7547866d76f990b08
me4502 <mnmiller1@hotmail.com>
2012-07-02 20:51:39 +1000

    Made sure effect 2001 on Particle Effect IC is a valid block.

--- circuits/src/main/java/com/sk89q/craftbook/gates/world/ParticleEffect.java
+++ circuits/src/main/java/com/sk89q/craftbook/gates/world/ParticleEffect.java
@@ -2,6 +2,7 @@ package com.sk89q.craftbook.gates.world;
 
 import net.minecraft.server.Packet61WorldEvent;
 
+import org.bukkit.Material;
 import org.bukkit.Server;
 import org.bukkit.block.Block;
 import org.bukkit.block.Sign;
@@ -33,30 +34,31 @@ public class ParticleEffect extends AbstractIC {
     @Override
     public void trigger(ChipState chip) {
         if (chip.getInput(0)) {
-        	doEffect(chip);
+            doEffect(chip);
         }
     }
-    
+
     public void doEffect(ChipState chip)
     {
-    	try
-    	{
-	    	int effectID = Integer.parseInt(getSign().getLine(2).split(":")[0]);
-	    	int effectData;
-	    	try {
-	    		effectData = Integer.parseInt(getSign().getLine(2).split(":")[1]);
-	    	}
-	    	catch(Exception e){
-	    		effectData = 0;
-	    	}
-	    	int times = Integer.parseInt(getSign().getLine(3));
-	    	Block b = SignUtil.getBackBlock(getSign().getBlock());
-	    	for(int i = 0; i < times; i++)
-	    		((CraftServer)getServer()).getHandle().sendPacketNearby(b.getX(), b.getY()+1,b.getZ(), 50, ((CraftWorld) getSign().getWorld()).getHandle().dimension, new Packet61WorldEvent(effectID, b.getX(), b.getY()+1,b.getZ(),effectData));
-    	}
-    	catch(Exception e){}
+        try
+        {
+            int effectID = Integer.parseInt(getSign().getLine(2).split(":")[0]);
+            int effectData;
+            try {
+                effectData = Integer.parseInt(getSign().getLine(2).split(":")[1]);
+            }
+            catch(Exception e){
+                effectData = 0;
+            }
+            if(effectID == 2001 && Material.getMaterial(effectData) == null) return;
+            int times = Integer.parseInt(getSign().getLine(3));
+            Block b = SignUtil.getBackBlock(getSign().getBlock());
+            for(int i = 0; i < times; i++)
+                ((CraftServer)getServer()).getHandle().sendPacketNearby(b.getX(), b.getY()+1,b.getZ(), 50, ((CraftWorld) getSign().getWorld()).getHandle().dimension, new Packet61WorldEvent(effectID, b.getX(), b.getY()+1,b.getZ(),effectData));
+        }
+        catch(Exception e){}
     }
-    
+
     public static class Factory extends AbstractICFactory {
 
         public Factory(Server server) {
