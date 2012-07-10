5442dc6494c634ab7a5300bc69751e9b96f883e3
me4502 <mnmiller1@hotmail.com>
2012-07-10 20:11:41 +1000

    Fix fire arrows being stuck in blocks

--- mechanisms/src/main/java/com/sk89q/craftbook/mech/dispenser/FireArrows.java
+++ mechanisms/src/main/java/com/sk89q/craftbook/mech/dispenser/FireArrows.java
@@ -1,9 +1,11 @@
 package com.sk89q.craftbook.mech.dispenser;
 
+import org.bukkit.block.BlockFace;
 import org.bukkit.block.Dispenser;
 import org.bukkit.entity.Arrow;
 import org.bukkit.event.block.BlockDispenseEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.material.MaterialData;
 import org.bukkit.util.Vector;
 
 public class FireArrows extends Recipe {
@@ -18,7 +20,10 @@ public class FireArrows extends Recipe {
 
     @Override
     public boolean doAction(Dispenser dis, ItemStack item, Vector velocity, BlockDispenseEvent event) {
-        Arrow a = dis.getWorld().spawnArrow(dis.getLocation(), velocity, 2.0f, 0.0f);
+        MaterialData d = dis.getBlock().getState().getData();
+        org.bukkit.material.Dispenser disp = (org.bukkit.material.Dispenser) d;
+        BlockFace face = disp.getFacing();
+        Arrow a = dis.getWorld().spawnArrow(dis.getBlock().getRelative(face).getLocation(), velocity, 4.0f, 0.0f);
         a.setFireTicks(5000);
         return true;
     }
