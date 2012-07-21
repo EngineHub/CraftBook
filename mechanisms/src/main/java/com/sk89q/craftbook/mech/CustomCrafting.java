9144e4ad05839179ea436d82c22d2e4d9af3253d
me4502 <mnmiller1@hotmail.com>
2012-07-21 11:23:54 +1000

    Fixed CustomCrafting

--- mechanisms/src/main/java/com/sk89q/craftbook/mech/CustomCrafting.java
+++ mechanisms/src/main/java/com/sk89q/craftbook/mech/CustomCrafting.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.regex.Pattern;
 
 import org.bukkit.Material;
 import org.bukkit.inventory.FurnaceRecipe;
@@ -28,7 +29,7 @@ public class CustomCrafting {
                 lastLine = lastLine.trim();
                 if(lastLine.length() == 0) continue;
                 if(lastLine.startsWith("@[")) { //Shapeless Recipe
-                    String output = lastLine.split("@[")[1].replace("]", "");
+                    String output = lastLine.split(Pattern.quote("@["))[1].replace("]", "");
                     int id = Integer.parseInt(output.split(":")[0]);
                     short data = Short.parseShort(output.split(":")[1].split("x")[0]);
                     int amount = Integer.parseInt(output.split(":")[1].split("x")[1]);
@@ -81,7 +82,7 @@ public class CustomCrafting {
                     }
                 }*/
                 else if(lastLine.startsWith("$[")) { //Furnace Recipe
-                    String output = lastLine.split("$[")[1].replace("]", "");
+                    String output = lastLine.split(Pattern.quote("$["))[1].replace("]", "");
                     int id = Integer.parseInt(output.split(":")[0]);
                     short data = Short.parseShort(output.split(":")[1].split("x")[0]);
                     int amount = Integer.parseInt(output.split(":")[1].split("x")[1]);
@@ -108,8 +109,8 @@ public class CustomCrafting {
                     else
                         plugin.getLogger().warning("Failed to add recipe!");
                 }
-                else if(lastLine.startsWith("[")) { //Shaped Recipe
-                    String output = lastLine.split("[")[1].replace("]", "");
+                else if(lastLine.startsWith("*[")) { //2x2 Shaped Recipe
+                    String output = lastLine.split(Pattern.quote("*["))[1].replace("]", "");
                     int id = Integer.parseInt(output.split(":")[0]);
                     short data = Short.parseShort(output.split(":")[1].split("x")[0]);
                     int amount = Integer.parseInt(output.split(":")[1].split("x")[1]);
@@ -120,8 +121,41 @@ public class CustomCrafting {
                     contents = contents.trim();
                     if(contents.length() == 0) continue;
                     String[] items = contents.split(",");
+                    r.shape(getShapeData(items[0].split(":")[0]) + getShapeData(items[1].split(":")[0]),
+                            getShapeData(items[2].split(":")[0]) + getShapeData(items[3].split(":")[0]));
+                    plugin.getLogger().severe(r.getShape().toString());
                     for(String item : items) {
-                        r.shape(items[0],items[1],items[2],items[3],items[4],items[5],items[6],items[7],items[8]);
+                        int iid = Integer.parseInt(item.split(":")[0]);
+                        String idata = item.split(":")[1];
+                        int iidata = -1;
+                        if(idata.equals("*"))
+                            iidata = -1;
+                        else
+                            iidata = Integer.parseInt(idata);
+
+                        r.setIngredient(new String(iid + "").charAt(0), Material.getMaterial(iid), iidata);
+                    }
+                    if(plugin.getServer().addRecipe(r))
+                        plugin.getLogger().info("Recipe Added!");
+                    else
+                        plugin.getLogger().warning("Failed to add recipe!");
+                }
+                else if(lastLine.startsWith("[")) { //Shaped Recipe
+                    String output = lastLine.split(Pattern.quote("["))[1].replace("]", "");
+                    int id = Integer.parseInt(output.split(":")[0]);
+                    short data = Short.parseShort(output.split(":")[1].split("x")[0]);
+                    int amount = Integer.parseInt(output.split(":")[1].split("x")[1]);
+                    ShapedRecipe r = new ShapedRecipe(new ItemStack(id,amount,data));
+                    String contents = br.readLine();
+                    if(contents == null) continue;
+                    contents = contents.split("#")[0];
+                    contents = contents.trim();
+                    if(contents.length() == 0) continue;
+                    String[] items = contents.split(",");
+                    r.shape(getShapeData(items[0].split(":")[0]) + getShapeData(items[1].split(":")[0]) + getShapeData(items[2].split(":")[0]),
+                            getShapeData(items[3].split(":")[0]) + getShapeData(items[4].split(":")[0]) + getShapeData(items[5].split(":")[0]),
+                            getShapeData(items[6].split(":")[0]) + getShapeData(items[7].split(":")[0]) + getShapeData(items[8].split(":")[0]));
+                    for(String item : items) {
                         int iid = Integer.parseInt(item.split(":")[0]);
                         String idata = item.split(":")[1];
                         int iidata = -1;
@@ -152,5 +186,10 @@ public class CustomCrafting {
             }
             catch(Exception ee){}
         }
+    }
+
+    public static String getShapeData(String s) {
+        s = String.valueOf(s.charAt(0));
+        return s;
     }
 }
