3284f6444a31a7a6500a0197b13104b16fb32b5e
Silthus <m.silthus@gmail.com>
2012-07-23 22:16:29 +0200

    fixed bug with material identity crisis

--- circuits/src/main/java/com/sk89q/craftbook/gates/world/SetDoor.java
+++ circuits/src/main/java/com/sk89q/craftbook/gates/world/SetDoor.java
@@ -39,16 +39,13 @@ public class SetDoor extends AbstractIC {
 				String[] split = line.split("-");
 				// parse the material data
 				if (split.length > 0) {
-					// parse the data that gets set when the block is toggled on
-					String[] strings = split[0].split(":");
-					onMaterial = Integer.parseInt(strings[0]);
-					if (strings.length > 0) onData = Integer.parseInt(strings[1]);
+					// parse the data that gets set when the block is toggled off
+					String[] strings = split[1].split(":");
+					offMaterial = Integer.parseInt(strings[0]);
+					if (strings.length > 0) offData = Integer.parseInt(strings[1]);
 				}
+				// parse the material and data for toggle on
 				String[] strings = split[0].split(":");
-				offMaterial = Integer.parseInt(strings[0]);
-				if (strings.length > 0) offData = Integer.parseInt(strings[1]);
-				// parse the data that gets set when the block is toggled on
-				strings = split[1].split(":");
 				onMaterial = Integer.parseInt(strings[0]);
 				if (strings.length > 0) onData = Integer.parseInt(strings[1]);
 			} catch (NumberFormatException e) {
