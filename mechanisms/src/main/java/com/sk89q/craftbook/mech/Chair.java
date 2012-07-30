14e72250714e02c5273cd7cd5aadeab221d1906a
me4502 <mnmiller1@hotmail.com>
2012-07-30 20:18:16 +1000

    Major bug fixes.
    Signed-off-by: me4502 <mnmiller1@hotmail.com>

--- mechanisms/src/main/java/com/sk89q/craftbook/mech/Chair.java
+++ mechanisms/src/main/java/com/sk89q/craftbook/mech/Chair.java
@@ -9,7 +9,7 @@ import net.minecraft.server.DataWatcher;
 import net.minecraft.server.Packet40EntityMetadata;
 import net.minecraft.server.WatchableObject;
 
-import org.bukkit.Chunk;
+import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.craftbukkit.entity.CraftPlayer;
 import org.bukkit.entity.Entity;
@@ -21,13 +21,11 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
-import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
 import org.bukkit.event.player.PlayerToggleSprintEvent;
 
 import com.sk89q.craftbook.bukkit.BukkitPlayer;
 import com.sk89q.craftbook.bukkit.MechanismsPlugin;
-import com.sk89q.craftbook.util.LocationUtil;
 
 public class Chair implements Listener {
 
@@ -49,11 +47,9 @@ public class Chair implements Listener {
 		    Player p = plugin.getServer().getPlayer(e.getKey());
 		    if(p!=null) {
 			Packet40EntityMetadata packet = new Packet40EntityMetadata(p.getEntityId(), new ChairWatcher((byte)0));
-			for(Chunk c : LocationUtil.getSurroundingChunks(event.getBlock(), plugin.getServer().getViewDistance() * 16)) {
-			    for(Entity ent : c.getEntities())
-				if(ent instanceof Player)
-				    ((CraftPlayer)ent).getHandle().netServerHandler.sendPacket(packet);
-			}
+			for(Entity ent : plugin.getServer().getOnlinePlayers())
+			    if(ent instanceof Player)
+				((CraftPlayer)ent).getHandle().netServerHandler.sendPacket(packet);
 			chairs.remove(p.getName());
 		    }
 		}
@@ -68,12 +64,6 @@ public class Chair implements Listener {
 
     @EventHandler
     public void onPlayerToggleSneak(PlayerToggleSneakEvent event) {
-	if(chairs.containsKey(event.getPlayer().getName()))
-	    event.setCancelled(true);
-    }
-
-    @EventHandler
-    public void onPlayerTeleport(PlayerTeleportEvent event) {
 	if(chairs.containsKey(event.getPlayer().getName()))
 	    event.setCancelled(true);
     }
@@ -105,11 +95,9 @@ public class Chair implements Listener {
 		    return;
 	    if(chairs.containsKey(player.getPlayer().getName())) { //Stand
 		Packet40EntityMetadata packet = new Packet40EntityMetadata(player.getPlayer().getEntityId(), new ChairWatcher((byte)0));
-		for(Chunk c : LocationUtil.getSurroundingChunks(event.getClickedBlock(), plugin.getServer().getViewDistance() * 16)) {
-		    for(Entity e : c.getEntities())
-			if(e instanceof Player)
-			    ((CraftPlayer)e).getHandle().netServerHandler.sendPacket(packet);
-		}
+		for(Entity e : plugin.getServer().getOnlinePlayers())
+		    if(e instanceof Player)
+			((CraftPlayer)e).getHandle().netServerHandler.sendPacket(packet);
 		chairs.remove(player.getPlayer().getName());
 	    }
 	    else { //Sit
@@ -117,11 +105,9 @@ public class Chair implements Listener {
 		    return;
 		player.getPlayer().teleport(event.getClickedBlock().getLocation().add(0.5, 0, 0.5)); //Teleport to the seat
 		Packet40EntityMetadata packet = new Packet40EntityMetadata(player.getPlayer().getEntityId(), new ChairWatcher((byte)4));
-		for(Chunk c : LocationUtil.getSurroundingChunks(event.getClickedBlock(), plugin.getServer().getViewDistance() * 16)) {
-		    for(Entity e : c.getEntities())
-			if(e instanceof Player)
-			    ((CraftPlayer)e).getHandle().netServerHandler.sendPacket(packet);
-		}
+		for(Entity e : plugin.getServer().getOnlinePlayers())
+		    if(e instanceof Player)
+			((CraftPlayer)e).getHandle().netServerHandler.sendPacket(packet);
 		chairs.put(player.getPlayer().getName(), event.getClickedBlock());
 	    }
 	}
@@ -129,9 +115,15 @@ public class Chair implements Listener {
 
     @EventHandler
     public void onMove(PlayerMoveEvent event) { //Stop players leaving there chair.
-	if(event.isCancelled()) return;
-	if(chairs.containsKey(event.getPlayer().getName()))
-	    event.getPlayer().teleport(chairs.get(event.getPlayer().getName()).getLocation());
+	if(chairs.containsKey(event.getPlayer().getName())) {
+	    if(chairs.get(event.getPlayer().getName()).getLocation().add(0.5, 0, 0.5).distance(event.getTo()) > 1.0D) {
+		Location loc = chairs.get(event.getPlayer().getName()).getLocation().add(0.5, 0, 0.5);
+		loc.setPitch(event.getPlayer().getLocation().getPitch());
+		loc.setYaw(event.getPlayer().getLocation().getYaw());
+		event.getPlayer().teleport(loc);
+		event.setCancelled(true);
+	    }
+	}
     }
 
     public static class ChairWatcher extends DataWatcher {
