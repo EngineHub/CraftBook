801eb22f848f1548d716d0a1091c0a7cd32bce28
me4502 <mnmiller1@hotmail.com>
2012-08-02 20:07:30 +1000

    Added Null checks
    Signed-off-by: me4502 <mnmiller1@hotmail.com>

--- mechanisms/src/main/java/com/sk89q/craftbook/mech/AIMechanic.java
+++ mechanisms/src/main/java/com/sk89q/craftbook/mech/AIMechanic.java
@@ -20,6 +20,7 @@ public class AIMechanic implements Listener {
     @EventHandler
     public void onEntityTarget(EntityTargetEvent event) {
 	if(!plugin.getLocalConfiguration().aiSettings.enabled) return;
+	if(event.getTarget() == null || event.getEntity() == null) return;
 	BaseAIMechanic ai = null;
 	if(event.getEntity() instanceof Zombie)
 	    ai = new ZombieAIMechanic(plugin);
