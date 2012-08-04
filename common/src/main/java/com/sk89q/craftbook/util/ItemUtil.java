f1f06c2c3a3636a221630ac99db00ffbca76ee56
me4502 <mnmiller1@hotmail.com>
2012-08-04 19:16:23 +1000

    Dispenser recipe tweaks. New ItemUtil thingy.
    Signed-off-by: me4502 <mnmiller1@hotmail.com>

--- common/src/main/java/com/sk89q/craftbook/util/ItemUtil.java
+++ common/src/main/java/com/sk89q/craftbook/util/ItemUtil.java
@@ -1,114 +1,129 @@
 package com.sk89q.craftbook.util;
 
-import com.sk89q.worldedit.blocks.BlockID;
-import com.sk89q.worldedit.blocks.ItemID;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.material.MaterialData;
+
+import com.sk89q.worldedit.blocks.BlockID;
+import com.sk89q.worldedit.blocks.ItemID;
 
 public class ItemUtil {
 
     public static boolean areItemsSimilar(ItemStack item, ItemStack item2) {
 
-        return item.getTypeId() == item2.getTypeId();
+	return item.getTypeId() == item2.getTypeId();
     }
 
     public static boolean areItemsIdentical(ItemStack item, ItemStack item2) {
 
-        if (item.getTypeId() == item2.getTypeId()) {
-            if (item.getData() == item2.getData()) {
-                return true;
-            }
-        }
-        return false;
+	if (item.getTypeId() == item2.getTypeId()) {
+	    if (item.getData() == item2.getData()) {
+		return true;
+	    }
+	}
+	return false;
     }
 
     public static boolean isItemSimilarTo(ItemStack item, int type) {
 
-        return item.getTypeId() == type;
+	return item.getTypeId() == type;
     }
 
     public static boolean isItemIdenticalTo(ItemStack item, int type, byte data) {
 
-        if (item.getTypeId() == type) {
-            if (item.getData().getData() == data) {
-                return true;
-            }
-        }
-        return false;
+	if (item.getTypeId() == type) {
+	    if (item.getData().getData() == data) {
+		return true;
+	    }
+	}
+	return false;
     }
 
     public static void setItemTypeAndData(ItemStack item, int type, byte data) {
 
-        item.setTypeId(type);
-        item.setData(new MaterialData(type, data));
+	item.setTypeId(type);
+	item.setData(new MaterialData(type, data));
     }
 
     public static boolean isStackValid(ItemStack item) {
 
-        return item != null && item.getAmount() > 0 && item.getTypeId() > 0;
+	return item != null && item.getAmount() > 0 && item.getTypeId() > 0;
     }
 
     public static boolean isCookable(ItemStack item) {
 
-        return getCookedResult(item) != null;
+	return getCookedResult(item) != null;
     }
 
     public static ItemStack getCookedResult(ItemStack item) {
 
-        switch (item.getTypeId()) {
-            case ItemID.RAW_BEEF:
-                return new ItemStack(ItemID.COOKED_BEEF);
-            case ItemID.RAW_CHICKEN:
-                return new ItemStack(ItemID.COOKED_CHICKEN);
-            case ItemID.RAW_FISH:
-                return new ItemStack(ItemID.COOKED_FISH);
-            case ItemID.RAW_PORKCHOP:
-                return new ItemStack(ItemID.COOKED_PORKCHOP);
-            default:
-                return null;
-        }
+	switch (item.getTypeId()) {
+	case ItemID.RAW_BEEF:
+	    return new ItemStack(ItemID.COOKED_BEEF);
+	case ItemID.RAW_CHICKEN:
+	    return new ItemStack(ItemID.COOKED_CHICKEN);
+	case ItemID.RAW_FISH:
+	    return new ItemStack(ItemID.COOKED_FISH);
+	case ItemID.RAW_PORKCHOP:
+	    return new ItemStack(ItemID.COOKED_PORKCHOP);
+	default:
+	    return null;
+	}
     }
 
     public static boolean isSmeltable(ItemStack item) {
 
-        return getSmeletedResult(item) != null;
+	return getSmeletedResult(item) != null;
     }
 
     public static ItemStack getSmeletedResult(ItemStack item) {
 
-        switch (item.getTypeId()) {
-            case BlockID.IRON_ORE:
-                return new ItemStack(ItemID.IRON_BAR);
-            case BlockID.GOLD_ORE:
-                return new ItemStack(ItemID.GOLD_BAR);
-            case BlockID.DIAMOND_ORE:
-                return new ItemStack(ItemID.DIAMOND);
-            case BlockID.SAND:
-                return new ItemStack(BlockID.GLASS);
-            case ItemID.CLAY_BALL:
-                return new ItemStack(ItemID.BRICK_BAR);
-            default:
-                return null;
-        }
+	switch (item.getTypeId()) {
+	case BlockID.IRON_ORE:
+	    return new ItemStack(ItemID.IRON_BAR);
+	case BlockID.GOLD_ORE:
+	    return new ItemStack(ItemID.GOLD_BAR);
+	case BlockID.DIAMOND_ORE:
+	    return new ItemStack(ItemID.DIAMOND);
+	case BlockID.SAND:
+	    return new ItemStack(BlockID.GLASS);
+	case ItemID.CLAY_BALL:
+	    return new ItemStack(ItemID.BRICK_BAR);
+	default:
+	    return null;
+	}
     }
 
     public static boolean containsRawFood(Inventory inv) {
 
-        for (ItemStack it : inv.getContents())
-            if (it != null && isCookable(it)) return true;
-        return false;
+	for (ItemStack it : inv.getContents())
+	    if (it != null && isCookable(it)) return true;
+	return false;
     }
 
     public static boolean containsRawMinerals(Inventory inv) {
 
-        for (ItemStack it : inv.getContents())
-            if (it != null && isSmeltable(it)) return true;
-        return false;
+	for (ItemStack it : inv.getContents())
+	    if (it != null && isSmeltable(it)) return true;
+	return false;
     }
 
     public static boolean isItemEdible(ItemStack item) {
 
-        return item.getType().isEdible();
+	return item.getType().isEdible();
+    }
+
+    public static ItemStack getUsedItem(ItemStack item) {
+	if (item.getTypeId() == ItemID.MUSHROOM_SOUP)
+	    item.setTypeId(ItemID.BOWL); //Get your bowl back
+	else if (item.getTypeId() == ItemID.POTION)
+	    item.setTypeId(ItemID.GLASS_BOTTLE); //Get your bottle back
+	else if (item.getTypeId() == ItemID.LAVA_BUCKET || item.getTypeId() == ItemID.WATER_BUCKET || item.getTypeId() == ItemID.MILK_BUCKET)
+	    item.setTypeId(ItemID.BUCKET); //Get your bucket back
+	else if (item.getAmount() == 1)
+	    item.setTypeId(0);
+	else
+	    item.setAmount(item.getAmount() - 1);
+	return item;
     }
 }
--- mechanisms/src/main/java/com/sk89q/craftbook/mech/dispenser/DispenserRecipes.java
+++ mechanisms/src/main/java/com/sk89q/craftbook/mech/dispenser/DispenserRecipes.java
@@ -1,6 +1,7 @@
 package com.sk89q.craftbook.mech.dispenser;
 
-import com.sk89q.craftbook.bukkit.MechanismsPlugin;
+import java.util.ArrayList;
+
 import org.bukkit.block.Dispenser;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -8,7 +9,8 @@ import org.bukkit.event.block.BlockDispenseEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.Vector;
 
-import java.util.ArrayList;
+import com.sk89q.craftbook.bukkit.MechanismsPlugin;
+import com.sk89q.craftbook.util.ItemUtil;
 
 /**
  * @author Me4502
@@ -21,71 +23,70 @@ public class DispenserRecipes implements Listener {
 
     public DispenserRecipes(MechanismsPlugin plugin) {
 
-        this.plugin = plugin;
-        recipes.add(new XPShooter());
-        recipes.add(new SnowShooter());
-        recipes.add(new FireArrows());
-        recipes.add(new Fan());
+	this.plugin = plugin;
+	addRecipe(new XPShooter());
+	addRecipe(new SnowShooter());
+	addRecipe(new FireArrows());
+	addRecipe(new Fan());
     }
 
     @EventHandler
     public void onBlockDispense(BlockDispenseEvent event) {
-
-        if (event.getBlock().getState() instanceof Dispenser && plugin.getLocalConfiguration().dispenserSettings
-                .enable) {
-            Dispenser dis = (Dispenser) event.getBlock().getState();
-            if (dispenseNew(dis, event.getItem(), event.getVelocity(), event)) {
-                event.setCancelled(true);
-            }
-        }
+	if (event.isCancelled()) return;
+	if (event.getBlock().getState() instanceof Dispenser && plugin.getLocalConfiguration().dispenserSettings.enable) {
+	    Dispenser dis = (Dispenser) event.getBlock().getState();
+	    if (dispenseNew(dis, event.getItem(), event.getVelocity(), event)) {
+		event.setCancelled(true);
+	    }
+	}
     }
 
-    public boolean dispenseNew(Dispenser dis, ItemStack item, Vector velocity, BlockDispenseEvent event) {
+    private boolean dispenseNew(Dispenser dis, ItemStack item, Vector velocity, BlockDispenseEvent event) {
 
-        if (dis == null || dis.getInventory() == null || dis.getInventory().getContents() == null) return false;
-        ItemStack[] stacks = dis.getInventory().getContents();
-        boolean toReturn = false;
-        try {
-            for (Recipe r : recipes) {
-                if (r == null) {
-                    recipes.remove(r); //Invalid recipe (save CPU cycles a little)
-                    continue;
-                }
-                current:
-                {
-                    if ((r.recipe[0] == 0 && stacks[0] == null) || (r.recipe[0] == stacks[0].getTypeId())) {
-                        for (int i = 1; i < stacks.length; i++) {
-                            if (!(!(r.recipe[i] != 0 && stacks[i] == null) && ((r.recipe[i] == 0 && stacks[i] ==
-                                    null) || (r.recipe[i] == stacks[i].getTypeId()))))
-                                break current; //This recipe is invalid.
-                        }
-                        toReturn = r.doAction(dis, item, velocity, event);
-                        for (int i = 1; i < stacks.length; i++) {
-                            if ((r.recipe[i] == 0 && stacks[i] == null) || (r.recipe[i] == stacks[i].getTypeId())) {
-                                if (stacks[i] == null || stacks[i].getTypeId() == 0 || r.recipe[i] == 0) {
+	if (dis == null || dis.getInventory() == null || dis.getInventory().getContents() == null) return false;
+	ItemStack[] stacks = dis.getInventory().getContents();
+	boolean toReturn = false;
+	try {
+	    for (Recipe r : recipes) {
+		if (r == null) {
+		    recipes.remove(r); //Invalid recipe (save CPU cycles a little)
+		    continue;
+		}
+		current:
+		{
+		    if (r.recipe[0] == 0 && stacks[0] == null || r.recipe[0] == stacks[0].getTypeId()) {
+			for (int i = 1; i < stacks.length; i++) {
+			    if (!(!(r.recipe[i] != 0 && stacks[i] == null) && (r.recipe[i] == 0 && stacks[i] ==
+				    null || r.recipe[i] == stacks[i].getTypeId())))
+				break current; //This recipe is wrong.
+			}
+			toReturn = r.doAction(dis, item, velocity, event);
+			for (int i = 1; i < stacks.length; i++) {
+			    if (r.recipe[i] == 0 && stacks[i] == null || r.recipe[i] == stacks[i].getTypeId()) {
+				if (stacks[i] == null || stacks[i].getTypeId() == 0 || r.recipe[i] == 0)
+				    continue;
+				stacks[i] = ItemUtil.getUsedItem(stacks[i]);
+			    } else
+				return true; //Cancel the event, as obviously something went wrong.
+			}
+			dis.getInventory().setContents(stacks);
+		    }
+		    break current;
+		}
+	    }
+	    return toReturn; //Leave it be.
+	} catch (Exception e) {
+	    return false;
+	}
+    }
 
-                                } else if (stacks[i].getTypeId() == 282)
-                                    stacks[i].setTypeId(281); //Get your bowl back
-                                else if (stacks[i].getTypeId() == 373)
-                                    stacks[i].setTypeId(374); //Get your bottle back
-                                else if (stacks[i].getTypeId() == 326 || stacks[i].getTypeId() == 327 || stacks[i]
-                                        .getTypeId() == 335)
-                                    stacks[i].setTypeId(325); //Get your bucket back
-                                else if (stacks[i].getAmount() == 1)
-                                    stacks[i].setTypeId(0);
-                                else
-                                    stacks[i].setAmount(stacks[i].getAmount() - 1);
-                            } else
-                                return true; //Cancel the event, as obviously something went wrong.
-                        }
-                        dis.getInventory().setContents(stacks);
-                    }
-                    break current;
-                }
-            }
-            return toReturn; //Leave it be.
-        } catch (Exception e) {
-            return false;
-        }
+    /**
+     * Adds a dispenser recipe.
+     * 
+     * @param recipe
+     */
+    public void addRecipe(Recipe recipe) {
+	if(recipes.contains(recipe)) return;
+	recipes.add(recipe);
     }
 }
