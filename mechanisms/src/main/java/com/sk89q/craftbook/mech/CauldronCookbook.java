6f6dc53e3350d5479e1ec54ec19f3b71cd0047b2
Silthus <m.silthus@gmail.com>
2012-08-08 14:35:57 +0200

    fixed cauldron dupe bug, reverted 3-bit random change since it should work

--- mechanisms/src/main/java/com/sk89q/craftbook/mech/CauldronCookbook.java
+++ mechanisms/src/main/java/com/sk89q/craftbook/mech/CauldronCookbook.java
@@ -29,7 +29,10 @@ import java.util.logging.Logger;
  * Store of recipes.
  *
  * @author sk89q
+ *
+ * @deprecated Use {@link com.sk89q.craftbook.mech.cauldron.ImprovedCauldronCookbook} instead
  */
+@Deprecated
 public class CauldronCookbook {
 
     /**
