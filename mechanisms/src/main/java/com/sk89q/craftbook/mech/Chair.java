22f1f2c9cabf7973e7d6e6c3b4b4135455a99dd4
me4502 <mnmiller1@hotmail.com>
2012-08-15 16:51:08 +1000

    Fix chair NPE
    Signed-off-by: me4502 <mnmiller1@hotmail.com>

--- mechanisms/src/main/java/com/sk89q/craftbook/mech/Chair.java
+++ mechanisms/src/main/java/com/sk89q/craftbook/mech/Chair.java
@@ -1,11 +1,14 @@
 package com.sk89q.craftbook.mech;
 
-import com.sk89q.craftbook.bukkit.BukkitPlayer;
-import com.sk89q.craftbook.bukkit.MechanismsPlugin;
-import com.sk89q.craftbook.util.LocationUtil;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import net.minecraft.server.DataWatcher;
 import net.minecraft.server.Packet40EntityMetadata;
 import net.minecraft.server.WatchableObject;
+
 import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.craftbukkit.entity.CraftPlayer;
@@ -13,12 +16,15 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.player.*;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerToggleSneakEvent;
+import org.bukkit.event.player.PlayerToggleSprintEvent;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Map.Entry;
+import com.sk89q.craftbook.bukkit.BukkitPlayer;
+import com.sk89q.craftbook.bukkit.MechanismsPlugin;
+import com.sk89q.craftbook.util.LocationUtil;
 
 
 /**
@@ -75,7 +81,7 @@ public class Chair implements Listener {
     public void onRightClick(PlayerInteractEvent event) {
 
         if (!plugin.getLocalConfiguration().chairSettings.enable) return;
-        if (!plugin.getLocalConfiguration().chairSettings.canUseBlock(event.getClickedBlock().getType()))
+        if (event.getClickedBlock() == null || !plugin.getLocalConfiguration().chairSettings.canUseBlock(event.getClickedBlock().getType()))
             return; //???
 
         BukkitPlayer player = new BukkitPlayer(plugin, event.getPlayer());
