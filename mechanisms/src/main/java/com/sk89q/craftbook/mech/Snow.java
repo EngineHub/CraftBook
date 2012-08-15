2f37f1d41c8427b59ce162acd1cd1b1647c6fc3c
me4502 <mnmiller1@hotmail.com>
2012-08-15 20:33:32 +1000

    Fix a snow trample bug
    Signed-off-by: me4502 <mnmiller1@hotmail.com>

--- mechanisms/src/main/java/com/sk89q/craftbook/mech/Snow.java
+++ mechanisms/src/main/java/com/sk89q/craftbook/mech/Snow.java
@@ -1,9 +1,8 @@
 package com.sk89q.craftbook.mech;
 
-import com.sk89q.craftbook.LocalPlayer;
-import com.sk89q.craftbook.bukkit.MechanismsPlugin;
-import com.sk89q.worldedit.blocks.BlockID;
-import com.sk89q.worldedit.blocks.ItemID;
+import java.util.Random;
+import java.util.logging.Level;
+
 import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
@@ -15,8 +14,10 @@ import org.bukkit.event.block.BlockFormEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 
-import java.util.Random;
-import java.util.logging.Level;
+import com.sk89q.craftbook.LocalPlayer;
+import com.sk89q.craftbook.bukkit.MechanismsPlugin;
+import com.sk89q.worldedit.blocks.BlockID;
+import com.sk89q.worldedit.blocks.ItemID;
 
 /**
  * Snow fall mechanism. Builds up/tramples snow
@@ -51,7 +52,7 @@ public class Snow implements Listener {
                     && event.getPlayer().getWorld().getBlockAt(event.getClickedBlock().getLocation().add(0, 1, 0))
                     .getTypeId() == 0) {
                 event.getPlayer().getWorld().getBlockAt(event.getClickedBlock().getLocation().add(0, 1, 0))
-                        .setTypeId(78);
+                .setTypeId(78);
                 incrementData(event.getPlayer().getWorld().getBlockAt(event.getClickedBlock().getLocation()
                         .add(0, 1, 0)));
             }
@@ -72,12 +73,12 @@ public class Snow implements Listener {
             return;
         if (random.nextInt(10) == 6) {
             Block b = event.getPlayer().getWorld().getBlockAt(event.getPlayer().getLocation());
-            if (b.getTypeId() == 78 || b.getTypeId() == 80) {
+            if (b.getTypeId() == 78) {
                 lowerData(b);
             }
 
             b = event.getPlayer().getWorld().getBlockAt(event.getPlayer().getLocation().subtract(0, 1, 0));
-            if (b.getTypeId() == 78 || b.getTypeId() == 80) {
+            if (b.getTypeId() == 78) {
                 lowerData(b);
             }
         }
