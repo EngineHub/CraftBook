5af095bf4482c09ecfb74c9613b8f4ee0b49a2cd
me4502 <mnmiller1@hotmail.com>
2012-09-01 09:47:53 +1000

    Item Dispenser can now dispense potions.
    Signed-off-by: me4502 <mnmiller1@hotmail.com>

--- circuits/src/main/java/com/sk89q/craftbook/bukkit/CircuitsPlugin.java
+++ circuits/src/main/java/com/sk89q/craftbook/bukkit/CircuitsPlugin.java
@@ -259,7 +259,6 @@ public class CircuitsPlugin extends BaseBukkitPlugin {
         registerIC("MC1270", new Melody.Factory(server), familySISO, familyAISO);
         registerIC("MC1271", new EntitySensor.Factory(server), familySISO, familyAISO);        // Restricted
         registerIC("MC1272", new PlayerDetection.Factory(server), familySISO, familyAISO);     // Restricted
-        //TODO Why was this here? registerIC("MC1299", new ParticleEffect.Factory(server), familySISO, familyAISO);      // Restricted
         registerIC("MC1420", new ClockDivider.Factory(server), familySISO, familyAISO);
         registerIC("MC1510", new MessageSender.Factory(server), familySISO, familyAISO);
         registerIC("MC2100", new Delayer.Factory(server), familySISO, familyAISO);
