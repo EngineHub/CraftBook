b30e8dd81fb07f114ab40f8257f46cc3e353481c
me4502 <mnmiller1@hotmail.com>
2012-09-09 17:04:47 +1000

    Possibly fixed Melody IC.
    Signed-off-by: me4502 <mnmiller1@hotmail.com>

--- circuits/src/main/java/com/sk89q/craftbook/gates/world/Melody.java
+++ circuits/src/main/java/com/sk89q/craftbook/gates/world/Melody.java
@@ -1,5 +1,16 @@
 package com.sk89q.craftbook.gates.world;
 
+import java.io.File;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.logging.Level;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Server;
+import org.bukkit.block.Sign;
+import org.bukkit.entity.Player;
+
 import com.sk89q.craftbook.bukkit.CircuitsPlugin;
 import com.sk89q.craftbook.ic.AbstractIC;
 import com.sk89q.craftbook.ic.AbstractICFactory;
@@ -7,16 +18,6 @@ import com.sk89q.craftbook.ic.ChipState;
 import com.sk89q.craftbook.ic.IC;
 import com.sk89q.jinglenote.JingleNoteComponent;
 import com.sk89q.jinglenote.MidiJingleSequencer;
-import org.bukkit.ChatColor;
-import org.bukkit.Server;
-import org.bukkit.block.Sign;
-import org.bukkit.entity.Player;
-
-import java.io.File;
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.io.Writer;
-import java.util.logging.Level;
 
 /**
  * @author Me4502
@@ -73,13 +74,13 @@ public class Melody extends AbstractIC {
                 File[] trialPaths = {
                         new File(CircuitsPlugin.getInst().getDataFolder(),
                                 "midi/" + midiName),
-                        new File(CircuitsPlugin.getInst().getDataFolder(),
-                                "midi/" + midiName + ".mid"),
-                        new File(CircuitsPlugin.getInst().getDataFolder(),
-                                "midi/" + midiName + ".midi"),
-                        new File("midi", midiName),
-                        new File("midi", midiName + ".mid"),
-                        new File("midi", midiName + ".midi"),};
+                                new File(CircuitsPlugin.getInst().getDataFolder(),
+                                        "midi/" + midiName + ".mid"),
+                                        new File(CircuitsPlugin.getInst().getDataFolder(),
+                                                "midi/" + midiName + ".midi"),
+                                                new File("midi", midiName),
+                                                new File("midi", midiName + ".mid"),
+                                                new File("midi", midiName + ".midi"),};
 
                 File file = null;
 
@@ -96,7 +97,7 @@ public class Melody extends AbstractIC {
                     return;
                 }
 
-                if (sequencer != null || jNote != null) {
+                if (sequencer != null && jNote != null) {
                     for (Player player : getServer().getOnlinePlayers()) {
                         if (getSign().getLine(3).equalsIgnoreCase("START")) break;
                         jNote.getJingleNoteManager().stop(player);
@@ -110,8 +111,7 @@ public class Melody extends AbstractIC {
                         continue;
                     if (radius > 0 && player.getLocation().distance(getSign().getLocation()) > radius) continue;
                     jNote.getJingleNoteManager().play(player, sequencer, 0);
-                    player.sendMessage(ChatColor.YELLOW + "Playing " + midiName
-                            + "...");
+                    player.sendMessage(ChatColor.YELLOW + "Playing " + midiName + "...");
                 }
             } else if (sequencer != null && chip.getInput(0) && !getSign().getLine(3).equalsIgnoreCase("START")) {
                 if (getSign().getLine(3).equalsIgnoreCase("START")) return;
