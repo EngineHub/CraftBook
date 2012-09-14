695001468c2d7936dde8ee83224cb208d2c2a60b
me4502 <mnmiller1@hotmail.com>
2012-09-14 21:03:53 +1000

    Neatening up painting switcher.
    Signed-off-by: me4502 <mnmiller1@hotmail.com>

--- mechanisms/src/main/java/com/sk89q/craftbook/mech/PaintingSwitch.java
+++ mechanisms/src/main/java/com/sk89q/craftbook/mech/PaintingSwitch.java
@@ -4,12 +4,14 @@ import java.util.HashMap;
 
 import org.bukkit.Art;
 import org.bukkit.entity.Painting;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerItemHeldEvent;
 
+import com.sk89q.craftbook.LocalPlayer;
 import com.sk89q.craftbook.bukkit.MechanismsPlugin;
 
 /**
@@ -26,37 +28,38 @@ public class PaintingSwitch implements Listener {
         this.plugin = plugin;
     }
 
+    public boolean isBeingEdited(Painting paint) {
+        if(paintings.get(paint) != null && players.get(paintings.get(paint)) != null) {
+            Player p = plugin.getServer().getPlayer(paintings.get(paint));
+            if(p == null || p.isDead()) return false;
+            return true;
+        }
+        return false;
+    }
+
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onPlayerInteractEntity(PlayerInteractEntityEvent event) {
 
         if (event.getRightClicked() instanceof Painting) {
+            LocalPlayer player = plugin.wrap(event.getPlayer());
             if (!plugin.getLocalConfiguration().paintingSettings.enabled) return;
             Painting paint = (Painting) event.getRightClicked();
             if(!plugin.canUseInArea(paint.getLocation(), event.getPlayer()))
                 return;
-            try {
-                if (event.getPlayer().hasPermission("craftbook.mech.paintingswitch.use")) {
-                    if (players.get(event.getPlayer().getName()) == null || paintings.get(paint) == null && !(players.get
-                            (event.getPlayer().getName()).getLocation().distance(paint.getLocation()) < 1) || plugin
-                            .getServer().getPlayer(paintings.get(paint)) == null) {
-                        paintings.put(paint, event.getPlayer().getName());
-                        players.put(event.getPlayer().getName(), paint);
-                        event.getPlayer().sendMessage("You are now editing the painting!");
-                        event.setCancelled(true);
-                    } else if (paintings.get(paint).equalsIgnoreCase(event.getPlayer().getName())) {
-                        paintings.remove(paint);
-                        players.remove(event.getPlayer().getName());
-                        event.getPlayer().sendMessage("You are no longer editing the painting!");
-                        event.setCancelled(true);
-                    } else if (paintings.get(paint) != null && plugin.getServer().getPlayer(paintings.get(paint)) != null) {
-                        event.getPlayer().sendMessage("The painting is already being edited by " + paintings.get(paint) +
-                                "!");
-                        event.setCancelled(true);
-                    }
-                }
-            }
-            catch(Exception e) {
-
+            if (player.hasPermission("craftbook.mech.paintingswitch.use")) {
+                if (!isBeingEdited(paint)) {
+                    paintings.put(paint, player.getName());
+                    players.put(player.getName(), paint);
+                    player.print("You are now editing the painting!");
+                } else if (paintings.get(paint).equalsIgnoreCase(player.getName())) {
+                    paintings.remove(paint);
+                    players.remove(player.getName());
+                    player.print("You are no longer editing the painting!");
+                } else if (isBeingEdited(paint))
+                    player.print("The painting is already being edited by " + paintings.get(paint) + "!");
+                else
+                    return;
+                event.setCancelled(true);
             }
         }
     }
@@ -65,8 +68,9 @@ public class PaintingSwitch implements Listener {
     public void onHeldItemChange(PlayerItemHeldEvent event) {
 
         if (!plugin.getLocalConfiguration().paintingSettings.enabled) return;
-        if (!event.getPlayer().hasPermission("craftbook.mech.paintingswitch.use")) return;
-        if (players.get(event.getPlayer().getName()) == null || players.get(event.getPlayer().getName()).isDead()) {
+        LocalPlayer player = plugin.wrap(event.getPlayer());
+        if (!player.hasPermission("craftbook.mech.paintingswitch.use")) return;
+        if (players.get(player.getName()) == null || players.get(event.getPlayer().getName()).isDead()) {
             try {
                 paintings.remove(players.get(event.getPlayer().getName()));
                 players.remove(event.getPlayer().getName());
@@ -78,8 +82,8 @@ public class PaintingSwitch implements Listener {
         if (event.getNewSlot() > event.getPreviousSlot()) isForwards = true;
         else if (event.getNewSlot() < event.getPreviousSlot()) isForwards = false;
         else return;
-        if (event.getPreviousSlot() < 1 && event.getNewSlot() > 7) isForwards = false;
-        else if (event.getPreviousSlot() > 7 && event.getNewSlot() < 1) isForwards = true;
+        if (event.getPreviousSlot() == 0 && event.getNewSlot() == 8) isForwards = false;
+        else if (event.getPreviousSlot() == 8 && event.getNewSlot() == 0) isForwards = true;
         Art[] art = Art.values();
         Painting paint = players.get(event.getPlayer().getName());
         int newID = paint.getArt().getId() + (isForwards ? -1 : 1);
