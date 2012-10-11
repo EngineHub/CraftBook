48e24abd25bdf564370e59f7b41ff5048d5c360e
me4502 <mnmiller1@hotmail.com>
2012-10-11 18:01:22 +1000

    Fixed missing files :| Need a more permanent fix as git seems to be messing up.
    Signed-off-by: me4502 <mnmiller1@hotmail.com>

---
+++ circuits/src/main/java/com/sk89q/craftbook/gates/world/PumpST.java
@@ -0,0 +1,51 @@
+package com.sk89q.craftbook.gates.world;
+
+import org.bukkit.Server;
+import org.bukkit.block.Sign;
+
+import com.sk89q.craftbook.ic.ChipState;
+import com.sk89q.craftbook.ic.IC;
+import com.sk89q.craftbook.ic.ICFactory;
+import com.sk89q.craftbook.ic.SelfTriggeredIC;
+
+public class PumpST extends Pump implements SelfTriggeredIC {
+
+    public PumpST(Server server, Sign block, ICFactory factory) {
+        super(server, block, factory);
+    }
+
+    @Override
+    public String getTitle() {
+        return "Self-Triggered Pump";
+    }
+
+    @Override
+    public String getSignTitle() {
+        return "PUMP ST";
+    }
+
+    @Override
+    public boolean isActive() {
+        return true;
+    }
+
+    @Override
+    public void think(ChipState state) {
+        if(state.getInput(0))
+            state.setOutput(0, scan());
+    }
+
+    public static class Factory extends Pump.Factory {
+
+        public Factory(Server server) {
+
+            super(server);
+        }
+
+        @Override
+        public IC create(Sign sign) {
+
+            return new PumpST(getServer(), sign, this);
+        }
+    }
+}
