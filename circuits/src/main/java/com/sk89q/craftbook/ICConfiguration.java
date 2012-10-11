1eab9c01ec4645a2f504950f3719ebe5357aae23
me4502 <mnmiller1@hotmail.com>
2012-10-11 17:22:57 +1000

    Fixed missing files.
    Signed-off-by: me4502 <mnmiller1@hotmail.com>

---
+++ circuits/src/main/java/com/sk89q/craftbook/ICConfiguration.java
@@ -0,0 +1,32 @@
+package com.sk89q.craftbook;
+
+import java.io.File;
+
+import org.bukkit.configuration.file.FileConfiguration;
+
+import com.sk89q.craftbook.bukkit.CircuitsPlugin;
+import com.sk89q.craftbook.ic.RegisteredICFactory;
+
+public class ICConfiguration extends BaseConfiguration {
+
+    /**
+     * Initialized IC Config. Run after IC's are registered.
+     * 
+     * @param cfg
+     * @param dataFolder
+     */
+    public ICConfiguration(FileConfiguration cfg, File dataFolder) {
+        super(cfg, dataFolder,false);
+
+        for(RegisteredICFactory factory : CircuitsPlugin.getInst().icManager.registered.values()) {
+            if(factory.getId().startsWith("MCA")) continue;
+            try {
+                factory.getFactory().addConfiguration(cfg.getConfigurationSection(factory.getId()));
+            }
+            catch(NullPointerException npe) {
+                cfg.createSection(factory.getId());
+                factory.getFactory().addConfiguration(cfg.getConfigurationSection(factory.getId()));
+            }
+        }
+    }
+}
