78dea75f30134b86292c71bc53de629e6489da4c
me4502 <mnmiller1@hotmail.com>
2012-11-03 10:28:33 +1000

    Fixed all the non-working IC's. (@Silthus broke them)
    Signed-off-by: me4502 <mnmiller1@hotmail.com>

--- circuits/src/main/java/com/sk89q/craftbook/ic/AbstractChipState.java
+++ circuits/src/main/java/com/sk89q/craftbook/ic/AbstractChipState.java
@@ -1,12 +1,13 @@
 package com.sk89q.craftbook.ic;
 
-import com.sk89q.craftbook.util.SignUtil;
-import com.sk89q.worldedit.BlockWorldVector;
-import com.sk89q.worldedit.bukkit.BukkitUtil;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.Sign;
 import org.bukkit.material.Diode;
+
+import com.sk89q.craftbook.util.SignUtil;
+import com.sk89q.worldedit.BlockWorldVector;
+import com.sk89q.worldedit.bukkit.BukkitUtil;
 
 /**
  * @author Silthus
@@ -15,36 +16,34 @@ public abstract class AbstractChipState implements ChipState {
 
     protected final Sign sign;
     protected final BlockWorldVector source;
-	protected final boolean selfTriggered;
-	protected final Block icBlock;
+    protected final boolean selfTriggered;
+    protected final Block icBlock;
 
-	protected AbstractChipState(BlockWorldVector source, Sign sign, boolean selfTriggered) {
+    protected AbstractChipState(BlockWorldVector source, Sign sign, boolean selfTriggered) {
 
-		this.sign = sign;
-		this.source = source;
-		this.selfTriggered = selfTriggered;
-		this.icBlock = SignUtil.getBackBlock(sign.getBlock());
-	}
+        this.sign = sign;
+        this.source = source;
+        this.selfTriggered = selfTriggered;
+        icBlock = SignUtil.getBackBlock(sign.getBlock());
+    }
 
-	protected abstract Block getBlock(int pin);
+    protected abstract Block getBlock(int pin);
 
-	@Override
-	public boolean get(int pin) {
+    @Override
+    public boolean get(int pin) {
 
-		Block block = getBlock(pin);
-		return block != null
-				&& (selfTriggered || isTriggered(pin))
-				&& block.isBlockIndirectlyPowered();
-	}
+        Block block = getBlock(pin);
+        return block != null && block.isBlockIndirectlyPowered();
+    }
 
-	@Override
-	public void set(int pin, boolean value) {
+    @Override
+    public void set(int pin, boolean value) {
 
-		Block block = getBlock(pin);
-		if (block != null) {
-			ICUtil.setState(block, value, icBlock);
-		}
-	}
+        Block block = getBlock(pin);
+        if (block != null) {
+            ICUtil.setState(block, value, icBlock);
+        }
+    }
 
     @Override
     public boolean isTriggered(int pin) {
