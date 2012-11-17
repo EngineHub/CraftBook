1cc17b90dd6271727e7940b8f3595ec0f370496a
me4502 <mnmiller1@hotmail.com>
2012-11-17 20:04:23 +1000

    Added Sorter and SorterST IC's
    Will put to left or right depending if the item is in the chest or not.
    
    Signed-off-by: me4502 <mnmiller1@hotmail.com>

---
+++ circuits/src/main/java/com/sk89q/craftbook/gates/world/items/Sorter.java
@@ -0,0 +1,133 @@
+package com.sk89q.craftbook.gates.world.items;
+
+import org.bukkit.Location;
+import org.bukkit.Server;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.block.Chest;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Item;
+import org.bukkit.inventory.ItemStack;
+
+import com.sk89q.craftbook.ChangedSign;
+import com.sk89q.craftbook.bukkit.BukkitUtil;
+import com.sk89q.craftbook.ic.AbstractIC;
+import com.sk89q.craftbook.ic.AbstractICFactory;
+import com.sk89q.craftbook.ic.ChipState;
+import com.sk89q.craftbook.ic.IC;
+import com.sk89q.craftbook.ic.ICFactory;
+import com.sk89q.craftbook.util.ItemUtil;
+import com.sk89q.craftbook.util.SignUtil;
+import com.sk89q.worldedit.blocks.BlockID;
+
+public class Sorter extends AbstractIC {
+
+    public Sorter (Server server, ChangedSign sign, ICFactory factory) {
+        super(server, sign, factory);
+        load();
+    }
+
+    Block chestBlock;
+    boolean inverted = false;
+
+    public void load() {
+        try {
+            chestBlock = SignUtil.getBackBlock(BukkitUtil.toSign(getSign()).getBlock()).getRelative(0,1,0);
+
+            inverted = getSign().getLine(2).equalsIgnoreCase("invert");
+        }
+        catch(Exception e){}
+    }
+
+    @Override
+    public String getTitle () {
+        return "Sorter";
+    }
+
+    @Override
+    public String getSignTitle () {
+        return "SORTER";
+    }
+
+    @Override
+    public void trigger (ChipState chip) {
+        if(chip.getInput(0))
+            chip.setOutput(0, sort());
+    }
+
+    public boolean sort() {
+
+        boolean returnValue = false;
+
+        for (Entity en : BukkitUtil.toSign(getSign()).getChunk().getEntities()) {
+            if (!(en instanceof Item)) {
+                continue;
+            }
+            Item item = (Item) en;
+            ItemStack stack = item.getItemStack();
+            if (!ItemUtil.isStackValid(stack) || item.isDead() || !item.isValid()) {
+                continue;
+            }
+            Location location = item.getLocation();
+            int ix = location.getBlockX();
+            int iy = location.getBlockY();
+            int iz = location.getBlockZ();
+            if (ix == getSign().getX() && iy == getSign().getY() && iz == getSign().getZ()) {
+
+                BlockFace back = SignUtil.getBack(BukkitUtil.toSign(getSign()).getBlock());
+                Block b;
+
+                if(isInAboveChest(stack) || inverted) {
+                    b = SignUtil.getRightBlock(BukkitUtil.toSign(getSign()).getBlock()).getRelative(back);
+                }
+                else {
+                    b = SignUtil.getLeftBlock(BukkitUtil.toSign(getSign()).getBlock()).getRelative(back);
+                }
+
+                item.teleport(b.getLocation().add(0.5, 0.5, 0.5));
+
+                returnValue = true;
+            }
+        }
+        return returnValue;
+    }
+
+    public boolean isInAboveChest(ItemStack item) {
+
+        if(chestBlock.getTypeId() == BlockID.CHEST) {
+            Chest chest = (Chest) chestBlock.getState();
+            return chest.getInventory().contains(new ItemStack(item.getTypeId(), 1, item.getDurability(), item.getData().getData()));
+        }
+        return false;
+    }
+
+    public static class Factory extends AbstractICFactory {
+
+        public Factory(Server server) {
+
+            super(server);
+        }
+
+        @Override
+        public IC create(ChangedSign sign) {
+
+            return new Sorter(getServer(), sign, this);
+        }
+
+        @Override
+        public String getDescription() {
+
+            return "Sorts items and spits out left/right depending on above chest.";
+        }
+
+        @Override
+        public String[] getLineHelp() {
+
+            String[] lines = new String[] {
+                    "invert - to invert output sides",
+                    null
+            };
+            return lines;
+        }
+    }
+}
---
+++ circuits/src/main/java/com/sk89q/craftbook/gates/world/items/SorterST.java
@@ -0,0 +1,50 @@
+package com.sk89q.craftbook.gates.world.items;
+
+import org.bukkit.Server;
+
+import com.sk89q.craftbook.ChangedSign;
+import com.sk89q.craftbook.ic.ChipState;
+import com.sk89q.craftbook.ic.IC;
+import com.sk89q.craftbook.ic.ICFactory;
+import com.sk89q.craftbook.ic.SelfTriggeredIC;
+
+public class SorterST extends Sorter implements SelfTriggeredIC {
+
+    public SorterST (Server server, ChangedSign sign, ICFactory factory) {
+        super(server, sign, factory);
+    }
+
+    @Override
+    public String getTitle () {
+        return "Self-Triggered Sorter";
+    }
+
+    @Override
+    public String getSignTitle () {
+        return "SORTER ST";
+    }
+
+    @Override
+    public boolean isActive () {
+        return true;
+    }
+
+    @Override
+    public void think (ChipState state) {
+        state.setOutput(0, sort());
+    }
+
+    public static class Factory extends Sorter.Factory {
+
+        public Factory(Server server) {
+
+            super(server);
+        }
+
+        @Override
+        public IC create(ChangedSign sign) {
+
+            return new SorterST(getServer(), sign, this);
+        }
+    }
+}
