7c51a6312523c38381b40c1bdaadfc0cfd72831b
me4502 <mnmiller1@hotmail.com>
2012-11-18 16:20:04 +1000

    Revert last commit.
    Signed-off-by: me4502 <mnmiller1@hotmail.com>

--- mechanisms/src/main/java/com/sk89q/craftbook/mech/CustomDropManager.java
+++ mechanisms/src/main/java/com/sk89q/craftbook/mech/CustomDropManager.java
@@ -18,6 +18,11 @@
 
 package com.sk89q.craftbook.mech;
 
+import com.sk89q.craftbook.bukkit.BaseBukkitPlugin;
+import com.sk89q.craftbook.bukkit.MechanismsPlugin;
+import org.bukkit.Bukkit;
+import org.bukkit.inventory.ItemStack;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -26,12 +31,6 @@ import java.util.Map;
 import java.util.TreeMap;
 import java.util.logging.Level;
 import java.util.regex.Pattern;
-
-import org.bukkit.Bukkit;
-import org.bukkit.inventory.ItemStack;
-
-import com.sk89q.craftbook.bukkit.BaseBukkitPlugin;
-import com.sk89q.craftbook.bukkit.MechanismsPlugin;
 
 /**
  * Storage class for custom drop definitions.
@@ -110,7 +109,7 @@ public final class CustomDropManager {
                         prelude = "Error on line " + currentLine + " of drop definition file " +
                                 file.getAbsolutePath() + ": " + line + "\n"; //Error prelude used for parse messages.
 
-                        line = COMMENT_PATTERN.split(line)[1]; //Remove comments
+                        line = COMMENT_PATTERN.split(line)[0]; //Remove comments
                         line = line.trim(); //Remove excess whitespace
 
                         if (line.isEmpty()) {
