289e42cb58ec544ac86cb2e94664f842af038ea0
me4502 <mnmiller1@hotmail.com>
2012-12-06 11:22:16 +1000

    Added region support to PlayerDetection IC, and also optimized it.
    Signed-off-by: me4502 <mnmiller1@hotmail.com>

--- circuits/src/main/java/com/sk89q/craftbook/gates/world/sensors/PlayerDetection.java
+++ circuits/src/main/java/com/sk89q/craftbook/gates/world/sensors/PlayerDetection.java
@@ -1,5 +1,6 @@
 package com.sk89q.craftbook.gates.world.sensors;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Server;
 import org.bukkit.entity.Player;
@@ -14,7 +15,9 @@ import com.sk89q.craftbook.ic.IC;
 import com.sk89q.craftbook.ic.ICFactory;
 import com.sk89q.craftbook.ic.ICUtil;
 import com.sk89q.craftbook.ic.RestrictedIC;
+import com.sk89q.craftbook.util.GeneralUtil;
 import com.sk89q.craftbook.util.LocationUtil;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 
 /**
  * @author Me4502
@@ -47,40 +50,111 @@ public class PlayerDetection extends AbstractIC {
         }
     }
 
-    protected boolean isDetected() {
+    int radius;
 
-        int radius = 10; //Default Radius
-        Location location = BukkitUtil.toSign(getSign()).getLocation();
+    int x,y,z;
+
+    Location location = null;
+    ProtectedRegion reg = null;
+
+    @Override
+    public void load() {
+        radius = 10; //Default Radius
+        location = BukkitUtil.toSign(getSign()).getLocation();
         try {
-            String[] splitEquals = ICUtil.EQUALS_PATTERN.split(getSign().getLine(2), 2);
-            radius = Integer.parseInt(splitEquals[0]);
-            if (getSign().getLine(2).contains("=")) {
-                String[] splitCoords = ICUtil.COLON_PATTERN.split(splitEquals[1]);
-                int x = Integer.parseInt(splitCoords[0]);
-                int y = Integer.parseInt(splitCoords[1]);
-                int z = Integer.parseInt(splitCoords[2]);
-                location.add(x, y, z);
+            if(getLine(2).startsWith("r:") && CircuitsPlugin.getInst().getWorldGuard() != null) {
+
+                String region = getLine(2).replace("r:", "");
+                reg = CircuitsPlugin.getInst().getWorldGuard().getRegionManager(BukkitUtil.toSign(getSign()).getWorld()).getRegion(region);
+                if(reg == null) {
+                    String[] splitEquals = ICUtil.EQUALS_PATTERN.split(getSign().getLine(2), 2);
+                    radius = Integer.parseInt(splitEquals[0]);
+                    if (getSign().getLine(2).contains("=")) {
+                        String[] splitCoords = ICUtil.COLON_PATTERN.split(splitEquals[1]);
+                        x = Integer.parseInt(splitCoords[0]);
+                        y = Integer.parseInt(splitCoords[1]);
+                        z = Integer.parseInt(splitCoords[2]);
+                        location.add(x, y, z);
+                    }
+                }
             }
+            else {
+                String[] splitEquals = ICUtil.EQUALS_PATTERN.split(getSign().getLine(2), 2);
+                radius = Integer.parseInt(splitEquals[0]);
+                if (getSign().getLine(2).contains("=")) {
+                    String[] splitCoords = ICUtil.COLON_PATTERN.split(splitEquals[1]);
+                    x = Integer.parseInt(splitCoords[0]);
+                    y = Integer.parseInt(splitCoords[1]);
+                    z = Integer.parseInt(splitCoords[2]);
+                    location.add(x, y, z);
+                }
+            }
+        } catch (NullPointerException e) {
+            Bukkit.getLogger().severe(GeneralUtil.getStackTrace(e));
         } catch (Exception ignored) {
         }
+    }
 
+    protected boolean isDetected() {
+
+        Type type = Type.PLAYER;
+        if(getLine(3).contains(":")) {
+            type = Type.getFromChar(getLine(3).toCharArray()[0]);
+        }
         String nameLine = getSign().getLine(3).replace("g:", "").replace("p:", "");
 
-        for (Player e : getServer().getOnlinePlayers()) {
-            if (e == null || !e.isValid()
-                    || !LocationUtil.isWithinRadius(location, e.getLocation(), radius)) {
-                continue;
-            }
+        if(reg != null) {
 
-            if (nameLine.isEmpty()) {
-                return true;
-            } else if (e.getName().toLowerCase().startsWith(nameLine.toLowerCase())
-                    || CircuitsPlugin.getInst().isInGroup(e.getName(), nameLine)) {
-                return true;
+            for(Player p : BukkitUtil.toSign(getSign()).getWorld().getPlayers()) {
+                if(reg.contains(p.getLocation().getBlockX(),p.getLocation().getBlockY(),p.getLocation().getBlockZ())) {
+                    return true;
+                }
+            }
+        }
+        else if(location != null) {
+            if(type == Type.PLAYER) {
+                Player p = Bukkit.getPlayer(nameLine);
+                if (p != null && LocationUtil.isWithinRadius(location, p.getLocation(), radius))
+                    return true;
+            }
+            for (Player e : getServer().getOnlinePlayers()) {
+                if (e == null || !e.isValid()
+                        || !LocationUtil.isWithinRadius(location, e.getLocation(), radius)) {
+                    continue;
+                }
+
+                if (nameLine.isEmpty()) {
+                    return true;
+                } else if (type == Type.PLAYER && e.getName().toLowerCase().startsWith(nameLine.toLowerCase())) {
+                    return true;
+                } else if (type == Type.GROUP && CircuitsPlugin.getInst().isInGroup(e.getName(), nameLine)) {
+                    return true;
+                }
             }
         }
 
         return false;
+    }
+
+    private enum Type {
+
+        PLAYER('p'),
+        GROUP('g');
+
+        private Type(char prefix) {
+
+            this.prefix = prefix;
+        }
+
+        char prefix;
+
+        public static Type getFromChar(char c) {
+            c = Character.toLowerCase(c);
+            for(Type t : values())
+                if(t.prefix == c)
+                    return t;
+            return PLAYER;
+        }
     }
 
     public static class Factory extends AbstractICFactory implements RestrictedIC {
