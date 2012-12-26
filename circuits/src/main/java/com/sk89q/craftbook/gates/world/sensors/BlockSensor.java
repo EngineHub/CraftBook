7ce487850de2cea4714253e1cd1eb9a0a378c27b
me4502 <mnmiller1@hotmail.com>
2012-12-26 11:02:54 +1000

    Revert "I now have a hatred for BlockSensor. I will never again use it."
    This reverts commit 9803dc730da9c87e7fbcdd99fe53cd986269fbe9.

--- circuits/src/main/java/com/sk89q/craftbook/gates/world/sensors/BlockSensor.java
+++ circuits/src/main/java/com/sk89q/craftbook/gates/world/sensors/BlockSensor.java
@@ -5,6 +5,7 @@ import java.util.regex.Pattern;
 import org.bukkit.Server;
 import org.bukkit.block.Block;
 
+import com.sk89q.craftbook.BaseConfiguration;
 import com.sk89q.craftbook.ChangedSign;
 import com.sk89q.craftbook.ic.AbstractIC;
 import com.sk89q.craftbook.ic.AbstractICFactory;
@@ -55,12 +56,12 @@ public class BlockSensor extends AbstractIC {
     public void trigger (ChipState chip) {
 
         if (chip.getInput(0)) {
-            chip.setOutput(0, !hasBlock());
+            chip.setOutput(0, ((Factory)getFactory()).invert ? !hasBlock() : hasBlock());
         }
     }
 
     /**
-     * Returns true if the sign has the specified block at the specified location.
+     * Returns true if the sign has water at the specified location.
      * 
      * @return
      */
@@ -69,7 +70,8 @@ public class BlockSensor extends AbstractIC {
         int blockID = center.getTypeId();
 
         if (data != (byte) -1)
-            if (blockID == id) return data == center.getData();
+            if (blockID == id) return
+                    data == center.getData();
         return blockID == id;
     }
 
@@ -111,6 +113,17 @@ public class BlockSensor extends AbstractIC {
 
             String[] lines = new String[] { "x:y:z", "id:data" };
             return lines;
+        }
+
+        @Override
+        public void addConfiguration (BaseConfiguration.BaseConfigurationSection section) {
+
+            invert = section.getBoolean("invert-output", true);
+        }
+
+        @Override
+        public boolean needsConfiguration () {
+            return true;
         }
     }
 }
