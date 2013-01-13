ca307538c3284106e77527ec345866b1562a2daf
Tobias Schulz <tobias@freehal.net>
2013-01-13 18:07:03 +0100

    Code cleanup...

--- src/main/java/com/sk89q/craftbook/mech/ProtectBlockListener.java
+++ src/main/java/com/sk89q/craftbook/mech/ProtectBlockListener.java
@@ -1,89 +1,121 @@
 package com.sk89q.craftbook.mech;
 
-import com.sk89q.craftbook.MechanicManager;
-import com.sk89q.craftbook.bukkit.CraftBookPlugin;
-import com.sk89q.craftbook.SourcedBlockRedstoneEvent;
-import com.sk89q.worldedit.*;
-import com.sk89q.worldedit.regions.*;
-import com.sk89q.worldedit.blocks.*;
-import com.sk89q.worldedit.bukkit.*;
-import org.bukkit.World;
-import org.bukkit.block.Block;
-import org.bukkit.block.BlockFace;
-import org.bukkit.event.*;
-import org.bukkit.event.block.*;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.event.world.ChunkLoadEvent;
-import org.bukkit.event.world.ChunkUnloadEvent;
-import org.bukkit.material.Diode;
+import java.util.HashMap;
+import java.util.Map;
 
-import java.util.*;
+import org.bukkit.block.Block;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+
+import com.sk89q.craftbook.bukkit.CraftBookPlugin;
+import com.sk89q.worldedit.WorldVector;
+import com.sk89q.worldedit.bukkit.BukkitUtil;
+import com.sk89q.worldedit.regions.CuboidRegion;
 
 public class ProtectBlockListener implements Listener {
 
-        protected static final Map<String, CuboidRegion> regions = new HashMap<String, CuboidRegion>();
-        protected static final Map<String, WorldVector> blocks = new HashMap<String, WorldVector>();
+	protected static final Map<String, CuboidRegion> regions = new HashMap<String, CuboidRegion>();
+	protected static final Map<String, WorldVector> blocks = new HashMap<String, WorldVector>();
 	private static ProtectBlockListener singleton = null;
 
-        public static void addCuboidRegion(CuboidRegion region) {
+	/**
+	 * Construct the listener.
+	 */
+	public ProtectBlockListener() {
+		CraftBookPlugin.registerEvents(this);
+	}
+
+	/**
+	 * Make sure there is an instance of the listener.
+	 */
+	public static void initialize() {
+		if (singleton == null) {
+			singleton = new ProtectBlockListener();
+		}
+	}
+
+	/**
+	 * Protect a cuboid region.
+	 * 
+	 * @param region
+	 *        the region
+	 */
+	public static void addCuboidRegion(CuboidRegion region) {
 		if (!regions.containsKey(region.toString()))
 			System.out.println("ProtectBlockListener: add: " + region);
 
 		regions.put(region.toString(), region);
-		if (singleton == null) {
-			singleton = new ProtectBlockListener();
-		}
-        }
+	}
 
-        public static void removeCuboidRegion(CuboidRegion region) {
+	/**
+	 * Don't protect a cuboid region any more.
+	 * 
+	 * @param region
+	 *        the region
+	 */
+	public static void removeCuboidRegion(CuboidRegion region) {
 		System.out.println("ProtectBlockListener: remove: " + region);
 		regions.remove(region.toString());
-        }
+	}
 
+	/**
+	 * Protect a block.
+	 * 
+	 * @param vector
+	 *        the vector
+	 */
 	public static void addBlock(WorldVector vector) {
 		if (!blocks.containsKey(vector.toString()))
 			System.out.println("ProtectBlockListener: add: " + vector);
 		blocks.put(vector.toString(), vector);
 	}
 
+	/**
+	 * Don't protect a block any more.
+	 * 
+	 * @param vector
+	 *        the vector
+	 */
 	public static void removeBlock(WorldVector vector) {
 		System.out.println("ProtectBlockListener: remove: " + vector);
 		blocks.remove(vector.toString());
 	}
 
-        /**
-         * Construct the listener.
-         */
-        public ProtectBlockListener() {
-		CraftBookPlugin.registerEvents(this);
-        }
-
-        @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-        public void onBlockPlace(BlockPlaceEvent event) {
-		if (isProtected(event.getBlock(), event)) {
+	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+	public void onBlockPlace(BlockPlaceEvent event) {
+		if (isProtected(event.getBlock())) {
+			event.setCancelled(true);
 			event.getPlayer().sendMessage("Protected area. Cannot place blocks here.");
 		}
-        }
+	}
 
-        @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-        public void onBlockBreak(BlockBreakEvent event) {
-		if (isProtected(event.getBlock(), event)) {
+	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+	public void onBlockBreak(BlockBreakEvent event) {
+		if (isProtected(event.getBlock())) {
+			event.setCancelled(true);
 			event.getPlayer().sendMessage("Protected area. Cannot break blocks here.");
 		}
 	}
 
-	public boolean isProtected(Block b, Cancellable event) {
+	/**
+	 * Check whether a block is protected.
+	 * 
+	 * @param b
+	 *        the block
+	 * @return {@code true} if it is protected, {@code false} otherwise.
+	 */
+	private boolean isProtected(Block b) {
 		if (blocks.containsKey(BukkitUtil.toWorldVector(b).toString())) {
-			event.setCancelled(true);
 			return true;
 		}
 		for (CuboidRegion region : regions.values()) {
 			if (region.contains(BukkitUtil.toVector(b))) {
-				event.setCancelled(true);
 				return true;
 			}
 		}
 		return false;
-        }
+	}
 }
-
