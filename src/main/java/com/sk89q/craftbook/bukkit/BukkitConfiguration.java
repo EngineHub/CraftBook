6e2d61833f6fc2c5de9cab532f8e58097b246c61
Matthew <Matthew@Matthew-Desktop>
2013-02-20 11:53:07 +1000

    Disable check-worldguard-flags by default.

--- src/main/java/com/sk89q/craftbook/bukkit/BukkitConfiguration.java
+++ src/main/java/com/sk89q/craftbook/bukkit/BukkitConfiguration.java
@@ -88,7 +88,7 @@ public class BukkitConfiguration extends YAMLConfiguration {
         useBlockDistance = config.getBoolean("use-block-distance", false);
 
         config.setComment("check-worldguard-flags", "Checks to see if WorldGuard allows building/using in the area when activating mechanics.");
-        obeyWorldguard = config.getBoolean("check-worldguard-flags", true);
+        obeyWorldguard = config.getBoolean("check-worldguard-flags", false);
 
         config.setComment("advanced-block-checks", "Use advanced methods to detect if a player can build or not. Use this if you use region protections other than WorldGuard, or experience issues with WorldGuard protection.");
         advancedBlockChecks = config.getBoolean("advanced-block-checks", false);
