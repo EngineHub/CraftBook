afefb4a4cc9905210a6586a6d8e31776f56d3ab7
me4502 <mnmiller1@hotmail.com>
2013-03-03 08:25:41 +1000

    Line ending test

--- src/main/java/com/sk89q/craftbook/LocalComponent.java
+++ src/main/java/com/sk89q/craftbook/LocalComponent.java
@@ -6,7 +6,13 @@ package com.sk89q.craftbook;
  */
 public interface LocalComponent {
 
+    /**
+     * Called to enable the component.
+     */
     public void enable();
 
+    /**
+     * Called to unload and disable the component.
+     */
     public void disable();
 }
