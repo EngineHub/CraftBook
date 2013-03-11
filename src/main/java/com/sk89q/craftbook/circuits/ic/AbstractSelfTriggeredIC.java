4c097be655465d58186d155649db1931bf230229
me4502 <mnmiller1@hotmail.com>
2013-03-11 18:11:00 +1000

    Derp. Thats why it was failing.

---
+++ src/main/java/com/sk89q/craftbook/circuits/ic/AbstractSelfTriggeredIC.java
@@ -0,0 +1,18 @@
+package com.sk89q.craftbook.circuits.ic;
+
+import org.bukkit.Server;
+
+import com.sk89q.craftbook.ChangedSign;
+
+public abstract class AbstractSelfTriggeredIC extends AbstractIC implements SelfTriggeredIC {
+
+    public AbstractSelfTriggeredIC (Server server, ChangedSign sign, ICFactory factory) {
+        super(server, sign, factory);
+    }
+
+    @Override
+    public boolean isActive() {
+
+        return true;
+    }
+}
