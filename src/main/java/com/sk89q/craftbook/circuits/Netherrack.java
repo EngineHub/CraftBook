40024b621f08c75c9b5edc1fdaf4dcfad46313d6
me4502 <mnmiller1@hotmail.com>
2013-03-24 13:13:58 +1000

    Small change to RedstoneNetherrack

--- src/main/java/com/sk89q/craftbook/circuits/Netherrack.java
+++ src/main/java/com/sk89q/craftbook/circuits/Netherrack.java
@@ -7,7 +7,7 @@
  * Software Foundation, either version 3 of the License, or (at your option) any later version.
  * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
-  * warranty of MERCHANTABILITY or
+ * warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License along with this program. If not,
@@ -70,9 +70,9 @@ public class Netherrack extends AbstractMechanic {
         Block above = event.getBlock().getRelative(0, 1, 0);
 
         if (event.getNewCurrent() > 0 && canReplaceWithFire(above.getTypeId())) {
-            above.setTypeId(BlockID.FIRE, false);
+            above.setTypeId(BlockID.FIRE);
         } else if (event.getNewCurrent() < 1 && above != null && above.getTypeId() == BlockID.FIRE) {
-            above.setTypeId(BlockID.AIR, false);
+            above.setTypeId(BlockID.AIR);
         }
     }
 
