18a568802a7ae9367a370045a5129a5cd76c1008
Dark_Arc <theexcel@rocketmail.com>
2013-04-20 21:11:04 -0400

    Tweak the Driller

--- src/main/java/com/sk89q/craftbook/circuits/gates/world/blocks/Driller.java
+++ src/main/java/com/sk89q/craftbook/circuits/gates/world/blocks/Driller.java
@@ -3,6 +3,7 @@ package com.sk89q.craftbook.circuits.gates.world.blocks;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.sk89q.worldedit.blocks.BlockType;
 import org.bukkit.Server;
 import org.bukkit.block.Block;
 import org.bukkit.inventory.InventoryHolder;
@@ -28,8 +29,7 @@ public class Driller extends AbstractSelfTriggeredIC {
     @Override
     public void think (ChipState chip) {
 
-        if(!chip.getInput(0))
-            chip.setOutput(0, drill());
+        if (!chip.getInput(0)) chip.setOutput(0, drill());
     }
 
     @Override
@@ -46,51 +46,48 @@ public class Driller extends AbstractSelfTriggeredIC {
 
     public boolean drill() {
 
-        if(CraftBookPlugin.inst().getRandom().nextInt(100) < 60)
-            return false;
+        if (CraftBookPlugin.inst().getRandom().nextInt(100) < 60) return false;
 
         Block center = getBackBlock().getRelative(0, -1, 0);
         InventoryHolder holder = null;
 
-        if(center.getRelative(0, 2, 0).getState() instanceof InventoryHolder) {
+        if (center.getRelative(0, 2, 0).getState() instanceof InventoryHolder) {
             holder = (InventoryHolder) center.getRelative(0, 2, 0).getState();
         }
 
-        boolean hasHadTrue = false;
+        boolean hasHadTrue;
 
-        int num = CraftBookPlugin.inst().getRandom().nextInt(9);
-
-        switch(num) {
+        switch (CraftBookPlugin.inst().getRandom().nextInt(9)) {
             case 0:
-                if(drillLine(holder, center.getRelative(-1, 0, -1)))
-                    hasHadTrue = true;
+                hasHadTrue = drillLine(holder, center.getRelative(-1, 0, -1));
+                break;
             case 1:
-                if(drillLine(holder, center.getRelative(-1, 0, 0)))
-                    hasHadTrue = true;
+                hasHadTrue = drillLine(holder, center.getRelative(-1, 0, 0));
+                break;
             case 2:
-                if(drillLine(holder, center.getRelative(-1, 0, 1)))
-                    hasHadTrue = true;
+                hasHadTrue = drillLine(holder, center.getRelative(-1, 0, 1));
+                break;
             case 3:
-                if(drillLine(holder, center.getRelative(0, 0, -1)))
-                    hasHadTrue = true;
+                hasHadTrue = drillLine(holder, center.getRelative(0, 0, -1));
+                break;
             case 4:
-                if(drillLine(holder, center.getRelative(0, 0, 0)))
-                    hasHadTrue = true;
+                hasHadTrue = drillLine(holder, center.getRelative(0, 0, 0));
+                break;
             case 5:
-                if(drillLine(holder, center.getRelative(0, 0, 1)))
-                    hasHadTrue = true;
+                hasHadTrue = drillLine(holder, center.getRelative(0, 0, 1));
+                break;
             case 6:
-                if(drillLine(holder, center.getRelative(1, 0, -1)))
-                    hasHadTrue = true;
+                hasHadTrue = drillLine(holder, center.getRelative(1, 0, -1));
+                break;
             case 7:
-                if(drillLine(holder, center.getRelative(1, 0, 0)))
-                    hasHadTrue = true;
+                hasHadTrue = drillLine(holder, center.getRelative(1, 0, 0));
+                break;
             case 8:
-                if(drillLine(holder, center.getRelative(1, 0, 1)))
-                    hasHadTrue = true;
+                hasHadTrue = drillLine(holder, center.getRelative(1, 0, 1));
+                break;
             default:
-                if(drillLine(holder, center.getRelative(0, 0, 0)))
-                    hasHadTrue = true;
+                hasHadTrue = drillLine(holder, center.getRelative(0, 0, 0));
+                break;
         }
 
         return hasHadTrue;
@@ -100,13 +97,11 @@ public class Driller extends AbstractSelfTriggeredIC {
 
         boolean hasChest = chest != null;
 
-        while(blockToBreak.getTypeId() == 0) {
+        while (blockToBreak.getTypeId() == 0) {
 
-            if(blockToBreak.getLocation().getBlockY() == 0)
-                return false;
+            if (blockToBreak.getLocation().getBlockY() == 0) return false;
             blockToBreak = blockToBreak.getRelative(0, -1, 0);
-            if(blockToBreak.getTypeId() == BlockID.BEDROCK)
-                return false;
+            if (blockToBreak.getTypeId() == BlockID.BEDROCK || !BlockType.isNaturalTerrainBlock(blockToBreak.getTypeId())) return false;
         }
 
         int brokenType = 0;
@@ -116,34 +111,33 @@ public class Driller extends AbstractSelfTriggeredIC {
             drops = new ArrayList<ItemStack>(blockToBreak.getDrops(chest.getInventory().getItem(0)));
         }
 
-        for(ItemStack stack : drops) {
+        for (ItemStack stack : drops) {
 
             List<ItemStack> toDrop = new ArrayList<ItemStack>();
             toDrop.add(stack);
 
-            if(hasChest) {
+            if (hasChest) {
                 toDrop = new ArrayList<ItemStack>(chest.getInventory().addItem(toDrop.toArray(new ItemStack[1])).values());
             }
 
-            if(!toDrop.isEmpty())
-                for(ItemStack d : toDrop)
+            if (!toDrop.isEmpty()) {
+                for (ItemStack d : toDrop) {
                     BukkitUtil.toSign(getSign()).getBlock().getWorld().dropItemNaturally(BukkitUtil.toSign(getSign()).getBlock().getLocation().add(0.5, 0.5, 0.5), d);
+                }
+            }
         }
 
         brokenType = blockToBreak.getTypeId();
         blockToBreak.setTypeId(0);
 
-        if(brokenType == BlockID.LAVA || brokenType == BlockID.WATER || brokenType == BlockID.STATIONARY_LAVA || brokenType == BlockID.STATIONARY_WATER)
-            return false;
+        return !(brokenType == BlockID.LAVA || brokenType == BlockID.WATER || brokenType == BlockID.STATIONARY_LAVA || brokenType == BlockID.STATIONARY_WATER);
 
-        return true;
     }
 
     @Override
     public void trigger (ChipState chip) {
 
-        if(chip.getInput(0))
-            chip.setOutput(0, drill());
+        if (chip.getInput(0)) chip.setOutput(0, drill());
     }
 
     public static class Factory extends AbstractICFactory implements RestrictedIC {
@@ -168,8 +162,7 @@ public class Driller extends AbstractSelfTriggeredIC {
         @Override
         public String[] getLineHelp() {
 
-            String[] lines = new String[] {null, null};
-            return lines;
+            return new String[] {null, null};
         }
     }
 }
