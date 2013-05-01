218fb1e3819c33e6502abbc8be59bbf7c28206f7
me4502 <mnmiller1@hotmail.com>
2013-05-01 19:25:07 +1000

    Updated to 1.5.2

--- src/main/java/com/sk89q/craftbook/mech/BetterPhysics.java
+++ src/main/java/com/sk89q/craftbook/mech/BetterPhysics.java
@@ -1,7 +1,6 @@
 package com.sk89q.craftbook.mech;
 
 import org.bukkit.Bukkit;
-import org.bukkit.Sound;
 import org.bukkit.block.Block;
 import org.bukkit.entity.FallingBlock;
 import org.bukkit.event.EventHandler;
@@ -9,19 +8,11 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPhysicsEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.event.entity.EntityChangeBlockEvent;
 
 import com.sk89q.craftbook.bukkit.CraftBookPlugin;
 import com.sk89q.worldedit.blocks.BlockID;
 
 public class BetterPhysics implements Listener {
-
-    @EventHandler(ignoreCancelled = true, priority = EventPriority.LOWEST)
-    public void onBlockCreatedEntity(EntityChangeBlockEvent event) {
-
-        if(event.getBlock().getTypeId() == BlockID.FLOWER_POT && CraftBookPlugin.inst().getConfiguration().physicsPots)
-            Bukkit.getScheduler().runTaskLater(CraftBookPlugin.inst(), new ShatteringPots(event.getBlock(), true), 1L);
-    }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.LOWEST)
     public void onBlockPlace(BlockPlaceEvent event) {
@@ -31,9 +22,6 @@ public class BetterPhysics implements Listener {
 
         if(event.getBlock().getTypeId() == BlockID.LADDER && CraftBookPlugin.inst().getConfiguration().physicsLadders)
             Bukkit.getScheduler().runTaskLater(CraftBookPlugin.inst(), new FallingLadders(event.getBlock()), 1L);
-
-        if(event.getBlock().getTypeId() == BlockID.FLOWER_POT && CraftBookPlugin.inst().getConfiguration().physicsPots)
-            Bukkit.getScheduler().runTaskLater(CraftBookPlugin.inst(), new ShatteringPots(event.getBlock(), false), 1L);
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.LOWEST)
@@ -44,9 +32,6 @@ public class BetterPhysics implements Listener {
 
         if(event.getBlock().getTypeId() == BlockID.LADDER && CraftBookPlugin.inst().getConfiguration().physicsLadders)
             Bukkit.getScheduler().runTaskLater(CraftBookPlugin.inst(), new FallingLadders(event.getBlock()), 1L);
-
-        if(event.getBlock().getTypeId() == BlockID.FLOWER_POT && CraftBookPlugin.inst().getConfiguration().physicsPots)
-            Bukkit.getScheduler().runTaskLater(CraftBookPlugin.inst(), new ShatteringPots(event.getBlock(), false), 1L);
     }
 
     public class FallingLadders implements Runnable {
@@ -63,31 +48,6 @@ public class BetterPhysics implements Listener {
             if(ladder.getRelative(0, -1, 0).getTypeId() != 0)
                 return;
             ladder.getWorld().spawn(ladder.getLocation(), FallingBlock.class);
-        }
-    }
-
-    public class ShatteringPots implements Runnable {
-
-        Block pot;
-        boolean shatter;
-
-        public ShatteringPots(Block pot, boolean shatter) {
-
-            this.pot = pot;
-            this.shatter = shatter;
-        }
-
-        @Override
-        public void run () {
-            if(shatter) {
-                pot.setTypeId(0);
-                pot.getWorld().playSound(pot.getLocation(), Sound.GLASS, 1.0f, 1.0f);
-            } else {
-                if(pot.getRelative(0, -1, 0).getTypeId() != 0)
-                    return;
-                FallingBlock b = pot.getWorld().spawn(pot.getLocation(), FallingBlock.class);
-                b.setDropItem(false);
-            }
         }
     }
 }
--- src/main/java/com/sk89q/craftbook/util/config/YAMLConfiguration.java
+++ src/main/java/com/sk89q/craftbook/util/config/YAMLConfiguration.java
@@ -82,8 +82,6 @@ public class YAMLConfiguration extends LocalConfiguration {
         // Better Physics Configuration Listener
         physicsEnabled = config.getBoolean("mechanics.better-physics.enable", false);
         physicsLadders = config.getBoolean("mechanics.better-physics.falling-ladders", false);
-        //physicsPots = config.getBoolean("mechanics.better-physics.smashing-pots", false);
-        physicsPots = false; //TODO enable when 1.5 comes out (It fixes the client crash caused by this)
 
         // Better Pistons Configuration Listener
         pistonsEnabled = config.getBoolean("mechanics.better-pistons.enable", true);
