d1cb2ac5ddacf35892ddd494e1472993064980ce
me4502 <mnmiller1@hotmail.com>
2013-05-11 14:18:57 +1000

    Added boolean and numerical commands to /cb var
    They can toggle boolean values and manipulate numbers.

--- src/main/java/com/sk89q/craftbook/bukkit/commands/VariableCommands.java
+++ src/main/java/com/sk89q/craftbook/bukkit/commands/VariableCommands.java
@@ -96,4 +96,175 @@ public class VariableCommands {
             if(CircuitCore.inst().getIcManager() != null) //Make sure IC's are enabled.
                 ICManager.emptyCache();
     }
+
+    @Command(aliases = "append", desc = "Append to a variable.", max=2, min=2)
+    @CommandPermissions("craftbook.variables.append")
+    public void append(CommandContext context, CommandSender sender) {
+
+        if(plugin.variableStore.containsKey(context.getString(0))) {
+
+            if(!RegexUtil.VARIABLE_PATTERN.matcher(context.getString(0)).find()) {
+                sender.sendMessage("Invalid Variable Name!");
+                return;
+            }
+            if(!RegexUtil.VARIABLE_PATTERN.matcher(context.getString(1)).find()) {
+                sender.sendMessage("Invalid Variable Value!");
+                return;
+            }
+            plugin.variableStore.put(context.getString(0), plugin.variableStore.get(context.getString(0)) + context.getString(1));
+            resetICCache();
+        } else
+            sender.sendMessage("Unknown Variable!");
+    }
+
+    @Command(aliases = "toggle", desc = "Toggle a boolean.", max=1, min=1)
+    @CommandPermissions("craftbook.variables.toggle")
+    public void toggle(CommandContext context, CommandSender sender) {
+
+        if(plugin.variableStore.containsKey(context.getString(0))) {
+
+            if(!RegexUtil.VARIABLE_PATTERN.matcher(context.getString(0)).find()) {
+                sender.sendMessage("Invalid Variable Name!");
+                return;
+            }
+            if(!RegexUtil.VARIABLE_PATTERN.matcher(context.getString(1)).find()) {
+                sender.sendMessage("Invalid Variable Value!");
+                return;
+            }
+            String var = plugin.variableStore.get(context.getString(0));
+            if(var.equalsIgnoreCase("0") || var.equalsIgnoreCase("1"))
+                var = var.equalsIgnoreCase("1") ? "0" : "1";
+            else if(var.equalsIgnoreCase("true") || var.equalsIgnoreCase("false"))
+                var = var.equalsIgnoreCase("true") ? "false" : "true";
+            else if(var.equalsIgnoreCase("yes") || var.equalsIgnoreCase("no"))
+                var = var.equalsIgnoreCase("yes") ? "no" : "yes";
+            else {
+                sender.sendMessage("Variable not of boolean type!");
+                return;
+            }
+            plugin.variableStore.put(context.getString(0), var);
+            resetICCache();
+        } else
+            sender.sendMessage("Unknown Variable!");
+    }
+
+    @Command(aliases = "add", desc = "Add to a numeric variable.", max=2, min=2)
+    @CommandPermissions("craftbook.variables.add")
+    public void add(CommandContext context, CommandSender sender) {
+
+        if(plugin.variableStore.containsKey(context.getString(0))) {
+
+            if(!RegexUtil.VARIABLE_PATTERN.matcher(context.getString(0)).find()) {
+                sender.sendMessage("Invalid Variable Name!");
+                return;
+            }
+            if(!RegexUtil.VARIABLE_PATTERN.matcher(context.getString(1)).find()) {
+                sender.sendMessage("Invalid Variable Value!");
+                return;
+            }
+            String var = plugin.variableStore.get(context.getString(0));
+            try {
+
+                double f = Double.parseDouble(var);
+                f += context.getDouble(1);
+                var = String.valueOf(f);
+            } catch(Exception e) {
+                sender.sendMessage("Variable not of numeric type!");
+                return;
+            }
+            plugin.variableStore.put(context.getString(0), var);
+            resetICCache();
+        } else
+            sender.sendMessage("Unknown Variable!");
+    }
+
+    @Command(aliases = "subtract", desc = "Subtract from a numeric variable.", max=2, min=2)
+    @CommandPermissions("craftbook.variables.subtract")
+    public void subtract(CommandContext context, CommandSender sender) {
+
+        if(plugin.variableStore.containsKey(context.getString(0))) {
+
+            if(!RegexUtil.VARIABLE_PATTERN.matcher(context.getString(0)).find()) {
+                sender.sendMessage("Invalid Variable Name!");
+                return;
+            }
+            if(!RegexUtil.VARIABLE_PATTERN.matcher(context.getString(1)).find()) {
+                sender.sendMessage("Invalid Variable Value!");
+                return;
+            }
+            String var = plugin.variableStore.get(context.getString(0));
+            try {
+
+                double f = Double.parseDouble(var);
+                f -= context.getDouble(1);
+                var = String.valueOf(f);
+            } catch(Exception e) {
+                sender.sendMessage("Variable not of numeric type!");
+                return;
+            }
+            plugin.variableStore.put(context.getString(0), var);
+            resetICCache();
+        } else
+            sender.sendMessage("Unknown Variable!");
+    }
+
+    @Command(aliases = "multiple", desc = "Multiply a numeric variable.", max=2, min=2)
+    @CommandPermissions("craftbook.variables.multiple")
+    public void multiple(CommandContext context, CommandSender sender) {
+
+        if(plugin.variableStore.containsKey(context.getString(0))) {
+
+            if(!RegexUtil.VARIABLE_PATTERN.matcher(context.getString(0)).find()) {
+                sender.sendMessage("Invalid Variable Name!");
+                return;
+            }
+            if(!RegexUtil.VARIABLE_PATTERN.matcher(context.getString(1)).find()) {
+                sender.sendMessage("Invalid Variable Value!");
+                return;
+            }
+            String var = plugin.variableStore.get(context.getString(0));
+            try {
+
+                double f = Double.parseDouble(var);
+                f *= context.getDouble(1);
+                var = String.valueOf(f);
+            } catch(Exception e) {
+                sender.sendMessage("Variable not of numeric type!");
+                return;
+            }
+            plugin.variableStore.put(context.getString(0), var);
+            resetICCache();
+        } else
+            sender.sendMessage("Unknown Variable!");
+    }
+
+    @Command(aliases = "divide", desc = "Divide a numeric variable.", max=2, min=2)
+    @CommandPermissions("craftbook.variables.divide")
+    public void divide(CommandContext context, CommandSender sender) {
+
+        if(plugin.variableStore.containsKey(context.getString(0))) {
+
+            if(!RegexUtil.VARIABLE_PATTERN.matcher(context.getString(0)).find()) {
+                sender.sendMessage("Invalid Variable Name!");
+                return;
+            }
+            if(!RegexUtil.VARIABLE_PATTERN.matcher(context.getString(1)).find()) {
+                sender.sendMessage("Invalid Variable Value!");
+                return;
+            }
+            String var = plugin.variableStore.get(context.getString(0));
+            try {
+
+                double f = Double.parseDouble(var);
+                f /= context.getDouble(1);
+                var = String.valueOf(f);
+            } catch(Exception e) {
+                sender.sendMessage("Variable not of numeric type!");
+                return;
+            }
+            plugin.variableStore.put(context.getString(0), var);
+            resetICCache();
+        } else
+            sender.sendMessage("Unknown Variable!");
+    }
 }
