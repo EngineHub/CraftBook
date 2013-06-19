926429fb9321fe83d28512e9a003ac6e381bd621
me4502 <mnmiller1@hotmail.com>
2013-06-19 19:36:48 +1000

    Removed the variable parsing from ItemUtil. It will cause issues with saving CommandsItems and CustomCrafting.

--- src/main/java/com/sk89q/craftbook/util/ItemUtil.java
+++ src/main/java/com/sk89q/craftbook/util/ItemUtil.java
@@ -466,8 +466,6 @@ public class ItemUtil {
         if (line == null || line.isEmpty())
             return null;
 
-        line = ParsingUtil.parseLine(line, null);
-
         int id = 0;
         int data = -1;
         int amount = 1;
