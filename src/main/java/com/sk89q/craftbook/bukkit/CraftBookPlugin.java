30982ca342df8ad100a0f6603b90296afaa6daf0
me4502 <mnmiller1@hotmail.com>
2013-08-02 21:03:13 +1000

    Add CraftBook 3.7 Changelog.

--- src/main/java/com/sk89q/craftbook/bukkit/CraftBookPlugin.java
+++ src/main/java/com/sk89q/craftbook/bukkit/CraftBookPlugin.java
@@ -166,7 +166,7 @@ public class CraftBookPlugin extends JavaPlugin {
 
     public static String getVersion() {
 
-        return "3.7b6";
+        return "3.7";
     }
 
     /**
@@ -176,7 +176,7 @@ public class CraftBookPlugin extends JavaPlugin {
      */
     public static String getStableBuild() {
 
-        return "2501";
+        return "2513";
     }
 
     /**
