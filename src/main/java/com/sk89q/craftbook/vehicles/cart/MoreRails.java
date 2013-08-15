46dfa78916a239d8e7a64efb78d8a603f94ca0b4
me4502 <mnmiller1@hotmail.com>
2013-08-15 18:28:37 +1000

    Fix a vehicles error.

--- src/main/java/com/sk89q/craftbook/vehicles/cart/MoreRails.java
+++ src/main/java/com/sk89q/craftbook/vehicles/cart/MoreRails.java
@@ -1,14 +1,17 @@
 package com.sk89q.craftbook.vehicles.cart;
 
+import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Minecart;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.vehicle.VehicleMoveEvent;
 import org.bukkit.material.Attachable;
+import org.bukkit.material.Vine;
 import org.bukkit.util.Vector;
 
 import com.sk89q.craftbook.bukkit.CraftBookPlugin;
+import com.sk89q.craftbook.util.LocationUtil;
 import com.sk89q.worldedit.blocks.BlockID;
 
 public class MoreRails implements Listener {
@@ -23,7 +26,19 @@ public class MoreRails implements Listener {
                 event.getVehicle().setVelocity(event.getVehicle().getVelocity().normalize().multiply(4));
 
         if (CraftBookPlugin.inst().getConfiguration().minecartMoreRailsLadder)
-            if (event.getTo().getBlock().getTypeId() == BlockID.LADDER || event.getTo().getBlock().getTypeId() == BlockID.VINE)
-                event.getVehicle().setVelocity(event.getVehicle().getVelocity().add(new Vector(((Attachable) event.getTo().getBlock().getState().getData()).getAttachedFace().getModX(),CraftBookPlugin.inst().getConfiguration().minecartMoreRailsLadderVelocity,((Attachable) event.getTo().getBlock().getState().getData()).getAttachedFace().getModY())));
+            if (event.getTo().getBlock().getTypeId() == BlockID.LADDER)
+                event.getVehicle().setVelocity(event.getVehicle().getVelocity().add(new Vector(((Attachable) event.getTo().getBlock().getState().getData()).getAttachedFace().getModX(),CraftBookPlugin.inst().getConfiguration().minecartMoreRailsLadderVelocity,((Attachable) event.getTo().getBlock().getState().getData()).getAttachedFace().getModZ())));
+            else if (event.getTo().getBlock().getTypeId() == BlockID.VINE) {
+                BlockFace movementFace = BlockFace.SELF;
+                Vine vine = (Vine) event.getTo().getBlock().getState().getData();
+                for(BlockFace test : LocationUtil.getDirectFaces())
+                    if(vine.isOnFace(test)) {
+                        movementFace = test;
+                        break;
+                    }
+                if(movementFace == BlockFace.SELF)
+                    return;
+                event.getVehicle().setVelocity(event.getVehicle().getVelocity().add(new Vector(movementFace.getModX(),CraftBookPlugin.inst().getConfiguration().minecartMoreRailsLadderVelocity,movementFace.getModZ())));
+            }
     }
 }
