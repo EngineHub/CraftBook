d377cae6b6aa5e6877827776c5d8026cbeff3014
me4502 <mnmiller1@hotmail.com>
2013-09-08 20:44:32 +1000

    Damage tools for TreeLopper.

--- src/main/java/com/sk89q/craftbook/mech/TreeLopper.java
+++ src/main/java/com/sk89q/craftbook/mech/TreeLopper.java
@@ -2,18 +2,19 @@ package com.sk89q.craftbook.mech;
 
 import java.util.HashSet;
 
-import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
 
 import com.sk89q.craftbook.AbstractMechanic;
 import com.sk89q.craftbook.AbstractMechanicFactory;
 import com.sk89q.craftbook.LocalPlayer;
 import com.sk89q.craftbook.bukkit.CraftBookPlugin;
 import com.sk89q.craftbook.bukkit.MechanicListenerAdapter;
+import com.sk89q.craftbook.util.ItemUtil;
 import com.sk89q.craftbook.util.LocationUtil;
 import com.sk89q.worldedit.BlockWorldVector;
 import com.sk89q.worldedit.bukkit.BukkitUtil;
@@ -45,27 +46,50 @@ public class TreeLopper extends AbstractMechanic {
 
         for(BlockFace face : plugin.getConfiguration().treeLopperAllowDiagonals ? LocationUtil.getIndirectFaces() : LocationUtil.getDirectFaces()) {
             if(block.getRelative(face).getTypeId() == blockId && (!plugin.getConfiguration().treeLopperEnforceData || block.getRelative(face).getData() == blockData))
-                searchBlock(event, block.getRelative(face));
+                if(searchBlock(event, block.getRelative(face))) {
+                    ItemStack heldItem = event.getPlayer().getItemInHand();
+                    if(heldItem != null && ItemUtil.getMaxDurability(heldItem.getTypeId()) > 0) {
+                        heldItem.setDurability((short) (heldItem.getDurability() + 1));
+                        if(heldItem.getDurability() < ItemUtil.getMaxDurability(heldItem.getTypeId()))
+                            event.getPlayer().setItemInHand(heldItem);
+                        else
+                            event.getPlayer().setItemInHand(null);
+                    }
+                }
         }
     }
 
-    public void searchBlock(BlockBreakEvent event, Block block) {
+    public boolean searchBlock(BlockBreakEvent event, Block block) {
 
+        LocalPlayer player = CraftBookPlugin.inst().wrapPlayer(event.getPlayer());
         if(visitedLocations.contains(block.getLocation()))
-            return;
+            return false;
         if(broken > plugin.getConfiguration().treeLopperMaxSize)
-            return;
+            return false;
+        if(!CraftBookPlugin.inst().getConfiguration().treeLopperItems.contains(player.getHeldItemType()))
+            return false;
         if(!plugin.canBuild(event.getPlayer(), block, false)) {
-            event.getPlayer().sendMessage(ChatColor.RED + "You do not have permission to build in this area!");
-            return;
+            player.printError("area.break-permissions");
+            return false;
         }
         block.breakNaturally(event.getPlayer().getItemInHand());
         visitedLocations.add(block.getLocation());
         broken += 1;
         for(BlockFace face : plugin.getConfiguration().treeLopperAllowDiagonals ? LocationUtil.getIndirectFaces() : LocationUtil.getDirectFaces()) {
             if(block.getRelative(face).getTypeId() == blockId && (!plugin.getConfiguration().treeLopperEnforceData || block.getRelative(face).getData() == blockData))
-                searchBlock(event, block.getRelative(face));
+                if(searchBlock(event, block.getRelative(face))) {
+                    ItemStack heldItem = event.getPlayer().getItemInHand();
+                    if(heldItem != null && ItemUtil.getMaxDurability(heldItem.getTypeId()) > 0) {
+                        heldItem.setDurability((short) (heldItem.getDurability() + 1));
+                        if(heldItem.getDurability() < ItemUtil.getMaxDurability(heldItem.getTypeId()))
+                            event.getPlayer().setItemInHand(heldItem);
+                        else
+                            event.getPlayer().setItemInHand(null);
+                    }
+                }
         }
+
+        return true;
     }
 
     public static class Factory extends AbstractMechanicFactory<TreeLopper> {
