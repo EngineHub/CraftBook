fadf42cb791299c4cebf0b0bc270a5cd2e05541c
me4502 <mnmiller1@hotmail.com>
2013-11-30 08:43:54 +1000

    Fixed colour messages.

--- src/main/java/com/sk89q/craftbook/bukkit/BukkitPlayer.java
+++ src/main/java/com/sk89q/craftbook/bukkit/BukkitPlayer.java
@@ -48,13 +48,13 @@ public class BukkitPlayer implements LocalPlayer {
     @Override
     public void print(String message) {
 
-        printRaw(ChatColor.GOLD + ChatColor.translateAlternateColorCodes('&', plugin.getLanguageManager().getString(message, plugin.getLanguageManager().getPlayersLanguage(player))));
+        player.sendMessage(ChatColor.GOLD + ChatColor.translateAlternateColorCodes('&', plugin.getLanguageManager().getString(message, plugin.getLanguageManager().getPlayersLanguage(player))));
     }
 
     @Override
     public void printError(String message) {
 
-        printRaw(ChatColor.RED + ChatColor.translateAlternateColorCodes('&', plugin.getLanguageManager().getString(message, plugin.getLanguageManager().getPlayersLanguage(player))));
+        player.sendMessage(ChatColor.RED + ChatColor.translateAlternateColorCodes('&', plugin.getLanguageManager().getString(message, plugin.getLanguageManager().getPlayersLanguage(player))));
     }
 
     @Override
