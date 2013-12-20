0ad5f4f73b783ab973819d941ef1df44217962e5
me4502 <mnmiller1@hotmail.com>
2013-12-20 17:11:21 +1000

    Fixed XPStorer error.

--- src/main/java/com/sk89q/craftbook/mech/XPStorer.java
+++ src/main/java/com/sk89q/craftbook/mech/XPStorer.java
@@ -2,6 +2,7 @@ package com.sk89q.craftbook.mech;
 
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
@@ -14,6 +15,7 @@ public class XPStorer extends AbstractCraftBookMechanic {
     @EventHandler(ignoreCancelled = true)
     public void onRightClick(PlayerInteractEvent event) {
 
+        if(event.getAction() != Action.RIGHT_CLICK_BLOCK) return;
         if(!CraftBookPlugin.inst().getConfiguration().xpStorerBlock.isSame(event.getClickedBlock())) return;
 
         LocalPlayer player = CraftBookPlugin.inst().wrapPlayer(event.getPlayer());
