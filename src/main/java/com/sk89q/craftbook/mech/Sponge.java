6f6e44a7a25c5d45c0a93a1345ea5fc5ebbeeb56
Me4502 <mnmiller1@hotmail.com>
2014-01-10 18:08:16 +1000

    Added place listener to Sponge mech.

--- src/main/java/com/sk89q/craftbook/mech/Sponge.java
+++ src/main/java/com/sk89q/craftbook/mech/Sponge.java
@@ -5,6 +5,7 @@ import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.block.BlockFromToEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 
 import com.sk89q.craftbook.AbstractCraftBookMechanic;
 import com.sk89q.craftbook.bukkit.CraftBookPlugin;
@@ -35,6 +36,26 @@ public class Sponge extends AbstractCraftBookMechanic {
                     if(sponge.getType() == Material.SPONGE) {
                         event.setCancelled(true);
                         return;
+                    }
+                }
+            }
+        }
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGH)
+    public void onBlockPlace(BlockPlaceEvent event) {
+
+        if(event.getBlock().getType() != Material.SPONGE) return;
+
+        if(CraftBookPlugin.inst().getConfiguration().spongeRedstone && !event.getBlock().isBlockIndirectlyPowered()) return;
+
+        for (int cx = -radius; cx <= radius; cx++) {
+            for (int cy = -radius; cy <= radius; cy++) {
+                for (int cz = -radius; cz <= radius; cz++) {
+                    Block water = event.getBlock().getRelative(cx, cy, cz);
+                    if(CraftBookPlugin.inst().getConfiguration().spongeCircleRadius && !LocationUtil.isWithinSphericalRadius(water.getLocation(), event.getBlock().getLocation(), radius)) return;
+                    if(water.getType() == Material.WATER || water.getType() == Material.STATIONARY_WATER) {
+                        water.setTypeId(0, false);
                     }
                 }
             }
