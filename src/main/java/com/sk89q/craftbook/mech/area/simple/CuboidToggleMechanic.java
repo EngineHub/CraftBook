818fa59d91793cc0902b817856ebe12445d505bc
Me4502 <mnmiller1@hotmail.com>
2014-03-10 15:28:06 +1000

    Fixed bridge/door transmutation bug.

--- src/main/java/com/sk89q/craftbook/mech/area/simple/CuboidToggleMechanic.java
+++ src/main/java/com/sk89q/craftbook/mech/area/simple/CuboidToggleMechanic.java
@@ -45,7 +45,7 @@ public abstract class CuboidToggleMechanic extends AbstractCraftBookMechanic {
         for (Vector bv : toggle) {
             Block b = sign.getWorld().getBlockAt(bv.getBlockX(), bv.getBlockY(), bv.getBlockZ());
             Material oldType = b.getType();
-            if (b.getType() == base.getType() || BlockUtil.isBlockReplacable(b.getType())) {
+            if (BlockUtil.areBlocksIdentical(b, base) || BlockUtil.isBlockReplacable(b.getType())) {
                 b.setType(Material.AIR);
                 if (CraftBookPlugin.inst().getConfiguration().safeDestruction) {
                     if (oldType == base.getType()) {
