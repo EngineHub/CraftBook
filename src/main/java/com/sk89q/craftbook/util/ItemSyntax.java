db5f970a7ecef24949dbf0916786f0dd52423c89
Me4502 <mnmiller1@hotmail.com>
2014-07-21 19:03:36 +1000

    [Experimental] Cache ItemSyntax getItem.

--- src/main/java/com/sk89q/craftbook/util/ItemSyntax.java
+++ src/main/java/com/sk89q/craftbook/util/ItemSyntax.java
@@ -4,6 +4,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map.Entry;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Pattern;
 
 import org.apache.commons.lang.StringUtils;
@@ -22,6 +23,10 @@ import org.bukkit.material.MaterialData;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
 
 /**
  * The Standard Item Syntax. This class is built to be able to survive on its own, without CraftBook.
@@ -100,6 +105,130 @@ public class ItemSyntax {
         return StringUtils.replace(builder.toString(), "\u00A7", "&");
     }
 
+    private static final Cache<String, ItemStack> itemCache = CacheBuilder.newBuilder().maximumSize(100).expireAfterAccess(60, TimeUnit.MINUTES).build(new CacheLoader<String, ItemStack>() {
+
+        @Override
+        public ItemStack load(String line) throws Exception {
+
+            Material material = Material.AIR;
+            int data = -1;
+            int amount = 1;
+
+            String[] advMetadataSplit = FSLASH_PATTERN.split(line);
+            String[] nameLoreSplit = PIPE_PATTERN.split(advMetadataSplit[0]);
+            String[] enchantSplit = SEMICOLON_PATTERN.split(nameLoreSplit[0]);
+            String[] amountSplit = ASTERISK_PATTERN.split(enchantSplit[0], 2);
+            String[] dataSplit = COLON_PATTERN.split(amountSplit[0], 2);
+
+            try {
+                material = Material.getMaterial(Integer.parseInt(dataSplit[0]));
+            } catch (NumberFormatException e) {
+                try {
+                    material = Material.matchMaterial(dataSplit[0]);
+                } catch (Exception ee) {
+                    try {
+                        try {
+                            Object itemType = Class.forName("com.sk89q.worldedit.blocks.ItemType").getMethod("lookup", String.class).invoke(null, dataSplit[0]);
+                            material = Material.getMaterial((Integer) itemType.getClass().getMethod("getID").invoke(itemType));
+                        }
+                        catch(Exception eee){
+                            Object blockType = Class.forName("com.sk89q.worldedit.blocks.BlockType").getMethod("lookup", String.class).invoke(null, dataSplit[0]);
+                            material = Material.getMaterial((Integer) blockType.getClass().getMethod("getID").invoke(blockType));
+                        }
+                    } catch(Throwable ignored){}
+                }
+            }
+            try {
+                if (dataSplit.length > 1)
+                    data = Integer.parseInt(dataSplit[1]);
+            } catch(Exception ignored){}
+            try {
+                if(amountSplit.length > 1)
+                    amount = Integer.parseInt(amountSplit[1]);
+            } catch(Exception ignored){}
+
+            if (material == null || material == Material.AIR) material = Material.STONE;
+
+            ItemStack rVal = new ItemStack(material, amount, (short) data);
+            rVal.setData(new MaterialData(material, (byte)data));
+
+            if(nameLoreSplit.length > 1) {
+
+                ItemMeta meta = rVal.getItemMeta();
+                //if(!nameLoreSplit[1].equalsIgnoreCase("$IGNORE"))
+                meta.setDisplayName(ChatColor.translateAlternateColorCodes('&', nameLoreSplit[1]));
+                if(nameLoreSplit.length > 2) {
+
+                    List<String> lore = new ArrayList<String>();
+                    for(int i = 2; i < nameLoreSplit.length; i++)
+                        lore.add(ChatColor.translateAlternateColorCodes('&', nameLoreSplit[i]));
+
+                    meta.setLore(lore);
+                }
+
+                rVal.setItemMeta(meta);
+            }
+            if(enchantSplit.length > 1) {
+
+                for(int i = 1; i < enchantSplit.length; i++) {
+
+                    try {
+                        String[] sp = COLON_PATTERN.split(enchantSplit[i]);
+                        Enchantment ench = Enchantment.getByName(sp[0]);
+                        if(ench == null)
+                            ench = Enchantment.getById(Integer.parseInt(sp[0]));
+                        rVal.addUnsafeEnchantment(ench, Integer.parseInt(sp[1]));
+                    }
+                    catch(NumberFormatException ignored){}
+                    catch(ArrayIndexOutOfBoundsException ignored){}
+                }
+            }
+            if(advMetadataSplit.length > 1) {
+
+                ItemMeta meta = rVal.getItemMeta();
+                for(int i = 1; i < advMetadataSplit.length; i++) {
+                    String section = advMetadataSplit[i];
+                    String[] bits = COLON_PATTERN.split(section, 2);
+
+                    //Invalid Bit check
+                    if (bits.length < 2) continue;
+
+                    if(bits[0].equalsIgnoreCase("player") && meta instanceof SkullMeta)
+                        ((SkullMeta) meta).setOwner(bits[1]);
+                    else if(bits[0].equalsIgnoreCase("author") && meta instanceof BookMeta)
+                        ((BookMeta) meta).setAuthor(bits[1]);
+                    else if(bits[0].equalsIgnoreCase("title") && meta instanceof BookMeta)
+                        ((BookMeta) meta).setTitle(bits[1]);
+                    else if(bits[0].equalsIgnoreCase("page") && meta instanceof BookMeta)
+                        ((BookMeta) meta).addPage(bits[1]);
+                    else if(bits[0].equalsIgnoreCase("color") && meta instanceof LeatherArmorMeta) {
+
+                        String[] cols = COMMA_PATTERN.split(bits[1]);
+                        ((LeatherArmorMeta) meta).setColor(org.bukkit.Color.fromRGB(Integer.parseInt(cols[0]),Integer.parseInt(cols[1]),Integer.parseInt(cols[2])));
+                    } else if(bits[0].equalsIgnoreCase("potion") && meta instanceof PotionMeta) {
+
+                        String[] effects = SEMICOLON_PATTERN.split(bits[1]);
+                        try {
+                            PotionEffect effect = new PotionEffect(PotionEffectType.getByName(effects[0]), Integer.parseInt(effects[1]), Integer.parseInt(effects[2]));
+                            ((PotionMeta) meta).addCustomEffect(effect, true);
+                        } catch(Exception ignored){}
+                    } else if(bits[0].equalsIgnoreCase("enchant") && meta instanceof EnchantmentStorageMeta) {
+                        try {
+                            String[] sp = SEMICOLON_PATTERN.split(bits[1]);
+                            Enchantment ench = Enchantment.getByName(sp[0]);
+                            if(ench == null)
+                                ench = Enchantment.getById(Integer.parseInt(sp[0]));
+                            ((EnchantmentStorageMeta) meta).addStoredEnchant(ench, Integer.parseInt(sp[1]), true);
+                        } catch(Exception ignored){}
+                    }
+                }
+                rVal.setItemMeta(meta);
+            }
+
+            return rVal;
+        }
+    });
+
     /**
      * Parse an item from a line of text.
      * 
@@ -134,121 +263,6 @@ public class ItemSyntax {
             }
         }
 
-        Material material = Material.AIR;
-        int data = -1;
-        int amount = 1;
-
-        String[] advMetadataSplit = FSLASH_PATTERN.split(line);
-        String[] nameLoreSplit = PIPE_PATTERN.split(advMetadataSplit[0]);
-        String[] enchantSplit = SEMICOLON_PATTERN.split(nameLoreSplit[0]);
-        String[] amountSplit = ASTERISK_PATTERN.split(enchantSplit[0], 2);
-        String[] dataSplit = COLON_PATTERN.split(amountSplit[0], 2);
-
-        try {
-            material = Material.getMaterial(Integer.parseInt(dataSplit[0]));
-        } catch (NumberFormatException e) {
-            try {
-                material = Material.matchMaterial(dataSplit[0]);
-            } catch (Exception ee) {
-                try {
-                    try {
-                        Object itemType = Class.forName("com.sk89q.worldedit.blocks.ItemType").getMethod("lookup", String.class).invoke(null, dataSplit[0]);
-                        material = Material.getMaterial((Integer) itemType.getClass().getMethod("getID").invoke(itemType));
-                    }
-                    catch(Exception eee){
-                        Object blockType = Class.forName("com.sk89q.worldedit.blocks.BlockType").getMethod("lookup", String.class).invoke(null, dataSplit[0]);
-                        material = Material.getMaterial((Integer) blockType.getClass().getMethod("getID").invoke(blockType));
-                    }
-                } catch(Throwable ignored){}
-            }
-        }
-        try {
-            if (dataSplit.length > 1)
-                data = Integer.parseInt(dataSplit[1]);
-        } catch(Exception ignored){}
-        try {
-            if(amountSplit.length > 1)
-                amount = Integer.parseInt(amountSplit[1]);
-        } catch(Exception ignored){}
-
-        if (material == null || material == Material.AIR) material = Material.STONE;
-
-        ItemStack rVal = new ItemStack(material, amount, (short) data);
-        rVal.setData(new MaterialData(material, (byte)data));
-
-        if(nameLoreSplit.length > 1) {
-
-            ItemMeta meta = rVal.getItemMeta();
-            //if(!nameLoreSplit[1].equalsIgnoreCase("$IGNORE"))
-            meta.setDisplayName(ChatColor.translateAlternateColorCodes('&', nameLoreSplit[1]));
-            if(nameLoreSplit.length > 2) {
-
-                List<String> lore = new ArrayList<String>();
-                for(int i = 2; i < nameLoreSplit.length; i++)
-                    lore.add(ChatColor.translateAlternateColorCodes('&', nameLoreSplit[i]));
-
-                meta.setLore(lore);
-            }
-
-            rVal.setItemMeta(meta);
-        }
-        if(enchantSplit.length > 1) {
-
-            for(int i = 1; i < enchantSplit.length; i++) {
-
-                try {
-                    String[] sp = COLON_PATTERN.split(enchantSplit[i]);
-                    Enchantment ench = Enchantment.getByName(sp[0]);
-                    if(ench == null)
-                        ench = Enchantment.getById(Integer.parseInt(sp[0]));
-                    rVal.addUnsafeEnchantment(ench, Integer.parseInt(sp[1]));
-                }
-                catch(NumberFormatException ignored){}
-                catch(ArrayIndexOutOfBoundsException ignored){}
-            }
-        }
-        if(advMetadataSplit.length > 1) {
-
-            ItemMeta meta = rVal.getItemMeta();
-            for(int i = 1; i < advMetadataSplit.length; i++) {
-                String section = advMetadataSplit[i];
-                String[] bits = COLON_PATTERN.split(section, 2);
-
-                //Invalid Bit check
-                if (bits.length < 2) continue;
-
-                if(bits[0].equalsIgnoreCase("player") && meta instanceof SkullMeta)
-                    ((SkullMeta) meta).setOwner(bits[1]);
-                else if(bits[0].equalsIgnoreCase("author") && meta instanceof BookMeta)
-                    ((BookMeta) meta).setAuthor(bits[1]);
-                else if(bits[0].equalsIgnoreCase("title") && meta instanceof BookMeta)
-                    ((BookMeta) meta).setTitle(bits[1]);
-                else if(bits[0].equalsIgnoreCase("page") && meta instanceof BookMeta)
-                    ((BookMeta) meta).addPage(bits[1]);
-                else if(bits[0].equalsIgnoreCase("color") && meta instanceof LeatherArmorMeta) {
-
-                    String[] cols = COMMA_PATTERN.split(bits[1]);
-                    ((LeatherArmorMeta) meta).setColor(org.bukkit.Color.fromRGB(Integer.parseInt(cols[0]),Integer.parseInt(cols[1]),Integer.parseInt(cols[2])));
-                } else if(bits[0].equalsIgnoreCase("potion") && meta instanceof PotionMeta) {
-
-                    String[] effects = SEMICOLON_PATTERN.split(bits[1]);
-                    try {
-                        PotionEffect effect = new PotionEffect(PotionEffectType.getByName(effects[0]), Integer.parseInt(effects[1]), Integer.parseInt(effects[2]));
-                        ((PotionMeta) meta).addCustomEffect(effect, true);
-                    } catch(Exception ignored){}
-                } else if(bits[0].equalsIgnoreCase("enchant") && meta instanceof EnchantmentStorageMeta) {
-                    try {
-                        String[] sp = SEMICOLON_PATTERN.split(bits[1]);
-                        Enchantment ench = Enchantment.getByName(sp[0]);
-                        if(ench == null)
-                            ench = Enchantment.getById(Integer.parseInt(sp[0]));
-                        ((EnchantmentStorageMeta) meta).addStoredEnchant(ench, Integer.parseInt(sp[1]), true);
-                    } catch(Exception ignored){}
-                }
-            }
-            rVal.setItemMeta(meta);
-        }
-
-        return rVal;
+        return itemCache.getUnchecked(line);
     }
 }
