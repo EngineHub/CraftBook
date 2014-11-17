2798707bdb3a7d6aea79f977b5489a5d80660868
Me4502 <mnmiller1@hotmail.com>
2014-11-17 16:35:24 +1000

    Improve build script.

--- src/main/java/com/sk89q/craftbook/CraftBookPlugin.java
+++ src/main/java/com/sk89q/craftbook/CraftBookPlugin.java
@@ -1,36 +1,51 @@
 package com.sk89q.craftbook;
 
 import java.lang.reflect.Field;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.Vector;
 
 import org.spongepowered.api.event.Subscribe;
-import org.spongepowered.api.event.state.ServerStartingEvent;
+import org.spongepowered.api.event.state.ServerStartedEvent;
 import org.spongepowered.api.plugin.Plugin;
 
 import com.sk89q.craftbook.mechanics.Mechanic;
+import com.sk89q.craftbook.mechanics.MechanicContainer;
 
 @Plugin(id = "CraftBook", name = "CraftBook", version = "4.0")
 public class CraftBookPlugin {
 
+	Set<MechanicContainer> availableMechanics = new HashSet<MechanicContainer>();
+
+	Set<MechanicContainer> enabledMechanics = new HashSet<MechanicContainer>();
+
 	@Subscribe
-	public void onServerStart(ServerStartingEvent event) {
-		
+	public void onServerStarted(ServerStartedEvent event) {
+
 		searchClasspath();
+
+		for(MechanicContainer mech : availableMechanics) {
+
+			//TODO is enabled check.
+
+			enabledMechanics.add(mech);
+		}
 	}
-	
+
+	@SuppressWarnings("unchecked")
 	public void searchClasspath() {
-		
+
 		try {
 			Field f = ClassLoader.class.getDeclaredField("classes");
-			  f.setAccessible(true);
+			f.setAccessible(true);
 
-			  Vector<Class> classes =  (Vector<Class>) f.get(getClass().getClassLoader());
-			  
-			  for(Class clazz : classes) {
-				  if(clazz.isAnnotationPresent(Mechanic.class)) { //It's a mechanic!
-					  
-				  }
-			  }
+			Vector<Class<?>> classes =  (Vector<Class<?>>) f.get(getClass().getClassLoader());
+
+			for(Class<?> clazz : classes) {
+				if(clazz.isAnnotationPresent(Mechanic.class)) { //It's a mechanic!
+					availableMechanics.add(new MechanicContainer(clazz));
+				}
+			}
 		} catch (SecurityException e) {
 			e.printStackTrace();
 		} catch (NoSuchFieldException e) {
---
+++ src/main/java/com/sk89q/craftbook/mechanics/MechanicContainer.java
@@ -0,0 +1,20 @@
+package com.sk89q.craftbook.mechanics;
+
+public class MechanicContainer {
+
+	private Class<?> clazz;
+
+	private Mechanic properties;
+
+	public MechanicContainer(Class<?> clazz) {
+
+		this.clazz = clazz;
+
+		properties = clazz.getAnnotation(Mechanic.class);
+	}
+
+	public String getName() {
+
+		return properties.name();
+	}
+}
