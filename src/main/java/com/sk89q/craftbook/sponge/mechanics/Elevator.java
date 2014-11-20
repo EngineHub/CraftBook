fe0820ce8bc12b056e6e18b3bb92f7a997c3ad02
Me4502 <mnmiller1@hotmail.com>
2014-11-20 14:33:11 +1000

    Improved elevator, and commented out missing content.

--- src/main/java/com/sk89q/craftbook/sponge/mechanics/Elevator.java
+++ src/main/java/com/sk89q/craftbook/sponge/mechanics/Elevator.java
@@ -1,11 +1,14 @@
 package com.sk89q.craftbook.sponge.mechanics;
 
-import org.spongepowered.api.event.Subscribe;
+import org.spongepowered.api.block.Block;
+import org.spongepowered.api.block.BlockTypes;
+import org.spongepowered.api.entity.Entity;
+import org.spongepowered.api.util.Direction;
 
 @Mechanic(name = "Elevator")
 public class Elevator {
 
-	@Subscribe
+	/*@Subscribe
 	public void onSignChange(SignChangeEvent event) {
 
 	}
@@ -13,5 +16,44 @@ public class Elevator {
 	@Subscribe
 	public void onPlayerInteract(PlayerInteractEvent event) {
 
+		transportEntity(event.getPlayer(), event.getBlock(), Direction.DOWN); //Can't read signs atm, assume down.
+	}*/
+
+	public void transportEntity(Entity entity, Block block, Direction direction) {
+
+		Block destination = findDestination(block, direction);
+
+		//TODO
+	}
+
+	/**
+	 * Gets the destination of an Elevator. If there is none, it returns the start.
+	 * 
+	 * @param block The starting block.
+	 * @param direction The direction to move in.
+	 * 
+	 * @return The elevator destination.
+	 */
+	private Block findDestination(Block block, Direction direction) {
+
+		int y = block.getY();
+
+		if(direction == Direction.UP || direction == Direction.DOWN) {
+
+			while(direction == Direction.UP ? y < 256 : y >= 0) {
+				Block test = block.getExtent().getBlock(block.getX(), y, block.getZ());
+
+				if(test.getType() == BlockTypes.WALL_SIGN || test.getType() == BlockTypes.STANDING_SIGN) {
+					//It's a sign.
+
+					//TODO Can't go any further yet with Sponge. Assume it's an elevator.
+					return test;
+				}
+			}
+		} else {
+			//We don't currently support non-up/down elevators, this isn't a Roald Dahl novel.
+		}
+
+		return block;
 	}
 }
--- src/main/java/com/sk89q/craftbook/sponge/mechanics/minecart/EmptyDecay.java
+++ src/main/java/com/sk89q/craftbook/sponge/mechanics/minecart/EmptyDecay.java
@@ -1,17 +1,16 @@
 package com.sk89q.craftbook.sponge.mechanics.minecart;
 
-import com.sk89q.craftbook.sponge.CraftBookPlugin;
 import com.sk89q.craftbook.sponge.mechanics.Mechanic;
 
 @Mechanic(name = "MinecartEmptyDecay")
 public class EmptyDecay {
 
-	public void onVehicleExit(EntityDismountEvent event) {
+	/*public void onVehicleExit(EntityDismountEvent event) {
 
 		event.getGame().getScheduler().runTaskAfter(CraftBookPlugin.instance, new MinecartDecay(event.getDismounted(), 40L));
 	}
 
-	public static class MinecartDecay extends Runnable {
+	public static class MinecartDecay implements Runnable {
 
 		Minecart cart;
 
@@ -20,11 +19,12 @@ public class EmptyDecay {
 			this.cart = cart;
 		}
 
+		@Override
 		public void run() {
 
 			if(cart.getRider() == null) {
 				cart.remove();
 			}
 		}
-	}
+	}*/
 }
