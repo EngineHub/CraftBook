add673caef45e956557eb05f2565115cfb4337d8
Me4502 <mnmiller1@hotmail.com>
2015-02-15 17:44:05 +1000

    Fixed missed diff

--- src/main/java/com/sk89q/craftbook/sponge/mechanics/integratedcircuits/ICSocket.java
+++ src/main/java/com/sk89q/craftbook/sponge/mechanics/integratedcircuits/ICSocket.java
@@ -1,29 +1,22 @@
 package com.sk89q.craftbook.sponge.mechanics.integratedcircuits;
 
-import com.sk89q.craftbook.core.util.CachePolicy;
 import com.sk89q.craftbook.sponge.mechanics.SpongeMechanic;
 
 public class ICSocket extends SpongeMechanic {
 
-	IC ic;
+    IC ic;
 
-	/**
-	 * Gets the IC that is in use by this IC Socket.
-	 * 
-	 * @return The IC
-	 */
-	public IC getIC() {
-		return ic;
-	}
+    /**
+     * Gets the IC that is in use by this IC Socket.
+     * 
+     * @return The IC
+     */
+    public IC getIC() {
+        return ic;
+    }
 
-	@Override
-	public String getName() {
-		return "IC";
-	}
-
-	@Override
-	public CachePolicy getCachePolicy() {
-		return null;
-	}
-
+    @Override
+    public String getName() {
+        return "IC";
+    }
 }
