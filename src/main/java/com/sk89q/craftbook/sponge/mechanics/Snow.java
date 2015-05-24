22345459a4ad3bbaf7029d42401a5968bf0d88e6
Me4502 <mnmiller1@me.com>
2015-05-24 18:39:42 +1000

    Added basic functionality to the Snow mechanic.

--- src/main/java/com/sk89q/craftbook/sponge/mechanics/Snow.java
+++ src/main/java/com/sk89q/craftbook/sponge/mechanics/Snow.java
@@ -1,6 +1,8 @@
 package com.sk89q.craftbook.sponge.mechanics;
 
+import com.google.common.base.Optional;
 import org.spongepowered.api.block.BlockTypes;
+import org.spongepowered.api.data.manipulator.block.LayeredData;
 import org.spongepowered.api.event.Subscribe;
 import org.spongepowered.api.event.block.BlockRandomTickEvent;
 import org.spongepowered.api.event.block.BlockUpdateEvent;
@@ -14,12 +16,53 @@ public class Snow extends SpongeMechanic {
 
     @Subscribe
     public void onBlockTick(BlockRandomTickEvent event) {
-
-        if (event.getBlock().getType() == BlockTypes.SNOW_LAYER) {
+        if (event.getBlock().getType() == BlockTypes.SNOW_LAYER || event.getBlock().getType() == BlockTypes.SNOW) {
             if (event.getBlock().getExtent().getWeather() != Weathers.CLEAR) {
-                // Temporarily just set to a full snow block.
-                event.getBlock().replaceWith(BlockTypes.SNOW);
+                //Higher the snow.
+                if (event.getBlock().getType() == BlockTypes.SNOW_LAYER)
+                    increaseSnow(event.getBlock());
+            } else {
+                //Lower the snow.
+                decreaseSnow(event.getBlock());
             }
+        }
+    }
+
+    public void increaseSnow(Location location) {
+
+        Optional<LayeredData> dataOptional = location.getData(LayeredData.class);
+        if(dataOptional.isPresent()) {
+            LayeredData data = dataOptional.get();
+            int currentHeight = data.getValue();
+            currentHeight ++;
+            if(currentHeight > data.getMaxValue())
+                location.replaceWith(BlockTypes.SNOW);
+            else {
+                data.setValue(currentHeight);
+                location.offer(data);
+            }
+        }
+    }
+
+    public void decreaseSnow(Location location) {
+
+        Optional<LayeredData> dataOptional = location.getData(LayeredData.class);
+        if(dataOptional.isPresent()) {
+            LayeredData data = dataOptional.get();
+            int currentHeight = data.getValue();
+            currentHeight --;
+            if(currentHeight < data.getMinValue())
+                location.replaceWith(BlockTypes.AIR);
+            else {
+                data.setValue(currentHeight);
+                location.offer(data);
+            }
+        } else {
+            location.replaceWith(BlockTypes.SNOW_LAYER);
+            dataOptional = location.getData(LayeredData.class);
+            LayeredData data = dataOptional.get();
+            data.setValue(data.getMaxValue());
+            location.offer(data);
         }
     }
 
