9d791bacf0cff510d7afea4821f56dfbeff6e9e7
Matthew Miller (Me4502) <mnmiller1@hotmail.com>
2016-02-12 19:43:33 +1000

    Added the basic bridge docs.

--- src/main/java/com/sk89q/craftbook/sponge/mechanics/area/Bridge.java
+++ src/main/java/com/sk89q/craftbook/sponge/mechanics/area/Bridge.java
@@ -148,7 +148,16 @@ public class Bridge extends SimpleArea implements DocumentationProvider {
 
     @Override
     public String getMainDocumentation() {
-        return "";
+        return  "=======" +
+                "Bridges" +
+                "=======" +
+                "**Bridges** are configurable-width flat sections of the world that you can toggle on and off. The width of the bridge can be changed." +
+                "" +
+                "They can be toggled using two different methods:" +
+                "* Right clicking a sign" +
+                "* Powering the sign with redstone" +
+                "" +
+                "";
     }
 
     @Override
