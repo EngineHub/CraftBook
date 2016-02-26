112cac060aaa5d3d2cad85bf66e0e96aa2a8d084
Matthew Miller (Me4502) <mnmiller1@hotmail.com>
2016-02-26 20:59:43 +1000

    Finish off the copy_docs script. It assumes that you are using run/ as the run dir, and that CraftBookDocs is in the parent dir.

--- copy_docs.py
+++ copy_docs.py
@@ -1,2 +1,18 @@
 import shutil
+import os
 
+def copy_tree(src, dst, symlinks=False, ignore=None):
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        if os.path.isdir(s):
+            if not os.path.exists(d):
+                os.mkdir(d)
+            copy_tree(s, d, symlinks, ignore)
+        else:
+            shutil.copy2(s, d)
+
+fromDir = "run/config/craftbook/documentation/"
+toDir = "../CraftBookDocs/source/"
+
+copy_tree(fromDir, toDir)
