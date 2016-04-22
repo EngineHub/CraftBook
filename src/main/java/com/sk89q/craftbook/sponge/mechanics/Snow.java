1844c211f1c565647caea6b43f1c1506dc24e836
Matthew Miller <mnmiller1@me.com>
2016-04-22 14:00:13 +1000

    Refactor snow to make it more manageable.

--- src/main/java/com/sk89q/craftbook/sponge/mechanics/Snow.java
+++ src/main/java/com/sk89q/craftbook/sponge/mechanics/Snow.java
@@ -26,7 +26,7 @@ import org.spongepowered.api.data.key.Keys;
 import org.spongepowered.api.data.manipulator.mutable.block.LayeredData;
 import org.spongepowered.api.data.property.block.ReplaceableProperty;
 import org.spongepowered.api.data.property.block.TemperatureProperty;
-import org.spongepowered.api.data.value.BoundedValue;
+import org.spongepowered.api.data.value.mutable.MutableBoundedValue;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.block.TickBlockEvent;
 import org.spongepowered.api.util.Direction;
@@ -39,88 +39,97 @@ import java.util.Optional;
 @Module(moduleName = "Snow", onEnable="onInitialize", onDisable="onDisable")
 public class Snow extends SpongeMechanic {
 
+    /**
+     * An array of directions that snow can move in. In order of preference.
+     */
+    private static final Direction[] VALID_SNOW_DIRECTIONS = new Direction[]{Direction.DOWN, Direction.NORTH, Direction.SOUTH, Direction.EAST, Direction.WEST, Direction.NONE};
+
+    @Override
+    public void onInitialize() throws CraftBookException {
+
+        super.onInitialize();
+
+        //Give the snow blocks the ability to tick randomly.
+        BlockTypes.SNOW_LAYER.setTickRandomly(true);
+        BlockTypes.SNOW.setTickRandomly(true);
+    }
+
     @Listener
     public void onBlockTick(TickBlockEvent event) {
-        if ((event.getTargetBlock().getState().getType() == BlockTypes.SNOW_LAYER || event.getTargetBlock().getState().getType() == BlockTypes.SNOW)) {
-            if (event.getTargetBlock().getLocation().get().getExtent().getWeather() != Weathers.CLEAR) {
-                //Higher the snow.
-                if (event.getTargetBlock().getLocation().get().getBlockType() == BlockTypes.SNOW_LAYER && canSeeSky(event.getTargetBlock().getLocation().get()) && event.getTargetBlock().getLocation().get().getProperty(TemperatureProperty.class).get().getValue() <= 0.15f) //Only increase snow at valid blocks, where snow could actually fall.
-                    increaseSnow(event.getTargetBlock().getLocation().get(), true);
-            } else if(!isBlockBuried(event.getTargetBlock().getLocation().get())) { //Only melt if on top, and too hot.
+        Location<World> location = event.getTargetBlock().getLocation().orElse(null);
+        if(location == null) return;
+        if ((event.getTargetBlock().getState().getType() == BlockTypes.SNOW_LAYER
+                || event.getTargetBlock().getState().getType() == BlockTypes.SNOW)) {
+            if (location.getExtent().getWeather() != Weathers.CLEAR) {
+                //Only increase snow at valid blocks, where snow could actually fall.
+                if (location.getBlockType() == BlockTypes.SNOW_LAYER && canSnowReach(location)
+                        && location.getProperty(TemperatureProperty.class).get().getValue() <= 0.15f)
+                    increaseSnow(location, true);
+            } else if(!isBlockBuried(location)) { //Only melt if on top, and too hot.
                 //Lower the snow.
-                Optional<LayeredData> data = event.getTargetBlock().getLocation().get().get(LayeredData.class);
-                if(data.isPresent()) {
-                    if(data.get().getValue(Keys.LAYER).get().get() == 0)
-                        return;
-                } else if (event.getTargetBlock().getLocation().get().getProperty(TemperatureProperty.class).get().getValue() > 0.15f) {
+                if(location.get(Keys.LAYER).orElse(-1) == 0) {
+                    return;
+                } else if (location.getProperty(TemperatureProperty.class).get().getValue() > 0.15f) {
                     return;
                 }
 
-                decreaseSnow(event.getTargetBlock().getLocation().get());
+                decreaseSnow(location);
             }
         }
     }
 
-    private void increaseSnow(Location<World> location, boolean disperse) {
-        Optional<LayeredData> dataOptional = location.get(LayeredData.class);
-        if(dataOptional.isPresent()) {
-            LayeredData data = dataOptional.get();
-            int currentHeight = data.getValue(Keys.LAYER).get().get();
-            currentHeight ++;
-            if(currentHeight > data.getValue(Keys.LAYER).get().getMaxValue())
+    private void increaseSnow(Location<?> location, boolean disperse) {
+        Optional<MutableBoundedValue<Integer>> optionalHeightValue = location.getValue(Keys.LAYER);
+        if(optionalHeightValue.isPresent()) {
+            MutableBoundedValue<Integer> heightValue = optionalHeightValue.get();
+            int currentHeight = heightValue.get() + 1;
+            if(currentHeight > heightValue.getMaxValue())
                 location.setBlockType(BlockTypes.SNOW);
             else {
                 if(disperse) {
                     disperseSnow(location, null);
                 } else {
-                    data.set(Keys.LAYER, currentHeight);
-                    location.offer(data);
+                    location.offer(Keys.LAYER, currentHeight);
                 }
             }
 
-            if(location.getRelative(Direction.DOWN).getBlockType() == BlockTypes.WATER || location.getRelative(Direction.DOWN).getBlockType() == BlockTypes.FLOWING_WATER)
+            if(location.getRelative(Direction.DOWN).getBlockType() == BlockTypes.WATER
+                    || location.getRelative(Direction.DOWN).getBlockType() == BlockTypes.FLOWING_WATER)
                 location.getRelative(Direction.DOWN).setBlockType(BlockTypes.ICE);
         } else {
             location.setBlockType(BlockTypes.SNOW_LAYER);
         }
     }
 
-    private void decreaseSnow(Location<World> location) {
-        Optional<LayeredData> dataOptional = location.get(LayeredData.class);
-        if(dataOptional.isPresent()) {
-            LayeredData data = dataOptional.get();
-            int currentHeight = data.getValue(Keys.LAYER).get().get();
-            currentHeight --;
-            if(currentHeight < data.getValue(Keys.LAYER).get().getMinValue())
+    private void decreaseSnow(Location<?> location) {
+        Optional<MutableBoundedValue<Integer>> optionalHeightValue = location.getValue(Keys.LAYER);
+        if(optionalHeightValue.isPresent()) {
+            MutableBoundedValue<Integer> heightValue = optionalHeightValue.get();
+            int currentHeight = heightValue.get() - 1;
+            if(currentHeight < heightValue.getMinValue())
                 location.setBlockType(BlockTypes.AIR);
-            else {
-                data.set(Keys.LAYER, currentHeight);
-                location.offer(data);
-            }
+            else
+                location.offer(Keys.LAYER, currentHeight);
         } else if (location.getBlockType() == BlockTypes.SNOW) {
             location.setBlockType(BlockTypes.SNOW_LAYER);
-            dataOptional = location.getOrCreate(LayeredData.class);
-            LayeredData data = dataOptional.get();
-            data.set(Keys.LAYER, (int)((BoundedValue)data.getValue(Keys.LAYER).get()).getMaxValue());
+            LayeredData data = location.getOrCreate(LayeredData.class).get();
+            data.set(Keys.LAYER, data.getValue(Keys.LAYER).get().getMaxValue());
             location.offer(data);
         }
     }
 
-    private static final Direction[] VALID_SNOW_DIRECTIONS = new Direction[]{Direction.DOWN, Direction.NORTH, Direction.SOUTH, Direction.EAST, Direction.WEST, Direction.NONE};
-
-    private void disperseSnow(final Location<World> location, Direction ignoredFace) {
-
-        Optional<LayeredData> heightData = location.getOrCreate(LayeredData.class);
-        int currentHeight = heightData.get().getValue(Keys.LAYER).get().get();
+    private void disperseSnow(final Location<?> location, Direction ignoredFace) {
+        int currentHeight = location.get(Keys.LAYER).orElse(-1);
+        if(currentHeight == -1)
+            return;
 
         for(final Direction dir : VALID_SNOW_DIRECTIONS) {
             if(dir == ignoredFace) continue;
             if(currentHeight == 0 && !(dir == Direction.DOWN || dir == Direction.NONE)) continue; //Stop snow moving around on the ground.
-            final Location<World> relative = location.getRelative(dir);
+            final Location<?> relative = location.getRelative(dir);
             if(canPlaceSnowAt(relative)) {
-                Optional<LayeredData> dataOptional = relative.get(LayeredData.class);
-                if(dataOptional.isPresent()) {
-                    int otherHeight = dataOptional.get().getValue(Keys.LAYER).get().get();
+                int otherHeight = relative.get(Keys.LAYER).orElse(-1);
+                if(otherHeight >= 0) {
                     if(dir != Direction.NONE && dir != Direction.DOWN && currentHeight <= otherHeight+1)
                         continue;
                 }
@@ -138,45 +147,56 @@ public class Snow extends SpongeMechanic {
         }
     }
 
-    private boolean canSeeSky(Location<World> location) {
+    /**
+     * Determines whether or not snow is able to reach this block.
+     *
+     * @param location The block.
+     * @return If snow can reach it.
+     */
+    private boolean canSnowReach(Location<?> location) {
         while(location.getBlockY() < location.getExtent().getBlockMax().getY()) {
             location = location.getRelative(Direction.UP);
-            if(location.getBlockType() != BlockTypes.AIR && location.getBlockType() != BlockTypes.LEAVES && location.getBlockType() != BlockTypes.LEAVES2)
+            if(location.getBlockType() != BlockTypes.AIR
+                    && location.getBlockType() != BlockTypes.LEAVES
+                    && location.getBlockType() != BlockTypes.LEAVES2)
                 return false;
         }
         return true;
     }
 
-    private boolean canPlaceSnowAt(Location<World> location) {
-        return location.getBlockType() == BlockTypes.SNOW_LAYER || !(location.getBlockType() == BlockTypes.WATER || location.getBlockType() == BlockTypes.FLOWING_WATER || location.getBlockType() == BlockTypes.LAVA || location.getBlockType() == BlockTypes.FLOWING_LAVA) && location.getBlockType().getProperty(ReplaceableProperty.class).get().getValue();
+    /**
+     * Gets whether the block is replacable with snow.
+     *
+     * @param location The block.
+     * @return If it can be replaced with snow.
+     */
+    private boolean canPlaceSnowAt(Location<?> location) {
+        if (location.getBlockType() == BlockTypes.SNOW_LAYER)
+            return true;
+
+        Optional<ReplaceableProperty> replaceableProperty = location.getBlockType().getProperty(ReplaceableProperty.class);
+
+        return replaceableProperty.isPresent()
+                && !(location.getBlockType() == BlockTypes.WATER
+                || location.getBlockType() == BlockTypes.FLOWING_WATER
+                || location.getBlockType() == BlockTypes.LAVA
+                || location.getBlockType() == BlockTypes.FLOWING_LAVA)
+                && replaceableProperty.get().getValue();
     }
 
-    private boolean isBlockBuried(Location<World> location) {
-        return location.getRelative(Direction.UP).getBlockType() == BlockTypes.SNOW_LAYER || location.getRelative(Direction.UP).getBlockType() == BlockTypes.SNOW;
+    /**
+     * Determines if the specified block is buried under snow.
+     *
+     * @param location The location of the block.
+     * @return If it is buried.
+     */
+    private boolean isBlockBuried(Location<?> location) {
+        return location.getRelative(Direction.UP).getBlockType() == BlockTypes.SNOW_LAYER
+                || location.getRelative(Direction.UP).getBlockType() == BlockTypes.SNOW;
     }
-
-    /*@Listener
-    public void onBlockUpdate(BlockUpdateEvent event) {
-
-        if (event.getBlock().getType() == BlockTypes.SNOW || event.getBlock().getType() == BlockTypes.SNOW_LAYER) {
-            // Occurred in a block where a snow-related change could have happened.
-            for (Location block : event.getLocations()) {
-
-            }
-        }
-    }*/
 
     @Override
     public String getName() {
         return "BetterSnow";
-    }
-
-    @Override
-    public void onInitialize() throws CraftBookException {
-
-        super.onInitialize();
-
-        BlockTypes.SNOW_LAYER.setTickRandomly(true);
-        BlockTypes.SNOW.setTickRandomly(true);
     }
 }
