90a3a63e1688ef4be0e3327ccfaed38245354754
Me4502 <mnmiller1@me.com>
2016-09-03 15:44:50 +1000

    Backported the command to toggle TreeLopper

--- src/main/java/com/sk89q/craftbook/sponge/mechanics/treelopper/TreeLopper.java
+++ src/main/java/com/sk89q/craftbook/sponge/mechanics/treelopper/TreeLopper.java
@@ -14,22 +14,28 @@
  * You should have received a copy of the GNU General Public License along with this program. If not,
  * see <http://www.gnu.org/licenses/>.
  */
-package com.sk89q.craftbook.sponge.mechanics;
+package com.sk89q.craftbook.sponge.mechanics.treelopper;
 
 import com.google.common.collect.Lists;
 import com.google.inject.Inject;
 import com.me4502.modularframework.module.Module;
 import com.me4502.modularframework.module.guice.ModuleConfiguration;
 import com.sk89q.craftbook.core.util.ConfigValue;
+import com.sk89q.craftbook.core.util.CraftBookException;
 import com.sk89q.craftbook.sponge.CraftBookPlugin;
+import com.sk89q.craftbook.sponge.mechanics.treelopper.command.ToggleCommand;
 import com.sk89q.craftbook.sponge.mechanics.types.SpongeMechanic;
 import com.sk89q.craftbook.sponge.util.BlockFilter;
 import com.sk89q.craftbook.sponge.util.BlockUtil;
+import com.sk89q.craftbook.sponge.util.SpongePermissionNode;
 import com.sk89q.craftbook.sponge.util.type.BlockFilterListTypeToken;
+import com.sk89q.craftbook.sponge.util.type.UUIDListTypeToken;
 import ninja.leaping.configurate.ConfigurationNode;
+import org.spongepowered.api.Sponge;
 import org.spongepowered.api.block.BlockTypes;
+import org.spongepowered.api.command.args.GenericArguments;
+import org.spongepowered.api.command.spec.CommandSpec;
 import org.spongepowered.api.data.key.Keys;
-import org.spongepowered.api.data.type.TreeType;
 import org.spongepowered.api.entity.EntityTypes;
 import org.spongepowered.api.entity.Item;
 import org.spongepowered.api.entity.living.player.Player;
@@ -38,16 +44,16 @@ import org.spongepowered.api.event.block.ChangeBlockEvent;
 import org.spongepowered.api.event.cause.Cause;
 import org.spongepowered.api.event.cause.NamedCause;
 import org.spongepowered.api.event.filter.cause.Named;
-import org.spongepowered.api.item.ItemTypes;
 import org.spongepowered.api.item.inventory.ItemStack;
-import org.spongepowered.api.item.inventory.ItemStackSnapshot;
+import org.spongepowered.api.service.permission.PermissionDescription;
+import org.spongepowered.api.text.Text;
 import org.spongepowered.api.util.Direction;
 import org.spongepowered.api.world.Location;
 import org.spongepowered.api.world.World;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+import java.util.UUID;
 
 @Module(moduleName = "TreeLopper", onEnable="onInitialize", onDisable="onDisable")
 public class TreeLopper extends SpongeMechanic {
@@ -57,10 +63,39 @@ public class TreeLopper extends SpongeMechanic {
     public ConfigurationNode config;
 
     private ConfigValue<List<BlockFilter>> allowedBlocks = new ConfigValue<>("allowed-blocks", "A list of blocks that are logs.", getDefaultBlocks(), new BlockFilterListTypeToken());
+    public ConfigValue<List<UUID>> disabledPlayers = new ConfigValue<>("disabled-users", "A list of users that have disabled the mechanic.", new ArrayList<>(), new UUIDListTypeToken());
+
+    private SpongePermissionNode togglePermission = new SpongePermissionNode("craftbook.treelopper.toggle", "Allows the user to toggle TreeLopper on and off.", PermissionDescription.ROLE_USER);
 
     @Override
-    public void onInitialize() {
+    public void onInitialize() throws CraftBookException {
+        super.onInitialize();
+
         allowedBlocks.load(config);
+        disabledPlayers.load(config);
+
+        togglePermission.register();
+
+        CommandSpec toggleCommand = CommandSpec.builder()
+                .description(Text.of("Toggles TreeLopper being enabled."))
+                .permission(togglePermission.getNode())
+                .arguments(GenericArguments.optional(GenericArguments.bool(Text.of("state"))))
+                .executor(new ToggleCommand(this))
+                .build();
+
+        CommandSpec treeLopperCommand = CommandSpec.builder()
+                .description(Text.of("Base TreeLopper command"))
+                .child(toggleCommand, "toggle")
+                .build();
+
+        Sponge.getCommandManager().register(CraftBookPlugin.inst(), treeLopperCommand, "treelopper", "timber");
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+
+        disabledPlayers.save(config);
     }
 
     @Listener
