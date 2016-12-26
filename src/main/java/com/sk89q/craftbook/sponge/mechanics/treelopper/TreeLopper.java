4f2ad52592749aabe8a2c7d4c08b9566bc52f7ca
Me4502 <mnmiller1@me.com>
2016-12-26 15:39:28 +1000

    Fixed TreeLopper not working. Still needs sponge change in order to add block drops.

--- src/main/java/com/sk89q/craftbook/sponge/mechanics/treelopper/TreeLopper.java
+++ src/main/java/com/sk89q/craftbook/sponge/mechanics/treelopper/TreeLopper.java
@@ -107,9 +107,14 @@ public class TreeLopper extends SpongeMechanic implements DocumentationProvider 
     @Listener
     public void onBlockBreak(ChangeBlockEvent.Break event, @Named(NamedCause.SOURCE) Player player) {
         if(ItemUtil.doesStackPassFilters(allowedItems.getValue(), player.getItemInHand(HandTypes.MAIN_HAND).orElse(null)) && !disabledPlayers.getValue().contains(player.getUniqueId())) {
-            event.getTransactions().stream().filter((t) -> BlockUtil.doesStatePassFilters(allowedBlocks.getValue(), t.getOriginal().getState())).forEach((transaction) -> {
+            event.getTransactions().stream().filter((t) -> BlockUtil.doesStatePassFilters(allowedBlocks.getValue(), t.getOriginal().getState())).forEach(transaction -> {
                 Optional<TreeType> treeType = transaction.getOriginal().get(Keys.TREE_TYPE);
-                treeType.ifPresent(treeType1 -> checkBlocks(transaction.getOriginal().getLocation().get(), player, treeType1, new ArrayList<>()));
+                treeType.ifPresent(treeType1 -> transaction.getOriginal().getLocation().ifPresent(location -> {
+                    List<Location> traversed = Lists.newArrayList(location);
+                    for(Direction dir : BlockUtil.getDirectFaces()) {
+                        checkBlocks(location.getRelative(dir), player, treeType1, traversed);
+                    }
+                }));
             });
         }
     }
@@ -122,8 +127,9 @@ public class TreeLopper extends SpongeMechanic implements DocumentationProvider 
         Optional<TreeType> data = block.getBlock().get(Keys.TREE_TYPE);
 
         if(data.isPresent() && data.get().equals(type)) { //Same tree type.
-            block.getExtent().digBlockWith(block.getBlockPosition(), player.getItemInHand(HandTypes.MAIN_HAND).orElse(null), Cause.of(NamedCause.simulated(player)));
-            block.removeBlock(Cause.of(NamedCause.source(player)));
+            // TODO Switch to FakePlayer when implemented.
+            //block.getExtent().digBlockWith(block.getBlockPosition(), player.getItemInHand(HandTypes.MAIN_HAND).orElse(null), Cause.of(NamedCause.simulated(player)));
+            block.removeBlock(CraftBookPlugin.spongeInst().getCause().build());
             for(Direction dir : BlockUtil.getDirectFaces()) {
                 checkBlocks(block.getRelative(dir), player, type, traversed);
             }
