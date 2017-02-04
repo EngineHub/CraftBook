265032e0262a2f9b0aa5c9c42e0138d3f3276a93
Me4502 <mnmiller1@me.com>
2017-02-04 15:10:30 +1000

    Added Speed Modifiers mechanic for boats.

--- src/main/java/com/sk89q/craftbook/sponge/mechanics/boat/SpeedModifiers.java
+++ src/main/java/com/sk89q/craftbook/sponge/mechanics/boat/SpeedModifiers.java
@@ -16,38 +16,54 @@
  */
 package com.sk89q.craftbook.sponge.mechanics.boat;
 
+import com.google.common.reflect.TypeToken;
 import com.google.inject.Inject;
 import com.me4502.modularframework.module.Module;
 import com.me4502.modularframework.module.guice.ModuleConfiguration;
+import com.sk89q.craftbook.core.util.ConfigValue;
 import com.sk89q.craftbook.core.util.CraftBookException;
 import com.sk89q.craftbook.core.util.documentation.DocumentationProvider;
 import com.sk89q.craftbook.sponge.mechanics.types.SpongeMechanic;
 import ninja.leaping.configurate.ConfigurationNode;
-import org.spongepowered.api.block.BlockTypes;
 import org.spongepowered.api.entity.vehicle.Boat;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.entity.SpawnEntityEvent;
-import org.spongepowered.api.world.Location;
-import org.spongepowered.api.world.World;
 
-@Module(moduleId = "boatwaterplaceonly", moduleName = "WaterPlaceOnly", onEnable="onInitialize", onDisable="onDisable")
-public class WaterPlaceOnly extends SpongeMechanic implements DocumentationProvider {
+@Module(moduleId = "boatspeedmodifiers", moduleName = "BoatSpeedModifiers", onEnable="onInitialize", onDisable="onDisable")
+public class SpeedModifiers extends SpongeMechanic implements DocumentationProvider {
 
     @Inject
     @ModuleConfiguration
     public ConfigurationNode config;
 
+    private ConfigValue<Double> maxSpeed = new ConfigValue<>("max-speed", "Sets the maximum speed of boats. -1 to disable.", -1d, TypeToken.of(Double.class));
+    private ConfigValue<Double> occupiedDeceleration = new ConfigValue<>("occupied-deceleration",
+            "Sets the occupied deceleration of boats. -1 to disable.", -1d, TypeToken.of(Double.class));
+    private ConfigValue<Double> unoccupiedDeceleration = new ConfigValue<>("unoccupied-deceleration",
+            "Sets the unoccupied deceleration of boats. -1 to disable.", -1d, TypeToken.of(Double.class));
+
+    @Override
+    public void onInitialize() throws CraftBookException {
+        super.onInitialize();
+
+        maxSpeed.load(config);
+        occupiedDeceleration.load(config);
+        unoccupiedDeceleration.load(config);
+    }
+
     @Listener
     public void onEntityConstruct(SpawnEntityEvent event) {
         event.getEntities().stream().filter(entity -> entity instanceof Boat).forEach(boat -> {
-            if (!isWater(boat.getLocation())) {
-                event.setCancelled(true);
+            if (maxSpeed.getValue() >= 0) {
+                ((Boat) boat).setMaxSpeed(maxSpeed.getValue());
+            }
+            if (occupiedDeceleration.getValue() >= 0) {
+                ((Boat) boat).setOccupiedDeceleration(occupiedDeceleration.getValue());
+            }
+            if (unoccupiedDeceleration.getValue() >= 0) {
+                ((Boat) boat).setUnoccupiedDeceleration(unoccupiedDeceleration.getValue());
             }
         });
-    }
-
-    private static boolean isWater(Location<World> location) {
-        return location.getBlockType() == BlockTypes.WATER || location.getBlockType() == BlockTypes.FLOWING_WATER;
     }
 
     @Override
@@ -57,6 +73,15 @@ public class WaterPlaceOnly extends SpongeMechanic implements DocumentationProvi
 
     @Override
     public String getPath() {
-        return "mechanics/boat/waterplaceonly";
+        return "mechanics/boat/speedmodifiers";
+    }
+
+    @Override
+    public ConfigValue<?>[] getConfigurationNodes() {
+        return new ConfigValue[] {
+                maxSpeed,
+                occupiedDeceleration,
+                unoccupiedDeceleration
+        };
     }
 }
