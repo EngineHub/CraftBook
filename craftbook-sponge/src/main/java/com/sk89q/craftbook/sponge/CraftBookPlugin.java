e2e4c57aed7b855fed8dea1ef8f3e4c17ea347ea
Matthew Miller <mnmiller1@me.com>
2018-01-09 14:09:12 +1000

    Added Item Pickup mechanic.

--- craftbook-sponge/src/main/java/com/sk89q/craftbook/sponge/CraftBookPlugin.java
+++ craftbook-sponge/src/main/java/com/sk89q/craftbook/sponge/CraftBookPlugin.java
@@ -297,6 +297,7 @@ public class CraftBookPlugin extends CraftBookAPI {
         //Minecart
         moduleController.registerModule("com.sk89q.craftbook.sponge.mechanics.minecart.EmptyDecay");
         moduleController.registerModule("com.sk89q.craftbook.sponge.mechanics.minecart.ExitRemover");
+        moduleController.registerModule("com.sk89q.craftbook.sponge.mechanics.minecart.ItemPickup");
         moduleController.registerModule("com.sk89q.craftbook.sponge.mechanics.minecart.MobBlocker");
         moduleController.registerModule("com.sk89q.craftbook.sponge.mechanics.minecart.MoreRails");
         moduleController.registerModule("com.sk89q.craftbook.sponge.mechanics.minecart.NoCollide");
