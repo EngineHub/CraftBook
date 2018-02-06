9e1f49f9b3581781264dc470f5aabad3cb13a53a
Matthew Miller <mnmiller1@me.com>
2018-02-06 12:29:53 +1000

    Added an ignore powered rail option to ConstantSpeed.

--- src/main/java/com/sk89q/craftbook/mechanics/minecart/ConstantSpeed.java
+++ src/main/java/com/sk89q/craftbook/mechanics/minecart/ConstantSpeed.java
@@ -1,5 +1,6 @@
 package com.sk89q.craftbook.mechanics.minecart;
 
+import org.bukkit.Material;
 import org.bukkit.entity.Minecart;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -21,17 +22,26 @@ public class ConstantSpeed extends AbstractCraftBookMechanic {
         if (!(event.getVehicle() instanceof Minecart)) return;
 
         if (RailUtil.isTrack(event.getTo().getBlock().getType()) && event.getVehicle().getVelocity().lengthSquared() > 0) {
+            if (event.getTo().getBlock().getType() == Material.POWERED_RAIL && !ignorePoweredRail) {
+                if ((event.getTo().getBlock().getData() & 8) == 0) {
+                    return;
+                }
+            }
             Vector vel = event.getVehicle().getVelocity();
             event.getVehicle().setVelocity(vel.normalize().multiply(speed));
         }
     }
 
     private double speed;
+    private boolean ignorePoweredRail;
 
     @Override
     public void loadConfiguration (YAMLProcessor config, String path) {
 
         config.setComment(path + "speed", "Sets the speed to move at constantly.");
         speed = config.getDouble(path + "speed", 0.5);
+
+        config.setComment(path + "ignore-powered-rail", "Whether or not powered rails should be ignored.");
+        ignorePoweredRail = config.getBoolean(path + "ignore-powered-rail", false);
     }
 }
