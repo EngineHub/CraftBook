9bb785eeaf55b5c5b06ad446e2cc0a64bf5233f4
Matthew Miller <mnmiller1@me.com>
2018-05-18 19:08:51 +1000

    Fixed WaterPlaceOnly

--- src/main/java/com/sk89q/craftbook/mechanics/boat/WaterPlaceOnly.java
+++ src/main/java/com/sk89q/craftbook/mechanics/boat/WaterPlaceOnly.java
@@ -1,5 +1,6 @@
 package com.sk89q.craftbook.mechanics.boat;
 
+import com.google.common.collect.ImmutableSet;
 import com.sk89q.craftbook.AbstractCraftBookMechanic;
 import com.sk89q.util.yaml.YAMLProcessor;
 import org.bukkit.ChatColor;
@@ -15,14 +16,14 @@ public class WaterPlaceOnly extends AbstractCraftBookMechanic {
 
     @EventHandler
     public void playerInteractEvent(PlayerInteractEvent event) {
-        if (event.getAction() == Action.RIGHT_CLICK_BLOCK && event.getHand() != EquipmentSlot.HAND) {
+        if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
             if(event.getItem() != null
-                    && event.getItem().getType() == Material.BOAT
-                    && event.getItem().getType() == Material.BOAT_ACACIA
-                    && event.getItem().getType() == Material.BOAT_BIRCH
-                    && event.getItem().getType() == Material.BOAT_DARK_OAK
-                    && event.getItem().getType() == Material.BOAT_JUNGLE
-                    && event.getItem().getType() == Material.BOAT_SPRUCE) {
+                    && (event.getItem().getType() == Material.BOAT
+                    || event.getItem().getType() == Material.BOAT_ACACIA
+                    || event.getItem().getType() == Material.BOAT_BIRCH
+                    || event.getItem().getType() == Material.BOAT_DARK_OAK
+                    || event.getItem().getType() == Material.BOAT_JUNGLE
+                    || event.getItem().getType() == Material.BOAT_SPRUCE)) {
                 Block above = event.getClickedBlock().getRelative(0,1,0);
                 if ((!isWater(above) || event.getClickedBlock().getY() == event.getClickedBlock().getWorld().getMaxHeight() - 1) && !isWater(event.getClickedBlock())) {
                     event.setCancelled(true);
