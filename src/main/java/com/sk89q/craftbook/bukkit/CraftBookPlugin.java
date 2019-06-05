5e617944f25d43ae5fd91e9350e30179b2edf51a
Matthew Miller <mnmiller1@me.com>
2019-06-05 22:11:32 +1000

    Bump to RC2

--- src/main/java/com/sk89q/craftbook/bukkit/CraftBookPlugin.java
+++ src/main/java/com/sk89q/craftbook/bukkit/CraftBookPlugin.java
@@ -320,7 +320,7 @@ public class CraftBookPlugin extends JavaPlugin {
 
     @Nullable
     public static String getVersion() {
-        return null;
+        return "CraftBook 3.10 Release Candidate 2";
     }
 
     public List<CraftBookMechanic> getMechanics() {
