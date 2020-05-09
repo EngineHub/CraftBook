cc030b4b5d92c4d04f353512c61d4df0fc838cbd
Matthew Miller <mnmiller1@me.com>
2020-05-09 21:17:19 +1000

    Work around a Spigot bug where ladders facing north return null for their attached face

--- src/main/java/com/sk89q/craftbook/mechanics/minecart/MoreRails.java
+++ src/main/java/com/sk89q/craftbook/mechanics/minecart/MoreRails.java
@@ -3,17 +3,16 @@ package com.sk89q.craftbook.mechanics.minecart;
 import org.bukkit.Material;
 import org.bukkit.Tag;
 import org.bukkit.block.BlockFace;
+import org.bukkit.block.data.Directional;
+import org.bukkit.block.data.MultipleFacing;
 import org.bukkit.entity.Minecart;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.vehicle.VehicleMoveEvent;
-import org.bukkit.material.Attachable;
-import org.bukkit.material.Vine;
 import org.bukkit.util.Vector;
 
 import com.sk89q.craftbook.AbstractCraftBookMechanic;
 import com.sk89q.craftbook.util.EventUtil;
-import com.sk89q.craftbook.util.LocationUtil;
 import com.sk89q.util.yaml.YAMLProcessor;
 
 
@@ -42,31 +41,32 @@ public class MoreRails extends AbstractCraftBookMechanic {
                     || event.getTo().getBlock().getType() == Material.LIGHT_WEIGHTED_PRESSURE_PLATE)
                 event.getVehicle().setVelocity(event.getVehicle().getVelocity().normalize().multiply(4));
 
-        if (ladder)
+        if (ladder) {
+            double maxSpeed = ((Minecart) event.getVehicle()).getMaxSpeed();
             if (event.getTo().getBlock().getType() == Material.LADDER) {
-                Attachable ladder = (Attachable) event.getTo().getBlock().getState().getData();
-                Vector velocity = new Vector(0,ladderVerticalVelocity,((Attachable) event.getTo().getBlock().getState().getData()).getAttachedFace().getModZ());
-                if(velocity.length() > ((Minecart) event.getVehicle()).getMaxSpeed()) {
-                    double length = velocity.length()/((Minecart) event.getVehicle()).getMaxSpeed();
+                BlockFace face = ((Directional) event.getTo().getBlock().getBlockData()).getFacing().getOppositeFace();
+                Vector velocity = new Vector(0, ladderVerticalVelocity, face.getModZ());
+                if (velocity.length() > maxSpeed) {
+                    double length = velocity.length() / maxSpeed;
                     velocity.setX(velocity.getX() / length);
                     velocity.setY(velocity.getY() / length);
                     velocity.setZ(velocity.getZ() / length);
                 }
-                velocity.add(new Vector(ladder.getAttachedFace().getModX(), 0, ladder.getAttachedFace().getModZ()));
+                velocity.add(new Vector(face.getModX(), 0, face.getModZ()));
                 event.getVehicle().setVelocity(event.getVehicle().getVelocity().add(velocity));
             } else if (event.getTo().getBlock().getType() == Material.VINE) {
                 BlockFace movementFace = BlockFace.SELF;
-                Vine vine = (Vine) event.getTo().getBlock().getState().getData();
-                for(BlockFace test : LocationUtil.getDirectFaces())
-                    if(vine.isOnFace(test)) {
+                MultipleFacing vine = (MultipleFacing) event.getTo().getBlock().getBlockData();
+                for (BlockFace test : vine.getAllowedFaces())
+                    if (vine.hasFace(test)) {
                         movementFace = test;
                         break;
                     }
-                if(movementFace == BlockFace.SELF)
+                if (movementFace == BlockFace.SELF)
                     return;
-                Vector velocity = new Vector(0,ladderVerticalVelocity,0);
-                if(velocity.length() > ((Minecart) event.getVehicle()).getMaxSpeed()) {
-                    double length = velocity.length()/((Minecart) event.getVehicle()).getMaxSpeed();
+                Vector velocity = new Vector(0, ladderVerticalVelocity, 0);
+                if (velocity.length() > maxSpeed) {
+                    double length = velocity.length() / maxSpeed;
                     velocity.setX(velocity.getX() / length);
                     velocity.setY(velocity.getY() / length);
                     velocity.setZ(velocity.getZ() / length);
@@ -74,6 +74,7 @@ public class MoreRails extends AbstractCraftBookMechanic {
                 velocity.add(new Vector(movementFace.getModX(), 0, movementFace.getModZ()));
                 event.getVehicle().setVelocity(event.getVehicle().getVelocity().add(velocity));
             }
+        }
     }
 
     public boolean ladder;
