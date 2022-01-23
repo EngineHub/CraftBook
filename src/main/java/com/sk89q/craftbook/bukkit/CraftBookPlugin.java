a91298e3d6b7bee823392c9ec4212d90f152b6ec
Madeline Miller <mnmiller1@me.com>
2022-01-23 15:58:37 +1000

    Release 3.10.6

--- src/main/java/com/sk89q/craftbook/bukkit/CraftBookPlugin.java
+++ src/main/java/com/sk89q/craftbook/bukkit/CraftBookPlugin.java
@@ -320,7 +320,7 @@ public class CraftBookPlugin extends JavaPlugin {
 
     @Nullable
     public static String getVersion() {
-        return null;
+        return "3.10.6";
     }
 
     public List<CraftBookMechanic> getMechanics() {
