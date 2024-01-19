13803e9d9a14d2ab64b40022ff0ed9ba34084cfe
Madeline Miller <mnmiller1@me.com>
2024-01-19 21:09:57 +1000

    Update language distribution

--- craftbook-core/build.gradle.kts
+++ craftbook-core/build.gradle.kts
@@ -31,7 +31,7 @@ dependencies {
     "implementation"("com.google.code.findbugs:jsr305:1.3.9")
     "implementation"("com.google.code.gson:gson")
     "implementation"("it.unimi.dsi:fastutil")
-    "languageFiles"("${project.group}:craftbook-lang:5.0.0:773@zip")
+    "languageFiles"("${project.group}:craftbook-lang:5.0.0:1310@zip")
 
     "implementation"("org.apache.logging.log4j:log4j-api:${Versions.LOG4J}")
 
