20db7108056287adf64679a69c8389ed0d73c8dd
Philip Hell <blvckbytes@gmail.com>
2024-09-04 17:07:21 +0200

    altered indenting of PipeFilterEvent to be conform with other files

--- src/main/java/com/sk89q/craftbook/mechanics/pipe/PipeFilterEvent.java
+++ src/main/java/com/sk89q/craftbook/mechanics/pipe/PipeFilterEvent.java
@@ -10,44 +10,44 @@ import java.util.Set;
 
 public class PipeFilterEvent extends PipeEvent {
 
-  private static final HandlerList handlers = new HandlerList();
+    private static final HandlerList handlers = new HandlerList();
 
-  private Set<ItemStack> filters;
-  private Set<ItemStack> exceptions;
-  private List<ItemStack> filteredItems;
+    private Set<ItemStack> filters;
+    private Set<ItemStack> exceptions;
+    private List<ItemStack> filteredItems;
 
-  public PipeFilterEvent(Block theBlock, List<ItemStack> items, Set<ItemStack> filters, Set<ItemStack> exceptions, List<ItemStack> filteredItems) {
-    super(theBlock, items);
+    public PipeFilterEvent(Block theBlock, List<ItemStack> items, Set<ItemStack> filters, Set<ItemStack> exceptions, List<ItemStack> filteredItems) {
+        super(theBlock, items);
 
-    this.filters = filters;
-    this.exceptions = exceptions;
-    this.filteredItems = filteredItems;
-  }
+        this.filters = filters;
+        this.exceptions = exceptions;
+        this.filteredItems = filteredItems;
+    }
 
-  public Set<ItemStack> getFilters() {
-    return filters;
-  }
+    public Set<ItemStack> getFilters() {
+        return filters;
+    }
 
-  public Set<ItemStack> getExceptions() {
-    return exceptions;
-  }
+    public Set<ItemStack> getExceptions() {
+        return exceptions;
+    }
 
-  public List<ItemStack> getFilteredItems() {
-    return filteredItems;
-  }
+    public List<ItemStack> getFilteredItems() {
+        return filteredItems;
+    }
 
-  public void setFilteredItems(List<ItemStack> filteredItems) {
-    this.filteredItems = filteredItems;
-  }
+    public void setFilteredItems(List<ItemStack> filteredItems) {
+        this.filteredItems = filteredItems;
+    }
 
-  @Override
-  @Nonnull
-  public HandlerList getHandlers() {
-    return handlers;
-  }
+    @Override
+    @Nonnull
+    public HandlerList getHandlers() {
+        return handlers;
+    }
 
-  @Nonnull
-  public static HandlerList getHandlerList() {
-    return handlers;
-  }
+    @Nonnull
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
 }
