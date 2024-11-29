c1f57c9efb20d6690b652dcceb04c5d7d2201dba
Madeline Miller <mnmiller1@me.com>
2024-11-29 22:00:17 +1000

    Back to SNAPSHOT for continued development

--- src/main/java/com/sk89q/craftbook/bukkit/CraftBookPlugin.java
+++ src/main/java/com/sk89q/craftbook/bukkit/CraftBookPlugin.java
@@ -317,7 +317,7 @@ public class CraftBookPlugin extends JavaPlugin {
 
     @Nullable
     public static String getVersion() {
-        return "3.10.12";
+        return null;
     }
 
     public List<CraftBookMechanic> getMechanics() {
