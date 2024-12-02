e394600c06e103fffc4e55e11caac9a113302082
ANRAR4 <85985268+ANRAR4@users.noreply.github.com>
2024-12-02 13:10:00 +0100

    Pipe support for `crafter`, `decorated_pot` and `chiseled_bookshelf` (#1333)
    * add pipe support for crafter, decorated_pot and chiseled_bookshelf
    
    added crafter, decorated_pot as pipe input and output
    added chiseled_bookshelf as pipe input
    fixed non book items getting deleted when using chiseled_bookshelf as pipe output
    
    * fix pipe event leftovers not respecting disabled slots of crafters
    
    ---------
    
    Co-authored-by: ANRAR4 <->

--- src/main/java/com/sk89q/craftbook/mechanics/pipe/Pipes.java
+++ src/main/java/com/sk89q/craftbook/mechanics/pipe/Pipes.java
@@ -29,6 +29,7 @@ import org.bukkit.block.BlockFace;
 import org.bukkit.block.Dropper;
 import org.bukkit.block.Furnace;
 import org.bukkit.block.Jukebox;
+import org.bukkit.block.Crafter;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.block.data.Directional;
 import org.bukkit.block.data.type.Piston;
@@ -357,6 +358,9 @@ public class Pipes extends AbstractCraftBookMechanic {
                     || fac.getType() == Material.DISPENSER
                     || fac.getType() == Material.HOPPER
                     || fac.getType() == Material.BARREL
+                    || fac.getType() == Material.CHISELED_BOOKSHELF
+                    || fac.getType() == Material.CRAFTER
+                    || fac.getType() == Material.DECORATED_POT
                     || Tag.SHULKER_BOXES.isTagged(fac.getType())) {
                 for (ItemStack stack : ((InventoryHolder) fac.getState()).getInventory().getContents()) {
 
@@ -382,9 +386,13 @@ public class Pipes extends AbstractCraftBookMechanic {
                 }
 
                 if (!items.isEmpty()) {
-                    for (ItemStack item : items) {
-                        if (item == null) continue;
-                        leftovers.addAll(((InventoryHolder) fac.getState()).getInventory().addItem(item).values());
+                    if (fac.getType() == Material.CRAFTER)
+                        leftovers.addAll(InventoryUtil.addItemsToCrafter((Crafter) fac.getState(), items.toArray(new ItemStack[items.size()])));
+                    else {
+                        for (ItemStack item : items) {
+                            if (item == null) continue;
+                            leftovers.addAll(((InventoryHolder) fac.getState()).getInventory().addItem(item).values());
+                        }
                     }
                 }
             } else if (fac.getType() == Material.FURNACE || fac.getType() == Material.BLAST_FURNACE || fac.getType() == Material.SMOKER) {
